a1d846c2f711412aa95c29516731682f674f98b6
Rob May <rob.may@burohappold.com>
2016-03-21 18:07:59 +0000

    COM Interop and registry switched off
    to prevent file-access permissions error on build.

--- BHoM/Properties/AssemblyInfo.cs
+++ BHoM/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b5a280f1-824c-4670-86fd-2322fcc6edb1")]
