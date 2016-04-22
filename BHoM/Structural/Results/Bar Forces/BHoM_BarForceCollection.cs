2dcc6711c03f843dfd87a214bd1e5ead4d39acb2
johannaisak <jomiisak@gmail.com>
2016-04-22 14:09:22 +0100

    Added NodalResult and NodalResultCollection
    Added NodalResult and NodalResultCollection. Also timestep added to key
    in BarForceCollection

--- BHoM/Structural/Results/Bar Forces/BHoM_BarForceCollection.cs
+++ BHoM/Structural/Results/Bar Forces/BHoM_BarForceCollection.cs
@@ -22,13 +22,14 @@ namespace BHoM.Structural.Results.Bars
         //// METHODS ////
         /////////////////
 
-        /// <summary>Adds a bar force to the collection, using a key "LoadcaseNumber:BarNumber:ForcePosition"</summary>
-        public void Add (BarForce barForce)
+        /// <summary>Adds a bar force to the collection, using a key "LoadcaseNumber:BarNumber:ForcePosition:timeStep"</summary>
+        public void Add (BarForce barForce, double timeStep)
         {
             string key = 
                 System.Convert.ToString(barForce.LoadcaseNumber)+ ":" +
                 System.Convert.ToString(barForce.BarNumber) + ":" +
-                System.Convert.ToString(barForce.Position);
+                System.Convert.ToString(barForce.Position) + ":" +
+                System.Convert.ToString(timeStep);
             if (!internalDict.ContainsKey(key)) internalDict.Add(key, barForce);
         }
 
@@ -39,13 +40,14 @@ namespace BHoM.Structural.Results.Bars
         /// <param name="barNumber"></param>
         /// <param name="position"></param>
         /// <returns></returns>
-        public BarForce TryGetBarForce(int loadcaseNumber, int barNumber, int position)
+        public BarForce TryGetBarForce(int loadcaseNumber, int barNumber, int position, double timeStep)
         {
             BarForce barForce;
             internalDict.TryGetValue(
                 System.Convert.ToString(loadcaseNumber) + ":" +
                 System.Convert.ToString(barNumber) + ":" +
-                System.Convert.ToString(position),
+                System.Convert.ToString(position) + ":" +
+                System.Convert.ToString(timeStep),
                 out barForce);
             return barForce;
         }
---
+++ BHoM/Structural/Results/Bar Forces/BHoM_NodalResultCollection.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+
+namespace BHoM.Structural.Results.Nodes
+{
+    /// <summary>
+    /// Collection of bar forces, defines a set of bar forces with the
+    /// ability to get and set envelopes
+    /// </summary>
+    public class NodalResultCollection 
+    {
+        /// <summary>Nodal result dictionary which forms the collection</summary>
+        private Dictionary<string, NodalResult> internalDict { get; set; }
+
+        /// <summary>Constructs and empty nodal result collection</summary>
+        public NodalResultCollection()
+        {
+            internalDict = new Dictionary<string, NodalResult>();
+        }
+        
+        /////////////////
+        //// METHODS ////
+        /////////////////
+
+        /// <summary>Adds a nodal result to the collection, using a key "NodeNumber:timeStep"</summary>
+        public void Add (NodalResult nodalResult, double timeStep)
+        {
+            string key = 
+                System.Convert.ToString(nodalResult.NodeNumber) + ":" +
+                System.Convert.ToString(timeStep);
+            if (!internalDict.ContainsKey(key)) internalDict.Add(key, nodalResult);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nodeNumber"></param>
+        /// <param name="timeStep"></param>
+        /// <returns></returns>
+        public NodalResult TryGetNodalResult(int nodeNumber, double timeStep)
+        {
+            NodalResult nodalResult;
+            internalDict.TryGetValue(
+                System.Convert.ToString(nodeNumber) + ":" +
+                System.Convert.ToString(timeStep),
+                out nodalResult);
+            return nodalResult;
+        }
+    }
+}
