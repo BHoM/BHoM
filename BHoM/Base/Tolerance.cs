16d20f73a67b8158dc7868fa2a22be5782f64a88
adecler <arnaud.declercq@burohappold.com>
2016-08-16 14:59:32 +0100

    adding tolerance definition

---
+++ BHoM/Base/Tolerance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BHoM.Base
+{
+    public static class Tolerance
+    {
+        public const double MIN_DIST = 1e-12;
+    }
+}
