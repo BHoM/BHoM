a5fc3229bd8556a67f5b77648822a3035ba89c53
adecler <arnaud.declercq@burohappold.com>
2017-03-08 17:11:36 +0000

    Commented out legacy code

--- BHoMTest/Program.cs
+++ BHoMTest/Program.cs
@@ -84,7 +84,7 @@ namespace BHoMTest
             }
         }
 
-        public static void BrepJoin()
+        /*public static void BrepJoin()
         {
             List<Point> p1 = new List<Point>();
             p1.Add(new Point(0, 0, 0));
@@ -115,11 +115,11 @@ namespace BHoMTest
             TestDesignElementCreation();
             Console.Read();
             
-        }
+        }*/
 
 
 
-        private static void TestDesignElementCreation()
+        /*private static void TestDesignElementCreation()
         {
             Point p1, p2, p3, p4, p5, p6, p7;
             p1 = new Point(0, 0, 0);
@@ -149,54 +149,54 @@ namespace BHoMTest
             //elem.GetSpanCombinations();
             //Beam beam = new Beam();
             //beam.AddBars(new Bar[] { b3, b1, b4, b2});
-        }
+        }*/
 
 
 
-        public static void TestWriteJson()
-        {
+        //public static void TestWriteJson()
+        //{
 
 
-            /**********************************/
+        //    /**********************************/
 
-            Line line = new Line(new Point(1, 2, 3), new Point(3, 4, 5));
-            string json = JSONWriter.Write(line);
-            Console.WriteLine(json);
+        //    Line line = new Line(new Point(1, 2, 3), new Point(3, 4, 5));
+        //    string json = JSONWriter.Write(line);
+        //    Console.WriteLine(json);
 
-            var item = JsonReader.ReadObject(json);
+        //    var item = JsonReader.ReadObject(json);
 
-            /**********************************/
+        //    /**********************************/
 
-            Bar bar = new Bar(new Point(0, 0, 0), new Point(1, 2, 3));
-            bar.SectionProperty = new SteelSection(ShapeType.Circle, 200, 200, 1, 1, 1, 1);
-            bar.SectionProperty.Material = new BHoM.Materials.Material("TestMaterial");
+        //    Bar bar = new Bar(new Point(0, 0, 0), new Point(1, 2, 3));
+        //    bar.SectionProperty = new SteelSection(ShapeType.Circle, 200, 200, 1, 1, 1, 1);
+        //    bar.SectionProperty.Material = new BHoM.Materials.Material("TestMaterial");
 
-            string json1 = JSONWriter.Write(bar);
-            Console.WriteLine(json1);
+        //    string json1 = JSONWriter.Write(bar);
+        //    Console.WriteLine(json1);
 
-            var item1 = JsonReader.ReadObject(json1);
+        //    var item1 = JsonReader.ReadObject(json1);
 
-            /**********************************/
+        //    /**********************************/
 
-            Dictionary<string, object> dic = new Dictionary<string, object>();
-            dic["A"] = 1;
-            dic["B"] = new int[] { 2, 3, 4 };
-            dic["C"] = new List<string> { "X", "Y", "Z" };
-            dic["D"] = @"\\srv\Project\stuff.file";
+        //    Dictionary<string, object> dic = new Dictionary<string, object>();
+        //    dic["A"] = 1;
+        //    dic["B"] = new int[] { 2, 3, 4 };
+        //    dic["C"] = new List<string> { "X", "Y", "Z" };
+        //    dic["D"] = @"\\srv\Project\stuff.file";
 
-            string json2 = JSONWriter.Write(dic);
-            Console.WriteLine(json2);
+        //    string json2 = JSONWriter.Write(dic);
+        //    Console.WriteLine(json2);
 
-            var item2 = JsonReader.ReadObject(json2);
+        //    var item2 = JsonReader.ReadObject(json2);
 
-            /**********************************/
+        //    /**********************************/
 
-            string json3 = "{\"A\":[1,2,3], \"B\":2}";
-            var item3 = JsonReader.ReadObject(json3);
-        }
+        //    string json3 = "{\"A\":[1,2,3], \"B\":2}";
+        //    var item3 = JsonReader.ReadObject(json3);
+        //}
         
 
-        static void TestProject()
+        /*static void TestProject()
         {
             List<BHoMObject> collection = new List<BHoMObject>();
 
@@ -224,14 +224,14 @@ namespace BHoMTest
             string json = BHoMJSON.WritePackage(collection);
 
             List<BHoMObject> objects = BHoMJSON.ReadPackage(json);
-        }
+        }*/
 
-        static void TestJSON2()
+        /*static void TestJSON2()
         {
 
             string json = "{\"Type\":\"BHoM.Structural.Panel\",\"Primitive\":\"BHoM.Structural.Panel; BHoM; Version=1.0.0.0; Culture=neutral; PublicKeyToken=null\",\"Properties\":{\"Edges\":{\"Primitive\":\"group\",\"groupType\":\"BHoM.Geometry.Curve\",\"group\":[{\"Primitive\":\"line\",\"start\":[18.5527278433439,0.647867078636319,45.315],\"end\":[20.9634789624637,-2.40652705182553,45.315]},{\"Primitive\":\"line\",\"start\":[20.9634789624637,-2.40652705182553,45.315],\"end\":[19.8252869247859,-3.30487140799036,45.315]},{\"Primitive\":\"line\",\"start\":[19.8252869247859,-3.30487140799036,45.315],\"end\":[21.4405330528925,-5.3513698017116,45.315]},{\"Primitive\":\"line\",\"start\":[21.4405330528925,-5.3513698017116,45.315],\"end\":[15.9309504513109,-9.69993513261395,45.315]},{\"Primitive\":\"line\",\"start\":[15.9309504513109,-9.69993513261395,45.315],\"end\":[20.4193086231399,-15.3866336668278,45.315]},{\"Primitive\":\"line\",\"start\":[20.4193086231399,-15.3866336668278,45.315],\"end\":[15.4786819336972,-19.286137402097,45.315]},{\"Primitive\":\"line\",\"start\":[15.4786819336972,-19.286137402097,45.315],\"end\":[21.6090839034293,-27.0532866715617,45.315]},{\"Primitive\":\"line\",\"start\":[21.6090839034293,-27.0532866715617,45.315],\"end\":[59.5345962173439,2.880299941271,45.315]},{\"Primitive\":\"line\",\"start\":[59.5345962173439,2.880299941271,45.315],\"end\":[48.9158845088935,16.3339070306786,45.315]},{\"Primitive\":\"line\",\"start\":[48.9158845088935,16.3339070306786,45.315],\"end\":[41.1255022473884,10.1851678853345,45.315]},{\"Primitive\":\"line\",\"start\":[41.1255022473884,10.1851678853345,45.315],\"end\":[42.0238466035529,9.04697584765641,45.315]},{\"Primitive\":\"line\",\"start\":[42.0238466035529,9.04697584765641,45.315],\"end\":[38.9695268966916,6.6362834690844,45.315]},{\"Primitive\":\"line\",\"start\":[38.9695268966916,6.6362834690844,45.315],\"end\":[38.0711825405269,7.77447550676216,45.315]},{\"Primitive\":\"line\",\"start\":[38.0711825405269,7.77447550676216,45.315],\"end\":[34.1219325940382,4.65743876509753,45.315]},{\"Primitive\":\"line\",\"start\":[34.1219325940382,4.65743876509753,45.315],\"end\":[31.8110184936057,7.58533602506226,45.315]},{\"Primitive\":\"line\",\"start\":[31.8110184936057,7.58533602506226,45.315],\"end\":[30.9194973068434,6.88168233568717,45.315]},{\"Primitive\":\"line\",\"start\":[30.9194973068434,6.88168233568717,45.315],\"end\":[27.9084980181773,10.696584040468,45.315]},{\"Primitive\":\"line\",\"start\":[27.9084980181773,10.696584040468,45.315],\"end\":[29.1330345821453,11.6630778084287,45.315]},{\"Primitive\":\"line\",\"start\":[29.1330345821453,11.6630778084287,45.315],\"end\":[24.5676327138692,17.4473809195925,45.315]},{\"Primitive\":\"line\",\"start\":[24.5676327138692,17.4473809195925,45.315],\"end\":[-41.5581049985984,17.4473809195925,45.315]},{\"Primitive\":\"line\",\"start\":[-41.5581049985984,17.4473809195925,45.315],\"end\":[-41.5581049985984,-17.5602601367444,45.315]},{\"Primitive\":\"line\",\"start\":[-41.5581049985984,-17.5602601367444,45.315],\"end\":[-24.4580312812881,-17.5602601367444,45.315]},{\"Primitive\":\"line\",\"start\":[-24.4580312812881,-17.5602601367444,45.315],\"end\":[-24.4580312812881,-0.212562545568443,45.315]},{\"Primitive\":\"line\",\"start\":[-24.4580312812881,-0.212562545568443,45.315],\"end\":[17.4625733379682,-0.212562545568443,45.315]},{\"Primitive\":\"line\",\"start\":[17.4625733379682,-0.212562545568443,45.315],\"end\":[18.5527278433439,0.647867078636319,45.315]}]},\"ThicknessProperty\":\"55f3838c-79ec-41ca-a934-d0b6e52cd834\",\"BHoM_Guid\":\"9dff7ffc-3f19-4956-b5f8-ced406a083a6\",\"CustomData\":{\"RevitId\":827391}}}";
             var obj = BHoMObject.FromJSON(json, Project.ActiveProject);
-        }
+        }*/
 
         static void TestBarNodes()
         {
@@ -245,7 +245,7 @@ namespace BHoMTest
         }
 
 
-        static void TestWrite()
+        /*static void TestWrite()
         {
             Node n1 = new Node(1, 1, 1);
             n1.CustomData.Add("Number", 1);
@@ -269,111 +269,111 @@ namespace BHoMTest
                 fs.WriteLine(json);
                 fs.Close();
             }
-        }
+        }*/
 
-        static void TestRead()
+        /*static void TestRead()
         {
             using (StreamReader fs = new StreamReader(@"C:\Users\edalton\Documents\temp.js"))
             {
                 Project p = Project.FromJSON(fs.ReadToEnd());
                 fs.Close();
             }
-        }
+        }*/
 
-        static void TestFastSQL()
-        {
-            Console.WriteLine("Program Test Running...");
-            string[] loadCase = new string[] { "DL", "LL", "SDL", "DL+LL", "LC1", "LC2", "LC3", "LC4", "LC5", "LC6", "LC1", "LC2", "LC3", "LC4", "LC5" };
-            ResultServer<BarForce<int, string, int>> results = new ResultServer<BarForce<int, string, int>>();// Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Test1.mdf"));
+        //static void TestFastSQL()
+        //{
+        //    Console.WriteLine("Program Test Running...");
+        //    string[] loadCase = new string[] { "DL", "LL", "SDL", "DL+LL", "LC1", "LC2", "LC3", "LC4", "LC5", "LC6", "LC1", "LC2", "LC3", "LC4", "LC5" };
+        //    ResultServer<BarForce<int, string, int>> results = new ResultServer<BarForce<int, string, int>>();// Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Test1.mdf"));
 
-            List<BarForce<int, string, int>> resultsList = new List<BarForce<int, string, int>>();
-            Random r = new Random();
-            List<string> barNums = new List<string>();
-            List<string> cases = new List<string>();
-            int counter = 0;
-            int tolalCount = 0;
-            results.ClearData();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
+        //    List<BarForce<int, string, int>> resultsList = new List<BarForce<int, string, int>>();
+        //    Random r = new Random();
+        //    List<string> barNums = new List<string>();
+        //    List<string> cases = new List<string>();
+        //    int counter = 0;
+        //    int tolalCount = 0;
+        //    results.ClearData();
+        //    Stopwatch sw = new Stopwatch();
+        //    sw.Start();
 
-            for (int j = 0; j < 5/*loadCase.Length*/; j++)
-            {
-                for (int timeStep = 1; timeStep < 5; timeStep++)
-                {
-                    for (int i = 0; i < 10000; i++)
-                    {
-                        for (int segment = 0; segment < 3; segment++)
-                        {
-                            resultsList.Add(new BarForce<int, string, int>((i + 1), j.ToString(), segment, 3, timeStep, (double)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20));
-                            counter++;
-                            tolalCount++;
-                            if (counter == 1000000)
-                            {
-                                results.StoreData(resultsList);
-                                resultsList.Clear();
-                                counter = 0;
-                                Console.WriteLine("Added block = {0}", sw.Elapsed);
+        //    for (int j = 0; j < 5/*loadCase.Length*/; j++)
+        //    {
+        //        for (int timeStep = 1; timeStep < 5; timeStep++)
+        //        {
+        //            for (int i = 0; i < 10000; i++)
+        //            {
+        //                for (int segment = 0; segment < 3; segment++)
+        //                {
+        //                    resultsList.Add(new BarForce<int, string, int>((i + 1), j.ToString(), segment, 3, timeStep, (double)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20, (float)r.NextDouble() * 20));
+        //                    counter++;
+        //                    tolalCount++;
+        //                    if (counter == 1000000)
+        //                    {
+        //                        results.StoreData(resultsList);
+        //                        resultsList.Clear();
+        //                        counter = 0;
+        //                        Console.WriteLine("Added block = {0}", sw.Elapsed);
 
-                            }
-                        }
-                    }
-                }
+        //                    }
+        //                }
+        //            }
+        //        }
 
-            }
-            results.StoreData(resultsList);
-            resultsList.Clear();
+        //    }
+        //    results.StoreData(resultsList);
+        //    resultsList.Clear();
 
-            for (int i = 0; i <= 2000; i++)
-            {
-                barNums.Add(i.ToString());
-            }
+        //    for (int i = 0; i <= 2000; i++)
+        //    {
+        //        barNums.Add(i.ToString());
+        //    }
 
-            for (int i = 0; i < 50; i++)
-            {
-                cases.Add(i.ToString());
-            }
+        //    for (int i = 0; i < 50; i++)
+        //    {
+        //        cases.Add(i.ToString());
+        //    }
 
-            // barNums.Reverse();      
+        //    // barNums.Reverse();      
 
-            Console.WriteLine("Time Elapsed to store {0} results to DB = {1}", tolalCount, sw.Elapsed);
-            //System.IO.
-            sw.Restart();
+        //    Console.WriteLine("Time Elapsed to store {0} results to DB = {1}", tolalCount, sw.Elapsed);
+        //    //System.IO.
+        //    sw.Restart();
 
-            results.NameSelection = barNums;
-            results.LoadcaseSelection = cases;
+        //    results.NameSelection = barNums;
+        //    results.LoadcaseSelection = cases;
 
-            results.LoadData();
+        //    results.LoadData();
 
-            Console.WriteLine("Time Elapsed to load {0} results into memory = {1}, ", results.ResultCount.ToString(), sw.Elapsed);
-            sw.Restart();
+        //    Console.WriteLine("Time Elapsed to load {0} results into memory = {1}, ", results.ResultCount.ToString(), sw.Elapsed);
+        //    sw.Restart();
 
-            results.MaxEnvelope();
+        //    results.MaxEnvelope();
 
-            Console.WriteLine("Time taken to Envelope all results = {0}", sw.Elapsed);
+        //    Console.WriteLine("Time taken to Envelope all results = {0}", sw.Elapsed);
 
-            sw.Restart();
-            List<BarForce<int, string, int>> barForce1 = results.ToList();
-
-
-            Console.WriteLine("Time taken to type convert all data to List = {0}", sw.Elapsed);
-
-            sw.Restart();
-            List<object[]> barForce3 = results.ToListData();
+        //    sw.Restart();
+        //    List<BarForce<int, string, int>> barForce1 = results.ToList();
 
 
-            Console.WriteLine("Time taken to load raw data to list = {0}", sw.Elapsed);
-            sw.Restart();
+        //    Console.WriteLine("Time taken to type convert all data to List = {0}", sw.Elapsed);
 
-            results.OrderBy = ResultOrder.Loadcase;
-
-            Console.WriteLine("Time taken To reorder = {0}", sw.Elapsed);
-
-            sw.Restart();
+        //    sw.Restart();
+        //    List<object[]> barForce3 = results.ToListData();
 
 
-            sw.Stop();
-            Console.ReadLine();
-        }
+        //    Console.WriteLine("Time taken to load raw data to list = {0}", sw.Elapsed);
+        //    sw.Restart();
+
+        //    results.OrderBy = ResultOrder.Loadcase;
+
+        //    Console.WriteLine("Time taken To reorder = {0}", sw.Elapsed);
+
+        //    sw.Restart();
+
+
+        //    sw.Stop();
+        //    Console.ReadLine();
+        //}
 
         static void TestJSON()
         {
