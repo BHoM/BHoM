2044cb03121f330f95bdec8f846e02925eeabd61
epignatel <epignatel@BTH-R90F43Q3>
2017-05-30 22:20:16 +0100

    Fixed Polyline Length

--- BHoM/Geometry/Curve/Polyline.cs
+++ BHoM/Geometry/Curve/Polyline.cs
@@ -10,11 +10,11 @@ namespace BHoM.Geometry
 {
     public class Polyline : Curve
     {
-        internal Polyline() { }
+        public Polyline() { }
 
         public Polyline(List<Point> points) : base(points) { }
 
-        internal Polyline(List<double[]> pnts) : base(pnts) { }
+        public Polyline(List<double[]> pnts) : base(pnts) { }
 
         public override void CreateNurbForm()
         {
@@ -35,11 +35,11 @@ namespace BHoM.Geometry
         public override double Length
         {
             get
-            {
+            {               
                 double length = 0;
-                for (int i = 0; i < m_ControlPoints.Length / (m_Dimensions + 1) - (m_Dimensions + 1); i++)
+                for (int i = 1; i < ControlPoints.Count; i++)
                 {
-                    length += VectorUtils.Length(VectorUtils.Sub(m_ControlPoints, i, i + m_Dimensions + 1, m_Dimensions + 1));
+                    length += new Vector(ControlPoints[i].X - ControlPoints[i-1].X, ControlPoints[i].Y - ControlPoints[i - 1].Y, ControlPoints[i].Z - ControlPoints[i - 1].Z).Length;
                 }
                 return length;
             }
