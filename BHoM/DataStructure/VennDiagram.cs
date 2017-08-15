01566c6020cee832d49546d8467a7041e52e5c23
Arnaud Declercq <arnaud.declercq@burohappold.com>
2017-08-15 16:41:10 +0100

    Adding Venn Diagram

---
+++ BHoM/DataStructure/VennDiagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.DataStructure
+{
+    public class VennDiagram<T>
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public List<Tuple<T,T>> Intersection { get; set; } = new List<Tuple<T,T>>();
+
+        public List<T> OnlySet1 { get; set; } = new List<T>();
+
+        public List<T> OnlySet2 { get; set; } = new List<T>();
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public VennDiagram() { }
+    }
+}
