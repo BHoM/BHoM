4c06dfc5f4cbb70ea793d9c59221a89481df33a2
Rob May <rob.may@burohappold.com>
2017-09-05 10:16:38 +0100

    Adding test grid (line for now)
    Curves[] causing crashes in Dynamo at the moment.

---
+++ BHoM/Architecture/Elements/Grid.cs
@@ -0,0 +1,36 @@
+﻿using BHoM.Geometry;
+using BHoM.Base;
+using System;
+using System.Reflection;
+using BHoM.Structural.Loads;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BHoM.Architectural.Elements
+{
+    /// <summary>
+    /// Building grids for setting out - curve (list of) based objects with name and text parameters
+    /// </summary>
+    public class Grid : BHoMObject 
+    {
+        public Grid()
+        {
+
+        }
+        /// <summary>
+        /// Construct grid by passing list of curves and name
+        /// </summary>
+        public Grid(BHoM.Geometry.Line line)
+        {
+            Line = line;
+        }
+        
+        /// <summary>
+        /// Grid curves
+        /// </summary>
+        public Curve Line
+        {
+            get;set;
+         }
+    }
+}
