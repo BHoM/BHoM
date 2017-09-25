61a7c102045f215fcc57f8fedbb8ad962bbe52b1
Rob May <rob.may@burohappold.com>
2017-09-25 08:20:35 +0100

    ISpatialObject interface added for discussion
    Added to Base, and implemented in structural 'Bar'. If an object is
    supposed to be a geometric object, we should have an interface that will
    control how the object geometry is named, accessed etc. This will become
    very important when we need to represent different geometries of an
    object for different levels of detail etc. We should discuss object
    geometry asap, as there is a potentially huge optimisation if we force
    the idea of similar geometries - identical geometries could be
    referenced to different objects with a 'location'. This way, if you had
    a very complex geometric object (a car engine) for a car park, you would
    only store the geometry once, alongside the location (parking spaces)
    for each engine (location).

---
+++ BHoM/Base/Interface/ISpatialObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Geometry;
+
+namespace BH.oM.Base
+{
+    public interface ISpatialObject
+    {
+        IBHoMGeometry Geometry { get; set; }
+    }
+}
