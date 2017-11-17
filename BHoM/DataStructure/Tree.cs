8b8e5725ae42ed182acc07bcb71c33c845bbca78
adecler <arnaud.declercq@burohappold.com>
2017-11-17 14:23:08 +0800

    Adding tree to data structures

---
+++ BHoM/DataStructure/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.DataStructure
+{
+    public class Tree<T>
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public Dictionary<string, Tree<T>> Children { get; set; } = new Dictionary<string, Tree<T>>();
+
+        public string Name { get; set; } = "";
+
+        public T Value { get; set; } = default(T);
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public Tree() { }
+
+        /***************************************************/
+
+        public Tree(T value, string name = "")
+        {
+            Value = value;
+            Name = name;
+        }
+
+        /***************************************************/
+
+        public Tree(Dictionary<string, Tree<T>> children, string name = "")
+        {
+            Children = children;
+            Name = name;
+        }
+    }
+}
