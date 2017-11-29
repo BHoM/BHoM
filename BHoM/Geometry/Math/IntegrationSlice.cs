f11371da4dc32a9dd7973120c2ae522a53e4d97d
INaslund <isak.naslund@burohappold.com>
2017-11-29 11:09:00 +0100

    IntegrationSlice
    Moved IntegrationSlice (Previously called Slice) from the Engine to the BHoM

---
+++ BHoM/Geometry/Math/IntegrationSlice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Geometry
+{
+    public struct IntegrationSlice
+    {
+        public double Width;
+        public double Length;
+        public double Centre;
+        public double[] Placement;
+
+        public IntegrationSlice(double width, double length, double centre, double[] placement)
+        {
+            Width = width;
+            Length = length;
+            Centre = centre;
+            Placement = placement;
+        }
+    }
+}
