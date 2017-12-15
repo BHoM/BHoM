5e0bf5a56723ba3797b51cf61fab293e4b550f58
Eduardo Pignatelli <eduardo.pignatelli@burohappold.com>
2017-12-15 10:04:32 +0000

    Fixed inheritance

--- BHoM/Acoustic/Parameters/RT60.cs
+++ BHoM/Acoustic/Parameters/RT60.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BH.oM.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BH.oM.Acoustic
 {
-    public class RT60 : IAcousticParameter
+    public class RT60 : BHoMObject, IAcousticParameter
     {
         /***************************************************/
         /**** Properties                                ****/
