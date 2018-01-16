021f0731b847a17b6da21384e12ccdfe59ecb3b7
IsakNaslund <isak.naslund@burohappold.com>
2018-01-16 12:48:54 +0100

    SLight tweak to comparers

--- BHoM/Structural/Results/Bar Results/BarResult.cs
+++ BHoM/Structural/Results/Bar Results/BarResult.cs
@@ -32,19 +32,19 @@ namespace BH.oM.Structural.Results
 
         public int CompareTo(IResult other)
         {
-            BarResult otherBar = other as BarResult;
+            BarResult otherRes = other as BarResult;
 
-            if (otherBar == null)
+            if (otherRes == null)
                 return this.GetType().Name.CompareTo(other.GetType().Name);
 
-            int n = this.ObjectId.CompareTo(otherBar.ObjectId);
+            int n = this.ObjectId.CompareTo(otherRes.ObjectId);
             if (n == 0)
             {
-                int l = this.LoadCase.CompareTo(otherBar.LoadCase);
+                int l = this.LoadCase.CompareTo(otherRes.LoadCase);
                 if (l == 0)
                 {
-                    int f = this.Position.CompareTo(otherBar.Position);
-                    return f == 0 ? this.TimeStep.CompareTo(otherBar.TimeStep) : f;
+                    int t = this.TimeStep.CompareTo(otherRes.TimeStep);
+                    return t == 0 ? this.Position.CompareTo(otherRes.Position) : t;
                 }
                 else
                 {
