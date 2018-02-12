4abc6d2c6cbd3397b84cca7e01df1335f0aaf4ce
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-02-12 14:42:10 +0000

    added type

--- BHoM/Environmental/Elements/BuildingElementPanel.cs
+++ BHoM/Environmental/Elements/BuildingElementPanel.cs
@@ -20,6 +20,7 @@ namespace BH.oM.Environmental.Elements
 
         //Geometry
         public PolyCurve PolyCurve { get; set; } = new PolyCurve();
+        public string Type { get; set; } = "";
 
         //Detail Surface Results
         public double ApertureFlowIn { get; set; } = 0.0;
