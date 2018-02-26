c3ba130b048ed4c2208874f26e8ce2d8a23eab02
Pawel Baran <pawel.baran@burohappold.com>
2018-02-26 18:00:30 +0100

    Distance tolerance fix

--- BHoM/Geometry/Misc/Tolerance.cs
+++ BHoM/Geometry/Misc/Tolerance.cs
@@ -2,7 +2,7 @@
 {
     public static class Tolerance
     {
-        public const double Distance = 1e-6;
+        public const double Distance = 1e-9;
         public const double SqrtDist = Distance * Distance;
         public const double Angle = 1e-4;
     }
