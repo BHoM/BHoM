04b596be9d70714a9b14fff45386b18c7b311be5
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-03-19 10:50:29 +0000

    Building class updated to reflect changes for building element adjacency

--- BHoM/Environmental/Elements/BuildingElement.cs
+++ BHoM/Environmental/Elements/BuildingElement.cs
@@ -2,6 +2,7 @@
 using BH.oM.Environmental.Interface;
 using BH.oM.Environmental.Properties;
 using BH.oM.Architecture.Elements;
+using System.Collections.Generic;
 
 namespace BH.oM.Environmental.Elements
 {
@@ -14,6 +15,7 @@ namespace BH.oM.Environmental.Elements
         public Level Level { get; set; } = new Level();
         public BuildingElementProperties BuildingElementProperties { get; set; } = new BuildingElementProperties();
         public IBuildingElementGeometry BuildingElementGeometry { get; set; } = new BuildingElementPanel();
+        public List<Space> AdjacentSpaces { get; set; } = new List<Space>();
 
         /***************************************************/
     }
