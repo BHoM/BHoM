22b1657c347ec6b698ee63ec9f4f5a540db1dff5
INaslund <isak.naslund@burohappold.com>
2018-04-12 16:46:46 +0200

    Tolerances updated

--- BHoM/Geometry/Misc/Tolerance.cs
+++ BHoM/Geometry/Misc/Tolerance.cs
@@ -2,8 +2,9 @@
 {
     public static class Tolerance
     {
-        public const double Distance = 1e-9;
-        public const double SqrtDist = Distance * Distance;
-        public const double Angle = 1e-9;
+        public const double MicroDistance = 1e-9;
+        public const double Distance = 1e-6;
+        public const double MacroDistance = 1e-3;
+        public const double Angle = 1e-6;
     }
 }
