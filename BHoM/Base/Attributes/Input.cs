2e688f0482ad07e3cd3b4f73d3ebbec4cc6712cc
adecler <arnaud.declercq@burohappold.com>
2018-04-29 11:10:10 +0800

    Adding custom attributes for method description

---
+++ BHoM/Base/Attributes/Input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class Input : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Name { get; private set; } = "";
+
+        public string Description { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public Input(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+
+        /***************************************************/
+    }
+}
---
+++ BHoM/Base/Attributes/Output.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class Output : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Description { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public Output(string description)
+        {
+            Description = description;
+        }
+
+
+        /***************************************************/
+    }
+}
