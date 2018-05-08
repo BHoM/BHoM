98fa797ea780df8e4fd9025cd54fa3e582929d95
adecler <arnaud.declercq@burohappold.com>
2018-05-08 11:17:50 +0800

    Adding Unit test objects

---
+++ BHoM/Reflection/Testing/TestData.cs
@@ -0,0 +1,24 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Testing
+{
+    public class TestData : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public List<object> Inputs { get; set; } = new List<object>();
+
+        public object Output { get; set; } = null;
+
+
+        /***************************************************/
+    }
+}
---
+++ BHoM/Reflection/Testing/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Testing
+{
+    public class UnitTest : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public MethodBase Method { get; set; } = null;
+
+        public List<TestData> Data { get; set; } = new List<TestData>();
+
+
+        /***************************************************/
+    }
+}
