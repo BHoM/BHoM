ec5841acd3497c71d4bdfdc6d322ea8afad193fe
al-fisher <al.fisher@burohappold.com>
2018-06-14 14:01:49 +0100

    Adding Planning Namespace and intial Object definitions migrated from GitHub_Toolkit

---
+++ BHoM/Planning/Milestone.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Base;
+using System;
+
+namespace BH.oM.Planning
+{
+    public class Milestone : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public DateTimeOffset DueOn { get; set; }
+
+        public ItemState State { get; set; }
+
+        /***************************************************/
+    }
+}
