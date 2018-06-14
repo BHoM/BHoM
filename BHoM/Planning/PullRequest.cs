d8677f02d4d5551ab702d3f5ddc0cad21c4f03d1
al-fisher <al.fisher@burohappold.com>
2018-06-14 14:01:49 +0100

    Adding Planning Namespace and intial Object definitions migrated from GitHub_Toolkit

---
+++ BHoM/Planning/PullRequest.cs
@@ -0,0 +1,34 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+
+namespace BH.oM.Planning
+{
+    public class PullRequest : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Creator { get; set; }
+
+        public string Title { get; set; }
+
+        public string RepoName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+
+        public string MilestoneName { get; set; }
+
+        public int Additions { get; set; }
+
+        public int Deletions { get; set; }
+
+        public List<string> Reviewers { get; set; } = new List<string>();
+
+
+        /***************************************************/
+    }
+}
