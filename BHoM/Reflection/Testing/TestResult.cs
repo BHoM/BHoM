b4fbe1282c1fb479b6a6ce1914153df68b0547c4
Isak Naslund <Isak.Naslund@burohappold.com>
2018-06-14 17:24:03 +0200

    Added test result class

---
+++ BHoM/Reflection/Testing/TestResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using BH.oM.Planning;
+using BH.oM.Base;
+
+namespace BH.oM.Reflection.Testing
+{
+    public class TestResult : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public MethodBase Method { get; set; } = null;
+
+        public List<bool> Results { get; set; } = new List<bool>();
+
+        public Issue Issue { get; set; } = null;
+
+        /***************************************************/
+    }
+}
