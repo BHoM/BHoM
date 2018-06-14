9b6bd3eb498bc7fc13f1f03625d75edca225eb38
al-fisher <al.fisher@burohappold.com>
2018-06-14 14:01:49 +0100

    Adding Planning Namespace and intial Object definitions migrated from GitHub_Toolkit

---
+++ BHoM/Planning/Issue.cs
@@ -0,0 +1,34 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+
+namespace BH.oM.Planning
+{
+    public class Issue : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Creator { get; set; }
+
+        public List<string> Assignees { get; set; }
+
+        public string Title { get; set; }
+
+        public string RepoName { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTimeOffset CreationDate { get; set; }
+
+        public string MilestoneName { get; set; }
+
+        public bool PullRequested { get; set; } = false;
+
+        public List<string> Labels { get; set; } = new List<string>();
+
+
+        /***************************************************/
+    }
+}
---
+++ BHoM/Planning/Label.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Base;
+using System;
+
+namespace BH.oM.Planning
+{
+    public class Label : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Colour { get; set; } = "";
+
+
+        /***************************************************/
+
+    }
+}
