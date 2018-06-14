8fb6e1d8cfd702a1e3cec7d9f70dc6fe31cbed4e
al-fisher <al.fisher@burohappold.com>
2018-06-14 14:21:53 +0100

    Adding Humans to BHoM :couple:

---
+++ BHoM/Humans/Human.cs
@@ -0,0 +1,37 @@
+﻿using BH.oM.Base;
+using BH.oM.Humans;
+
+namespace BH.oM.Humans
+{
+    public class Human : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Office { get; set; }
+
+        public string Discipline { get; set; }
+
+        public string Team { get; set; }
+
+        public string Grade { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string YammerLogin { get; set; }
+
+        public string GithubLogin { get; set; }
+
+        public Skeleton Skeleton { get; set; }
+
+        public string CodeDevelopmentRole { get; set; }
+
+        public string BusinessUnit { get; set; }
+
+
+        /***************************************************/
+    }
+}
