62f9b83a521687d003c9cc5ef02a9d0c8d6cf489
Isak Naslund <Isak.Naslund@burohappold.com>
2018-06-29 10:14:42 +0200

    Changed casting order from circle -> arc to arc-> circle

--- BHoM/Geometry/Curve/Arc.cs
+++ BHoM/Geometry/Curve/Arc.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System;
 
 namespace BH.oM.Geometry
 {
@@ -23,9 +24,38 @@ namespace BH.oM.Geometry
         /**** Explicit Casting                          ****/
         /***************************************************/
 
-        public static explicit operator Circle(Arc arc)
+        public static explicit operator Arc(Circle circle)
         {
-            return new Circle { Centre = arc.CoordinateSystem.Origin, Normal = arc.CoordinateSystem.Z, Radius = arc.Radius };
+            //No way of having explicit operators outside the class definition, why some code for crossproducts etc is stored in here.
+
+            double length = Math.Sqrt(circle.Normal.X * circle.Normal.X + circle.Normal.Y * circle.Normal.Y + circle.Normal.Z * circle.Normal.Z);
+
+            //Get normalised z vector
+            Vector z = new Vector { X = circle.Normal.X / length, Y = circle.Normal.Y / length, Z = circle.Normal.Z / length };
+
+            Vector x;
+            if (z.Y != 0 && z.Z != 0)
+                x = Vector.XAxis;
+            else
+                x = Vector.YAxis;
+
+            //Project local x to the plane
+            x = x - (x.X * z.X + x.Y * z.Y + x.Z * z.Z) * z;
+
+            //Normalise x
+            length = Math.Sqrt(x.X * x.X + x.Y * x.Y + x.Z * x.Z);
+            x = x / length;
+
+            //Crossproduct to get local y
+            Vector y = new Vector { X = z.Y * x.Z - z.Z * x.Y, Y = z.Z * x.X - z.X * x.Z, Z = z.X * x.Y - z.Y * x.X };
+
+            return new Arc
+            {
+                CoordinateSystem = new CoordinateSystem { X = x, Y = y, Z = z, Origin = circle.Centre },
+                Radius = circle.Radius,
+                StartAngle = 0,
+                EndAngle = Math.PI * 2
+            };
         }
 
         /***************************************************/
