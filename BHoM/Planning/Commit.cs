e304eefdea15966872466b5bb4bfbad12b8057af
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-10 13:07:38 +0800

    Moving Test classes to the Testing namespace

--- BHoM/Planning/Commit.cs
+++ BHoM/Planning/Commit.cs
@@ -12,6 +12,16 @@ namespace BH.oM.Planning
         public string RepoName { get; set; }
 
         public DateTimeOffset Date { get; set; }
+		
+		public string Author { get; set; }
+		
+		public string Committer { get; set; }
+
+        public int Additions { get; set; }
+
+        public int Deletions { get; set; }
+
+        public int TotalChanges { get; set; }
 
 
         /***************************************************/
