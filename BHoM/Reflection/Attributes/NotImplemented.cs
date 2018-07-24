e7e4be8cd2289a564772adbe0ad0b4d62c0e809a
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-24 16:13:34 +0800

    Addinf CustomAttributes for deprecated, released and not implemented

---
+++ BHoM/Reflection/Attributes/NotImplemented.cs
@@ -0,0 +1,32 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class NotImplemented : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Description { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public NotImplemented(string description = "")
+        {
+            Description = description;
+        }
+
+
+        /***************************************************/
+    }
+}
---
+++ BHoM/Reflection/Attributes/Released.cs
@@ -0,0 +1,35 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class Released : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Description { get; private set; } = "";
+
+        public string FromVersion { get; private set; } = "1.0.0.0";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public Released(string fromVersion, string description = "")
+        {
+            Description = description;
+            FromVersion = fromVersion;
+        }
+
+
+        /***************************************************/
+    }
+}
