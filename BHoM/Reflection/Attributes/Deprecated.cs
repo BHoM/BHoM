c8e8217fd33135bf9d158acb6a8db6d7ce5cf294
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-24 16:13:34 +0800

    Addinf CustomAttributes for deprecated, released and not implemented

---
+++ BHoM/Reflection/Attributes/Deprecated.cs
@@ -0,0 +1,43 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class Deprecated : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Description { get; private set; } = "";
+
+        public string FromVersion { get; private set; } = "1.0.0.0";
+
+        public Type ReplacingType { get; private set; } = null;
+
+        public string ReplacingMethod { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public Deprecated(string fromVersion, string description = "", Type newType = null, string newMethod = "")
+        {
+            Description = description;
+            FromVersion = fromVersion;
+            ReplacingType = newType;
+            ReplacingMethod = newMethod;
+        }
+
+
+
+        /***************************************************/
+    }
+}
