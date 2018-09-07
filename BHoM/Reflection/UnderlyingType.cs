5990b05dd165354fe715244de72c01a793e058c3
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-09-07 13:46:09 +0800

    Adding UnderlyingType object definition

---
+++ BHoM/Reflection/UnderlyingType.cs
@@ -0,0 +1,23 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection
+{
+    public class UnderlyingType : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public Type Type { get; set; } = null;
+
+        public int Depth { get; set; } = 0;
+
+
+        /***************************************************/
+    }
+}
