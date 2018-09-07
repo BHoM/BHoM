b3b559c22db4cbd2052abe8c653a8db04de79452
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-09-07 13:43:42 +0800

    Fixes #263 - multi-output support for methods

---
+++ BHoM/Reflection/Attributes/MultiOutputAttribute.cs
@@ -0,0 +1,38 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class MultiOutputAttribute : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public int Index { get; private set; } = 0;
+
+        public string Name { get; private set; } = "";
+
+        public string Description { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public MultiOutputAttribute(int index, string name, string description)
+        {
+            Index = index;
+            Name = name;
+            Description = description;
+        }
+
+
+        /***************************************************/
+    }
+}
