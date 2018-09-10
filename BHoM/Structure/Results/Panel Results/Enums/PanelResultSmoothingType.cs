5702e8fd575b6c973b22d3de58da612c4380c3e5
Rob May <rob.may@burohappold.com>
2018-09-10 14:36:40 +0100

    Add layer and smoothing enums and properties to PanelResult
    Also update PanelStress properties

---
+++ BHoM/Structure/Results/Panel Results/Enums/PanelResultSmoothingType.cs
@@ -0,0 +1,15 @@
+﻿namespace BH.oM.Structure.Results
+{
+    /***************************************************/
+
+    public enum PanelResultSmoothingType
+    {
+        None = 0,
+        WithinPanel = 1,
+        FiniteElementCentres = 2,
+        Global = 3,
+        BySelection = 4
+    }
+
+    /***************************************************/
+}
