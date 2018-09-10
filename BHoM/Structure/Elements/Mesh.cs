ed476dab44623bf8acf6d99b4e81f2c47d841cf6
Rob May <rob.may@burohappold.com>
2018-09-10 19:17:04 +0100

    Add structural Mesh object

---
+++ BHoM/Structure/Elements/Mesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using BH.oM.Structure.Properties;
+
+namespace BH.oM.Structure.Elements
+{
+    public class MeshFace : Base.BHoMObject, IAreaElement
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public IProperty2D Property { get; set; } = new ConstantThickness();
+        
+
+        /***************************************************/
+    }
+}
+     
