a1df2e0c177ef746d9b81e7404361dbf679ce4bf
Rob May <rob.may@burohappold.com>
2018-09-12 16:49:59 +0100

    Adding mesh result type (stress/force/displacement) and descriptions

---
+++ BHoM/Structure/Results/Mesh/Enums/MeshResultType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace BH.oM.Structure.Results
+{
+    /***************************************************/
+
+    [Description("Specifies which layer the results are extracted from or if it is a maxima/minima of the layers")]
+    public enum MeshResultType
+    {
+        [Description("Bending moments and shear forces out-of-plane, membrane forces in the plane of the mesh/element")]
+        Forces = 0,
+
+        [Description("Stresses in the plane of the mesh/element")]
+        Stresses = 1,
+
+        [Description("Displacements of the mesh/element nodes")]
+        Displacements = 2,
+    }
+
+    /***************************************************/
+}
