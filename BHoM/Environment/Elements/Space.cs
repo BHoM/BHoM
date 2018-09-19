bfcb4982b3c662dfa499bf1afb1f96212bec0094
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 10:12:44 +0100

    Removing unnecessary level properties from objects.
    Removing geometrical properties from the space - Issue #281

--- BHoM/Environment/Elements/Space.cs
+++ BHoM/Environment/Elements/Space.cs
@@ -13,17 +13,6 @@ namespace BH.oM.Environment.Elements
         /**** Properties                                ****/
         /***************************************************/
 
-        public Level Level { get; set; } = new Level();
-        public Point Location { get; set; } = new Point();
-        //TODO: remove BuildingElements from Space
-        /// <summary> 
-        /// BuidlingElements will be depreciated. Link between Space and Building moved to AdjacenSpaces in Building Element.
-        /// </summary>
-        public List<BuildingElement> BuildingElements { get; set; } = new List<BuildingElement>();
-        public List<IBuildingObject> BuildingObjects { get; set; } = new List<IBuildingObject>();
-        public List<InternalCondition> InternalConditions { get; set; } = new List<InternalCondition>();
-        public List<IEquipmentProperties> EquipmentProperties { get; set; } = new List<IEquipmentProperties>();
-
         public string Number { get; set; } = "";
 
         /***************************************************/
