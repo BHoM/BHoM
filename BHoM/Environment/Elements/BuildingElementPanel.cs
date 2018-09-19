d6171145461b8f4405fb99c08d65440229c05762
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 14:02:18 +0100

    Updated Building Element Panel properties following Al's comments - Issue #274

--- BHoM/Environment/Elements/BuildingElementPanel.cs
+++ BHoM/Environment/Elements/BuildingElementPanel.cs
@@ -2,7 +2,7 @@
 
 using BH.oM.Geometry;
 using BH.oM.Base;
-using BH.oM.Environment.Interface;
+using BH.oM.Environment.Properties;
 
 namespace BH.oM.Environment.Elements
 {
@@ -11,28 +11,11 @@ namespace BH.oM.Environment.Elements
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
-        //THIS IS THE ANALYTICAL REPRESENTATION OF THE BUILDING ELEMENT
 
-        //Detail Surface Results
-        public double ApertureFlowIn { get; set; } = 0.0;
-        public double ApertureFlowOut { get; set; } = 0.0;
-        public double ApertureOpening { get; set; } = 0.0;
-        public double ExternalCondensation { get; set; } = 0.0;
-        public double ExternalConduction { get; set; } = 0.0;
-        public double ExternalConvection { get; set; } = 0.0;
-        public double ExternalLongWave { get; set; } = 0.0;
-        public double ExternalSolar { get; set; } = 0.0;
-        public double ExternalTemperature { get; set; } = 0.0;
-        public double InternalCondensation { get; set; } = 0.0;
-        public double InternalConduction { get; set; } = 0.0;
-        public double InternalConvection { get; set; } = 0.0;
-        public double InternalLongWave { get; set; } = 0.0;
-        public double InternalSolar { get; set; } = 0.0;
-        public double InternalTemperature { get; set; } = 0.0;
-        public double InterstitialCondensation { get; set; } = 0.0;
+        public ICurve PanelCurve { get; set; } = new PolyCurve();
+        public List<BuildingElementOpening> Openings { get; set; } = new List<BuildingElementOpening>();
 
-        //boolean to toggle detail surface output
-        public bool SurfaceOutput { get; set; } = true;
+        public BuildingPanelProperties PanelProperties { get; set; } = new BuildingPanelProperties();
 
         /***************************************************/
     }
