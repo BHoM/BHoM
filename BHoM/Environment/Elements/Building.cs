a6d81ebd284e57870a12c0cb8f03943b8b3e58cf
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 15:06:26 +0100

    Removing commented out building elements

--- BHoM/Environment/Elements/Building.cs
+++ BHoM/Environment/Elements/Building.cs
@@ -19,14 +19,6 @@ namespace BH.oM.Environment.Elements
         public double Elevation { get; set; } = 0.0;
 
         public Point Location { get; set; } = new Point();
-        /*public List<IEquipment> Equipments { get; set; } = new List<IEquipment>();
-        public List<Space> Spaces { get; set; } = new List<Space>();
-        public List<Level> Levels { get; set; } = new List<Level>();
-        public List<Profile> Profiles { get; set; } = new List<Profile>();
-        public List<InternalCondition> InternalConditions { get; set; } = new List<InternalCondition>();
-        public List<IEquipmentProperties> EquipmentProperties { get; set; } = new List<IEquipmentProperties>();
-        public List<BuildingElementProperties> BuildingElementProperties { get; set; } = new List<BuildingElementProperties>();
-        public List<BuildingElement> BuildingElements { get; set; } = new List<BuildingElement>();*/
 
         /***************************************************/
     }
