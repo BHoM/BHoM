8dda0f4b0bc073c9ae9ebb04f433d0db9e8a47af
Andrew Wakeman <andrew.wakeman@burohappold.com>
2018-09-27 10:45:00 +0100

    Create nuget package build specifications
    Resolves #290

--- BHoM/Properties/AssemblyInfo.cs
+++ BHoM/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BH.oM")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Buildings and Habitats Object Model")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BuroHappold")]
 [assembly: AssemblyProduct("BH.oM")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
