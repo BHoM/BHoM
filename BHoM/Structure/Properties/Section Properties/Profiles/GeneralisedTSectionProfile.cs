1408062ac78c3177ca0d7ea9d3bfb5b930f34973
Isak Naslund <Isak.Naslund@burohappold.com>
2018-11-13 14:48:47 +0100

    Fixes #304 Adding a generalised T section Profile class

---
+++ BHoM/Structure/Properties/Section Properties/Profiles/GeneralisedTSectionProfile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Structure.Properties
+{
+    public class GeneralisedTSectionProfile : BHoMObject, IProfile, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public ShapeType Shape { get; } = ShapeType.Tee;
+
+        public double Height { get; }
+
+        public double WebThickness { get; }
+
+        public double LeftOutstandWidth { get; }
+
+        public double LeftOutstandThickness { get; }
+
+        public double RightOutstandWidth { get; }
+
+        public double RightOutstandThickness { get; }
+
+        public bool MirrorAboutLocalY { get; }
+
+        public ReadOnlyCollection<ICurve> Edges { get; }
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public GeneralisedTSectionProfile(double height, double webThickness, double leftOutstandWidth, double leftOutstandThickness, double rightOutstandWidth, double rightOutstandThickness, bool mirrorAboutLocalY, IEnumerable<ICurve> edges)
+        {
+            Height = height;
+            WebThickness = webThickness;
+            LeftOutstandWidth = leftOutstandWidth;
+            LeftOutstandThickness = leftOutstandThickness;
+            RightOutstandWidth = rightOutstandWidth;
+            RightOutstandThickness = rightOutstandThickness;
+            MirrorAboutLocalY = mirrorAboutLocalY;
+            Edges = new ReadOnlyCollection<ICurve>(edges.ToList());
+        }
+
+        /***************************************************/
+    }
+}                                                   
