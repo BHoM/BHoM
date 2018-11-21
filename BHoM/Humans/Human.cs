d4f1d9ed11a513495213eefcfe27c5c5306da1e2
Arnaud Declercq <arnaud.declercq@burohappold.com>
2018-11-21 17:29:20 +0800

    Adding properties to Human and Pull request

--- BHoM/Humans/Human.cs
+++ BHoM/Humans/Human.cs
@@ -1,4 +1,5 @@
 ﻿using BH.oM.Base;
+using System.Collections.Generic;
 
 namespace BH.oM.Humans
 {
@@ -7,6 +8,11 @@ namespace BH.oM.Humans
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
+
+        public List<IHumanRole> Roles { get; set; } = new List<IHumanRole>();
+
+
+        public string Company { get; set; }  // TODO: Move all those relevant to an Employee : IHumanRole class
 
         public string Office { get; set; }
 
@@ -20,17 +26,51 @@ namespace BH.oM.Humans
 
         public string Phone { get; set; }
 
-        public string YammerLogin { get; set; }
 
-        public string GitHubLogin { get; set; }
+        //public string YammerLogin { get; set; }
+
+        //public string GitHubLogin { get; set; }
+
 
         public Skeleton Skeleton { get; set; }
 
-        public string CodeDevelopmentRole { get; set; }
+        public string CodeDevelopmentRole { get; set; } 
 
         public string BusinessUnit { get; set; }
 
 
         /***************************************************/
     }
+
+    // Todo: Move this to its own file when the BHoM is available for PR 
+    public interface IHumanRole : IObject { }
+
+    /***************************************************/
+
+    public class Employee : IHumanRole
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Company { get; set; }  
+
+        public string Office { get; set; }
+
+        public string Discipline { get; set; }
+
+        public string Team { get; set; }
+
+        public string Grade { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        /***************************************************/
+    }
+
 }
+
+
+
