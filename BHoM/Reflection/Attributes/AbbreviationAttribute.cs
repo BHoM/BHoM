c074c872961b2dab7285a785c5cd575f46a7c19f
Al Fisher <al.fisher@burohappold.com>
2018-11-25 23:23:35 +0000

    Implementing Abbreviation Custom Attribute
    Solves #294

---
+++ BHoM/Reflection/Attributes/AbbreviationAttribute.cs
@@ -0,0 +1,32 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Reflection.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AbbreviationAttribute : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Name { get; private set; } = "";
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public AbbreviationAttribute(string name)
+        {
+            Name = name;
+        }
+
+
+        /***************************************************/
+    }
+}
