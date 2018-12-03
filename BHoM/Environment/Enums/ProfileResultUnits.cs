a57a061b086dc49b2c607c1551e4e8584b893131
fgreenro <fgreenro@BTH-PF16AVCY>
2018-12-03 10:33:45 +0000

    Fix #332 - profile results added

---
+++ BHoM/Environment/Enums/ProfileResultUnits.cs
@@ -0,0 +1,10 @@
+﻿namespace BH.oM.Environment.Results
+{
+    public enum ProfileResultUnits
+    {
+        Undefined,
+        Value, //Single item of data
+        Hourly, //24 hours of hourly by each hour
+        Yearly, //8760 hours
+    }
+}
---
+++ BHoM/Environment/Results/ProfileResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+
+namespace BH.oM.Environment.Results
+{
+    public class ProfileResults : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public ProfileResultUnits ResultUnit { get; set; } = ProfileResultUnits.Undefined;
+        public ProfileResultsCategory ResultCategory { get; set; } = ProfileResultsCategory.Undefined;
+        public ProfileResultType ResultType { get; set; } = ProfileResultType.Undefined;
+        public List<double> Results { get; set; } = new List<double>();
+    }
+}
