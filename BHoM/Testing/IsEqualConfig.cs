940b456a230c7f3aa22350faf24cb8787dc1738e
Isak Naslund <Isak.Naslund@burohappold.com>
2018-12-03 15:14:37 +0100

    Fixes #319
    Adding simple settings object for testing IsEqual int he Testing_Engine

---
+++ BHoM/Testing/IsEqualConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.oM.Testing
+{
+    public class IsEqualConfig : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public double NumericTolerance { get; set; } = 1e-6;
+
+        public bool IgnoreGuid { get; set; } = true;
+
+        public bool IgnoreCustomData { get; set; } = true;
+
+        public List<string> PropertiesToIgnore { get; set; } = new List<string>();
+
+        /***************************************************/
+    }
+}
