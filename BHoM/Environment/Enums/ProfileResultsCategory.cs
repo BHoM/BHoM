7c6a41fa34bdba360f04f61287a6edb2e45cfb66
fgreenro <fgreenro@BTH-PF16AVCY>
2018-12-03 10:33:45 +0000

    Fix #332 - profile results added

---
+++ BHoM/Environment/Enums/ProfileResultsCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace BH.oM.Environment.Results
+{
+    public enum ProfileResultsCategory
+    {
+        Undefined,
+        Building, //Building level results
+        Space, //Space level results
+        BuildingElement, //Element level results
+    }
+}
