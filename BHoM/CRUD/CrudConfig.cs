e06b1703b8e5631e80d1ef16dd43c7993c779576
Arnaud Declercq <arnaud.declercq@burohappold.com>
2018-12-15 18:19:41 +0800

    Provide the definitions necessary to replace the CRUD config dictionary

---
+++ BHoM/CRUD/CrudConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base.CRUD
+{
+    public class CrudConfig
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public PushActionType ActionType { get; set; } = PushActionType.Replace;
+
+        /***************************************************/
+    }
+}
---
+++ BHoM/CRUD/IExecuteParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base.CRUD
+{
+    public interface IExecuteParams
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+
+        /***************************************************/
+    }
+}
