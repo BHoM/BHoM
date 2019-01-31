318207b90fd1f2da9d74b2e7e5aa507a07ea44a2
Kayleigh Houde <Kayleigh.Houde@burohappold.com>
2019-01-31 13:29:17 -0500

    Added Duct and Node Objects

---
+++ Environment_oM/Elements/Duct.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+using BH.oM.Environment.Interface;
+using BH.oM.Environment.Materials;
+using BH.oM.Environment.Properties;
+
+namespace BH.oM.Environment.Elements
+{
+    public class Duct : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public Material Material { get; set; } = new Material();
+        public Node StartNode { get; set; }
+        public Node EndNode { get; set; }
+        public double Height { get; set; } = 0.0;
+        public double Width { get; set; } = 0.0;
+        public double Flow { get; set; } = 0.0;
+
+        /***************************************************/
+    }
+}
