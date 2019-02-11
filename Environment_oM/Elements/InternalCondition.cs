80a435e6dc29137dc2af6c826255883368e8d29b
fgreenro <fgreenro@BTH-PF16AVCY>
2019-02-11 15:09:17 +0000

    Change Gain to Gains in Internal Condition following chat with @tg359

--- Environment_oM/Elements/InternalCondition.cs
+++ Environment_oM/Elements/InternalCondition.cs
@@ -44,7 +44,7 @@ namespace BH.oM.Environment.Elements
 
         public List<SimulationDayType> DayTypes { get; set; } = new List<SimulationDayType>();
 
-        public Gain Gain { get; set; } = new Gain();
+        public List<Gain> Gains { get; set; } = new List<Gain>();
 
         public Thermostat Thermostat { get; set; } = new Thermostat();
         /***************************************************/
