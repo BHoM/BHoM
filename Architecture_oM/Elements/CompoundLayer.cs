3cd5f014143f1c397cefbe4d7ada60f198f2edd9
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2019-02-19 11:00:16 +0000

    Implementation of physical objects

---
+++ Architecture_oM/Elements/CompoundLayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Common.Materials;
+
+namespace BH.oM.Architecture.Elements
+{
+    public class CompoundLayer
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public Material Material { get; set; } = new Material();
+        public double Thickness { get; set; } = double.NaN;
+
+        /***************************************************/
+
+        /*
+        Methods to implement in BHoM_Engine
+
+        //Create
+        
+        public static CompoundLayer CompoundLayer(Material material, double thickness)
+        {
+            //Add checks
+        
+            return new CompoundLayer() 
+            {
+                Material = material,
+                Thickness = thickness 
+            };
+        }
+        
+        */
+    }
+}
---
+++ Architecture_oM/Elements/Floor.cs
@@ -0,0 +1,73 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using BH.oM.Architecture.Properties;
+using BH.oM.Geometry;
+
+namespace BH.oM.Architecture.Elements
+{
+    public class Floor : BHoMObject, IHostObject
+    {
+        public HostedObjectProperties HostedObjectProperties { get; set; } = new HostedObjectProperties();
+        public PolyCurve Profile { get; set; } = new PolyCurve();
+
+        /*
+        Methods to implement in BHoM_Engine
+
+        //Create
+
+        public static Floor Floor(PolyCurve profile, Material material, double thickness)
+        {
+            //Add checks
+            //Check if Profile is closed PolyCurve
+
+            Floor aFloor = new Floor();
+            aFloor.Profile = profile;
+            aFloor.HostedObjectProperties.AddCompoundLayer(material, thickness);
+
+            return aFloor;
+        }
+
+        public static Floor Floor(PolyCurve profile, Material material, double thickness)
+        {
+            //Add checks
+
+            Floor aFloor = new Floor();
+            aFloor.Profile = profile
+            aFloor.HostedObjectProperties.AddCompoundLayer(material, thickness);
+
+            return aFloor
+        }
+
+        public static Floor Floor(PolyCurve profile, HostedObjectProperties hostedObjectProperties)
+        {
+            //Add checks
+
+            Floor aFloor = new Floor()
+            {
+                Profile = profile,
+                HostedObjectProperties = hostedObjectProperties
+            };
+
+            return aFloor
+        }
+
+        public static Wall Wall(ICurve location, double height, HostedObjectProperties hostedObjectProperties)
+        {
+            //Add checks
+
+            //To implement GetProfile method from Location curve and height
+            PolyCurve aProfile = GetProfile(location, height)
+
+            Floor aFloor = new Floor()
+            {
+                Profile = aProfile,
+                HostedObjectProperties = hostedObjectProperties
+            };
+
+            return aFloor
+        }
+
+        */
+    }
+}
