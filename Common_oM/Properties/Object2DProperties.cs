1f935c2554ed71068ab1def52b14b2da13b24789
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2019-02-26 09:42:16 +0000

    Code reorganized according to comments
    IObject2D moded to Common.Interface
    Object2DProperties moved to Common.Properties
    CompoundLayer moved to Common.Properties

--- Common_oM/Properties/Object2DProperties.cs
+++ Common_oM/Properties/Object2DProperties.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 using BH.oM.Base;
-using BH.oM.Architecture.Elements;
 
-namespace BH.oM.Architecture.Properties
+namespace BH.oM.Common.Properties
 {
     public class Object2DProperties : BHoMObject
     {
