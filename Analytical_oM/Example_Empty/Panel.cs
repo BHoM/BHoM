6bd216d9e4e39f437cd8101e9803369899c89847
Isak Naslund <Isak.Naslund@burohappold.com>
2019-04-04 09:44:04 +0200

    Empty interfaces example

---
+++ Analytical_oM/Example_Empty/Panel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Analytical.Elements.Empty;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Analytical.ExampleEmpty
+{
+    public class Panel : BHoMObject, IPanel
+    {
+        public List<Edge> ExternalEdges { get; set; }
+
+        public List<Opening> Openings { get; set; }
+    }
+}
