cde888aa34233e1d40d30bf630f9a5a711532fcb
Isak Naslund <Isak.Naslund@burohappold.com>
2019-10-28 15:03:36 +0100

    Add table class

---
+++ Data_oM/Collections/Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.oM.Data.Collections
+{
+    public class Table : BHoMObject
+    {
+
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        public DataTable Data { get; set; }
+
+        public string MetaData { get; set; }
+
+        /***************************************************/
+
+    }
+
+}
