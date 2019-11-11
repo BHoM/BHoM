2c3c421152bce1956ac85ed39eac87198eecdd26
fgreenro <fgreenro@BTH-PF16AVCY>
2019-11-11 16:06:29 +0000

    #606 initial testing

---
+++ .ci/CloneTest.ps1
@@ -0,0 +1,15 @@
+Param([string]$repo)
+
+# **** Cloning BHoM Test_Toolkit ****
+$repo = "Test_Toolkit"
+
+# **** Defining Paths ****
+$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
+
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
+# **** Restore NuGet ****
+Write-Output ("Restoring NuGet packages for " + $repo)
+& NuGet.exe restore $slnPath
