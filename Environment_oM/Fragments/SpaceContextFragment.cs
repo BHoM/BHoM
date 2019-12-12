5e424a14910400ac80c62b035e6da4615e206491
Maja Lindroth <maja.lindroth@burohappold.com>
2019-12-12 15:24:41 +0000

    #634 Add description to SpaceContextFragment

--- Environment_oM/Fragments/SpaceContextFragment.cs
+++ Environment_oM/Fragments/SpaceContextFragment.cs
@@ -27,13 +27,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BH.oM.Base;
+using System.ComponentModel;
 
 namespace BH.oM.Environment.Fragments
 {
+    [Description("Fragment containing the space context property related to spaces")]
     public class SpaceContextFragment : IBHoMFragment
     {
+        [Description("Defines wheather the space is externally facing")]
         public bool IsExternal { get; set; } = false;
+
+        [Description("Defines the colour of the space")]
         public string Colour { get; set; } = "";
+
+        [Description("A collection of the elements which enclose the space described by their name or unique identifer as a string")]
         public List<string> ConnectedElements { get; set; } = new List<string>();
     }
 }
