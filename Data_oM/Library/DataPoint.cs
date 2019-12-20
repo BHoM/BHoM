2c11a346c6d4ce0998da99f508939c4b75fd414b
rolyh <roly.hudson@gmail.com>
2019-12-20 12:49:02 +0000

    Added DataPoint for testing and discussion

---
+++ Data_oM/Library/DataPoint.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.Base;
+using BH.oM.Data.Library;
+using System;
+using System.Collections.Generic;
+
+namespace BH.oM.Data.Library
+{
+    public class DataPoint : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public Source SourceInformation { get; set; }
+
+        public System.DateTime TimeOfCreation { get; set; }
+
+        public object Value { get; set; }
+
+    }
+}
