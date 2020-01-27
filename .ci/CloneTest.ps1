192dd118366b16e4f4902de3884bc2bf95f5e094
fgreenro <fgreenro@BTH-PF16AVCY>
2020-01-27 15:10:51 +0000

    Allow TestToolkit to build for compliance checks

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -1,5 +1,33 @@
 Param([string]$repo)
 
+# **** Cloning BHoM ****
+$repo = "BHoM"
+
+# **** Defining Paths ****
+$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
+
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
+# **** Restore NuGet ****
+Write-Output ("Restoring NuGet packages for " + $repo)
+& NuGet.exe restore $slnPath
+
+# **** Cloning BHoM_Engine ****
+$repo = "BHoM_Engine"
+
+# **** Defining Paths ****
+$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
+
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
+# **** Restore NuGet ****
+Write-Output ("Restoring NuGet packages for " + $repo)
+& NuGet.exe restore $slnPath
+
 # **** Cloning BHoM Test_Toolkit ****
 $repo = "Test_Toolkit"
 
