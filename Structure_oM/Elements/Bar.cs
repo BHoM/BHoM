b454cbb4a1cbba7577b2cff2ebea111d2c00d620
Isak Naslund <Isak.Naslund@burohappold.com>
2020-01-27 14:03:31 +0100

    Aligning text

--- Structure_oM/Elements/Bar.cs
+++ Structure_oM/Elements/Bar.cs
@@ -46,7 +46,7 @@ namespace BH.oM.Structure.Elements
         public ISectionProperty SectionProperty { get; set; } = null;
 
         [Description("Bar orientation angle in radians \n" +
-                     "For non-vertical members local z is aligned with global z and rotated with the orientation angle around the local x. \n " +
+                     "For non-vertical members the local z is aligned with the global z and rotated with the orientation angle around the local x. \n " +
                      "For vertical members the local y is aligned with the global y and rotated with the orientation angle around the local x. \n" +
                      "A bar is vertical if its projected length to the horizontal plane is less than 0.0001, i.e. a tolerance of 0.1mm on verticality. \n" +
                      "For general structural conventions please see  https://github.com/BHoM/documentation/wiki/BHoM-Structural-Conventions")]
