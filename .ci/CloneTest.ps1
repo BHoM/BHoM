e525b9d17b335f94f2a22a08a822b09a444292fc
fgreenro <fgreenro@BTH-PF16AVCY>
2020-01-27 16:50:44 +0000

    Fix check Installer

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -1,3 +1,5 @@
+Param([string]$repo)
+
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
 # **** Cloning BHoM ****
@@ -5,10 +7,6 @@ $repo = "BHoM"
 
 # **** Defining Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
-
-# **** Cloning Repo ****
-Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
-git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
 
 # **** Restore NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
