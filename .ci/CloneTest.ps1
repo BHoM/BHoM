00c3d9d6653dc9e8305ec3c4de98777396ccf16c
fgreenro <fgreenro@BTH-PF16AVCY>
2020-01-27 16:03:05 +0000

    Update CloneTest.ps1

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -1,4 +1,32 @@
-Param([string]$repo)
+$msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+
+# **** Cloning BHoM ****
+$repo = "BHoM"
+
+# **** Defining Paths ****
+$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
+
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
+# **** Restore NuGet ****
+Write-Output ("Restoring NuGet packages for " + $repo)
+& NuGet.exe restore $slnPath
+
+# **** Cloning BHoM_Engine ****
+$repo = "BHoM_Engine"
+
+# **** Defining Paths ****
+$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
+
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
+# **** Restore NuGet ****
+Write-Output ("Restoring NuGet packages for " + $repo)
+& NuGet.exe restore $slnPath
 
 # **** Cloning BHoM Test_Toolkit ****
 $repo = "Test_Toolkit"
@@ -13,3 +41,9 @@ git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURC
 # **** Restore NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
+
+write-Output ("Building BHoM.sln")
+& $msbuildPath -nologo "$ENV:BUILD_SOURCESDIRECTORY\BHoM\BHoM.sln" /verbosity:minimal
+
+write-Output ("Building BHoM_Engine.sln")
+& $msbuildPath -nologo "$ENV:BUILD_SOURCESDIRECTORY\BHoM_Engine\BHoM_Engine.sln" /verbosity:minimal
