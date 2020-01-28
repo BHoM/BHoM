4af3a86585b92c9e06e7cab8d4cb03ff013e4750
fgreenro <fgreenro@BTH-PF16AVCY>
2020-01-28 16:58:59 +0000

    Update CloneTest.ps1 per discussion

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -2,41 +2,43 @@ Param([string]$repo)
 
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
-# **** Cloning BHoM ****
+# *** Test_Toolkit requires BHoM and BHoM_Engine in order to build correctly for compliance tests, so here we make sure we have BHoM and BHoM_Engine ***
+
+# **** Preparing BHoM ****
 $repo = "BHoM"
 
-# **** Defining Paths ****
+# **** Defining BHoM Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
-# **** Restore NuGet ****
+# **** Restore BHoM NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
 
 # **** Cloning BHoM_Engine ****
 $repo = "BHoM_Engine"
 
-# **** Defining Paths ****
+# **** Defining BHoM_Engine Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
-# **** Cloning Repo ****
+# **** Cloning BHoM_Engine Repo ****
 Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
 git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
 
-# **** Restore NuGet ****
+# **** Restore BHoM_Engine NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
 
-# **** Cloning BHoM Test_Toolkit ****
+# **** Cloning Test_Toolkit ****
 $repo = "Test_Toolkit"
 
-# **** Defining Paths ****
+# **** Defining Test_Toolkit Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
-# **** Cloning Repo ****
+# **** Cloning Test_Toolkit Repo ****
 Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
 git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
 
-# **** Restore NuGet ****
+# **** Restore Test_Toolkit NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
 
