0beb93b0da18fe91c5df222aa4b794efb4e43d53
BHoMBot <BHoMBot@burohappold.com>
2020-01-30 12:22:19 +0000

    Update Copyright

--- Humans_oM/Employee.cs
+++ Humans_oM/Employee.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -53,6 +53,7 @@ namespace BH.oM.Humans
     }
 
 }
+
 
 
 
