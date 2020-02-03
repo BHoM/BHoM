0e2a84c29353269f5d62e6cfd9f315ab0956514c
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-03 14:54:32 +0100

    Adding quantities to all sections

--- Structure_oM/SectionProperties/ISectionProperty.cs
+++ Structure_oM/SectionProperties/ISectionProperty.cs
@@ -23,6 +23,7 @@
 using BH.oM.Base;
 using BH.oM.Structure.MaterialFragments;
 using System.ComponentModel;
+using BH.oM.Quantities.Attributes;
 
 namespace BH.oM.Structure.SectionProperties
 {
@@ -36,60 +37,79 @@ namespace BH.oM.Structure.SectionProperties
         [Description("Material of the section")]
         IMaterialFragment Material { get; set; }
 
+        [Area]
         [Description("Gross Area of the cross section")]
         double Area { get; }
 
+        [Length]
         [Description("Radius of Gyration about the local Y-Axis")]
         double Rgy { get; }
 
+        [Length]
         [Description("Radius of Gyration about the local Z-Axis")]
         double Rgz { get; }
 
+        [TorsionConstant]
         [Description("Torsion Constant")]
         double J { get; }
 
+        [SecondMomentOfArea]
         [Description("Moment of Inertia about the local Y-Axis")]
         double Iy { get; }
 
+        [SecondMomentOfArea]
         [Description("Moment of Inertia about the local Z-Axis")]
         double Iz { get; }
 
+        [WarpingConstant]
         [Description("Warping Constant")]
         double Iw { get; }
 
+        [SectionModulus]
         [Description("Elastic Modulus of the section about the local Y-Axis")]
         double Wely { get; }
 
+        [SectionModulus]
         [Description("Elastic Modulus of the section about the local Z-Axis")]
         double Welz { get; }
 
+        [SectionModulus]
         [Description("Plastic Modulus of the section about the local Y-Axis")]
         double Wply { get; }
 
+        [SectionModulus]
         [Description("Plastic Modulus of the section about the local Z-Axis")]
         double Wplz { get; }
 
+        [Length]
         [Description("Geometric centre of the section in the local Z direction")]
         double CentreZ { get; }
 
+        [Length]
         [Description("Geometric centre of the section in the local Y direction")]
         double CentreY { get; }
 
+        [Length]
         [Description("Z Distance from the centroid of the section to top edge of the section")]
         double Vz { get; }
 
+        [Length]
         [Description("Z Distance from the centroid of the section to bottom edge of the section")]
         double Vpz { get; }
 
+        [Length]
         [Description("Y Distance from the centroid of the section to right edge of the section")]
         double Vy { get; }
 
+        [Length]
         [Description("Y Distance from the centroid of the section to Left edge of the section")]
         double Vpy { get; }
 
+        [Area]
         [Description("Shear Area in the local Y direction")]
         double Asy { get; }
 
+        [Area]
         [Description("Shear Area in the local Z direction")]
         double Asz { get; }
 
