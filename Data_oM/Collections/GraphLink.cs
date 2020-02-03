427e942ddda1f40c0ce1d70e991bfd713d500e74
rolyh <roly.hudson@gmail.com>
2020-02-03 15:08:02 +0000

    Keep weight attribute on GraphLink

--- Data_oM/Collections/GraphLink.cs
+++ Data_oM/Collections/GraphLink.cs
@@ -30,11 +30,9 @@ namespace BH.oM.Data.Collections
         /***************************************************/
 
         public Dictionary<string,object> Attributes { get; set; } = new Dictionary<string, object>();
-
+        public double Weight { get; set; } = 1.0;
         public GraphNode<T> StartNode { get; set; } = new GraphNode<T>();
-
         public GraphNode<T> EndNode { get; set; } = new GraphNode<T>();
-
 
         /***************************************************/
     }
