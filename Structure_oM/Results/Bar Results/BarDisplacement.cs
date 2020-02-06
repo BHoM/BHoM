b96748915566d74ffa608724d9632fa19c3bc25b
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-06 15:13:56 +0100

    Update BarDisplacement.cs

--- Structure_oM/Results/Bar Results/BarDisplacement.cs
+++ Structure_oM/Results/Bar Results/BarDisplacement.cs
@@ -22,26 +22,39 @@
 
 using System;
 using System.ComponentModel;
+using BH.oM.Quantities.Attributes;
 
 namespace BH.oM.Structure.Results
 {
-    [Description("Total displacement of the bar. All results given in Global coordinates")]
+    [Description("Resulting total displacements in Global coordinates along the bar.")]
     public class BarDisplacement : BarResult
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
+        [Length]
+        [Description("Total displacement along the global x-axis")]
         public double UX { get; set; } = 0.0;
 
+        [Length]
+        [Description("Total displacement along the global y-axis")]
         public double UY { get; set; } = 0.0;
 
+        [Length]
+        [Description("Total displacement along the global z-axis")]
         public double UZ { get; set; } = 0.0;
 
+        [Angle]
+        [Description("Total rotation around the global x-axis")]
         public double RX { get; set; } = 0.0;
 
+        [Angle]
+        [Description("Total rotation around the global y-axis")]
         public double RY { get; set; } = 0.0;
 
+        [Angle]
+        [Description("Total rotation around the global z-axis")]
         public double RZ { get; set; } = 0.0;
 
         /***************************************************/
