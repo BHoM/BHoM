4369f96cbd5475b10a247397b8f82bb111475a8a
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-06 17:33:40 +0100

    Update MeshElementResult.cs

--- Structure_oM/Results/Mesh/MeshElementResult.cs
+++ Structure_oM/Results/Mesh/MeshElementResult.cs
@@ -24,29 +24,35 @@ using BH.oM.Common;
 using BH.oM.Geometry;
 using System.ComponentModel;
 using BH.oM.Base;
-using System.Collections.Generic;
 using System;
 
 namespace BH.oM.Structure.Results
 {
+    [Description("Base class for all discrete mesh element results, that is a result for an individual node and/or face. Stores all identifier information and how to sort the results in a collection.")]
     public abstract class MeshElementResult : IResult, IImmutable
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
-        public IComparable ObjectId { get; } = "";
+        [Description("Id of the Mesh that this result belongs to. When extracted from an analysis package, the object id will match the format and value used in that particular package.")]
+        public IComparable ObjectId { get; set; } = "";
 
+        [Description("Id of the Node in the mesh that this result belongs to. Will be empty for smoothing types not relating to Nodes. When extracted from an analysis package, the node id will correspond to the node id in the software and match the format and value used in that particular package.")]
         public IComparable NodeId { get; } = "";
 
+        [Description("Id of the Face that this result belongs to. Will be empty for smoothing types not relating to Faces. When extracted from an analysis package, the face id will correspond to the face id in the software and match the format and value used in that particular package.")]
         public IComparable MeshFaceId { get; } = "";
 
-        public IComparable ResultCase { get; } = "";
+        [Description("Identifier for the load case or load combination that the result belongs to. Is generally name or number of the loadcase, depending on the analysis package.")]
+        public IComparable ResultCase { get; set; } = "";
 
-        public double TimeStep { get; } = 0.0;
+        [Description("Time step for time history results")]
+        public double TimeStep { get; set; } = 0.0;
 
         public MeshResultLayer MeshResultLayer { get; }
 
+        [Description("Position within the element thickness that result is extracted from, normalised to 1. 0 = lower surface, 0.5 = middle, 1 = top surface")]
         public double LayerPosition { get; }
 
         public MeshResultSmoothingType Smoothing { get; }
@@ -84,6 +90,7 @@ namespace BH.oM.Structure.Results
         /**** IComparable Interface                     ****/
         /***************************************************/
 
+        [Description("Controls how this result is sorted in relation to other results. Sorts with the following priority: Type, ObjectId, ResultCase, MeshFaceId, NodeId, TimeStep")]
         public int CompareTo(IResult other)
         {
             MeshElementResult otherRes = other as MeshElementResult;
