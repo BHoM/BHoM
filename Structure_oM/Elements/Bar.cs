ff89de80483a6a8d17f1a0183241bec1b901520d
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-06 16:15:00 +0100

    One more tweak to Bar

--- Structure_oM/Elements/Bar.cs
+++ Structure_oM/Elements/Bar.cs
@@ -20,6 +20,7 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+
 using System.ComponentModel;
 using BH.oM.Base;
 using BH.oM.Geometry;
@@ -50,7 +51,8 @@ namespace BH.oM.Structure.Elements
         public ISectionProperty SectionProperty { get; set; } = null;
 
         [Angle]
-        [Description("For non-vertical members the local z is aligned with the global z and rotated with the orientation angle around the local x. \n" +
+        [Description("Controls the local axis orientation of the bar \n" +
+                     "For non-vertical members the local z is aligned with the global z and rotated with the orientation angle around the local x. \n" +
                      "For vertical members the local y is aligned with the global y and rotated with the orientation angle around the local x. \n"+
                      "A bar is vertical if its projected length to the horizontal plane is less than 0.0001, i.e. a tolerance of 0.1mm on verticality. \n" +
                      "For general structural conventions please see  https://github.com/BHoM/documentation/wiki/BHoM-Structural-Conventions.")]
