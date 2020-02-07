9c67cc3347712ace28a6f432744f4ed93dc434e9
rolyh <roly.hudson@gmail.com>
2020-02-07 13:03:09 +0000

    Add IComparable to DataPoint

--- Data_oM/Library/DataPoint.cs
+++ Data_oM/Library/DataPoint.cs
@@ -5,17 +5,46 @@ using System.Collections.Generic;
 
 namespace BH.oM.Data.Library
 {
-    public class DataPoint : BHoMObject
+    public class DataPoint : BHoMObject, IComparable<DataPoint>
     {
         /***************************************************/
         /**** Properties                                ****/
-        /***************************************************/
-
+        /**************************************************/
         public Source SourceInformation { get; set; }
-
         public System.DateTime TimeOfCreation { get; set; }
-
         public object Value { get; set; }
 
+        /***************************************************/
+        /**** IComparable Interface                     ****/
+        /***************************************************/
+        public int CompareTo(DataPoint other)
+        {
+            if (other == null) return 1;
+            string otherstring = other.Value.ToString();
+            string thisstring = this.Value.ToString();
+            if (other.Value.GetType() == typeof(double) && this.Value.GetType() == typeof(double))
+            {
+                double otherdouble = 0;
+                double thisdouble = 0;
+                if(double.TryParse(otherstring,out otherdouble)&& double.TryParse(thisstring, out thisdouble))
+                {
+                    return thisdouble.CompareTo(otherdouble);
+                }
+            }
+            else if (other.Value.GetType() == typeof(int) && this.Value.GetType() == typeof(int))
+            {
+                int otherint = 0;
+                int thisint = 0;
+                if (int.TryParse(otherstring, out otherint) && int.TryParse(thisstring, out thisint))
+                {
+                    return thisint.CompareTo(otherint);
+                }
+            }
+            else if(other.Value.GetType() == typeof(string) && this.Value.GetType() == typeof(string))
+            {
+                return thisstring.CompareTo(otherstring);
+            }
+            return 1;
+        }
     }
 }
