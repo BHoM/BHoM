3b2d8fa89df770438b595e05d188a26a6efc1ab0
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-07 15:56:17 +0100

    Update GlobalResultRequest.cs

--- Structure_oM/Requests/GlobalResultRequest.cs
+++ Structure_oM/Requests/GlobalResultRequest.cs
@@ -20,24 +20,29 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 
 
 namespace BH.oM.Structure.Requests
 {
+    [Description("Request for extracting global structural results from an adapter")]
     public class GlobalResultRequest : IStructuralResultRequest
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
+        [Description("Defines which type of results that should be extracted")]
         public GlobalResultType ResultType { get; set; } = GlobalResultType.Reactions;
 
+        [Description("Defines which cases and/or combinations that results should be extracted for. Can generally be set to either Loadcase or Loadcombination objects, or identifiers matching the software. If nothing is provided, results for all cases will be assumed.")]
         public List<object> Cases { get; set; } = new List<object>();
 
+        [Description("Defines for which modes results should be extracted. Only applicable for some casetypes. If nothing is provided, results for all modes will be assumed. ")]
         public List<string> Modes { get; set; } = new List<string>();
 
+        [Description("Defines which objects that results should be extracted for. Not used for most global results.")]
         public List<object> ObjectIds { get; set; } = new List<object>();
 
         /***************************************************/
