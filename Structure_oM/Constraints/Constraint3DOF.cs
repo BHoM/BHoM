6f4e378ea7cb2c8e65dda340cf3fe50a07ab786f
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-07 14:48:46 +0100

    Update Constraint3DOF.cs

--- Structure_oM/Constraints/Constraint3DOF.cs
+++ Structure_oM/Constraints/Constraint3DOF.cs
@@ -20,30 +20,38 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using System.ComponentModel;
 using BH.oM.Base;
 using BH.oM.Quantities.Attributes;
 
 namespace BH.oM.Structure.Constraints
 {
+    [Description("Constraint objects with 3 translational degrees of freedom, used for support of 2D analytical objects")]
     public class Constraint3DOF : BHoMObject
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
+        [Description("Defines the translational fixity in the X-direction")]
         public DOFType UX { get; set; }
 
+        [Description("Defines the translational fixity in the Y-direction")]
         public DOFType UY { get; set; }
 
+        [Description("Defines the translational fixity in the Z- or Normal-direction")]
         public DOFType Normal { get; set; }
 
         [ForcePerUnitLength]
+        [Description("Defines the stiffness in x-direction. Will only be active when corresponding degree of freedom has a DOFType with a stiffness dependency, such as Spring.")]
         public double KX { get; set; } = 0;
 
         [ForcePerUnitLength]
+        [Description("Defines the stiffness in y-direction. Will only be active when corresponding degree of freedom has a DOFType with a stiffness dependency, such as Spring.")]
         public double KY { get; set; } = 0;
 
         [ForcePerUnitLength]
+        [Description("Defines the stiffness in z- or Normal-direction. Will only be active when corresponding degree of freedom has a DOFType with a stiffness dependency, such as Spring.")]
         public double KNorm { get; set; } = 0;
 
 
