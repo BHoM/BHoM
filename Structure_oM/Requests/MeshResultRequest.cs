0bc9d154801a4ac9d691f8d4d52a1de280eb2965
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-07 15:56:15 +0100

    Update MeshResultRequest.cs

--- Structure_oM/Requests/MeshResultRequest.cs
+++ Structure_oM/Requests/MeshResultRequest.cs
@@ -20,7 +20,7 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using BH.oM.Structure.Results;
 using BH.oM.Geometry;
@@ -28,26 +28,33 @@ using BH.oM.Geometry;
 
 namespace BH.oM.Structure.Requests
 {
+    [Description("Request for extracting mesh results from an adapter")]
     public class MeshResultRequest : IStructuralResultRequest
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
+        [Description("Request for extracting bar results from an adapter")]
         public MeshResultType ResultType { get; set; } = MeshResultType.Forces;
+
 
         public MeshResultSmoothingType Smoothing { get; set; } = MeshResultSmoothingType.None;
 
         public MeshResultLayer Layer { get; set; } = MeshResultLayer.AbsoluteMaximum;
 
+        [Description("Position within the element thickness that result is extracted from, normalised to 1. I.e. 0 = lower surface, 0.5 = middle, 1 = top surface")]
         public double LayerPosition { get; set; } = 0.0;
 
-        public Basis Orientation { get; set; } = Basis.XY;
+        public Basis Orientation { get; set; } = null;
 
+        [Description("Defines which cases and/or combinations that results should be extracted for. Can generally be set to either Loadcase or Loadcombination objects, or identifiers matching the software. If nothing is provided, results for all cases will be assumed.")]
         public List<object> Cases { get; set; } = new List<object>();
 
+        [Description("Defines for which modes results should be extracted. Only applicable for some casetypes. If nothing is provided, results for all modes will be assumed. ")]
         public List<string> Modes { get; set; } = new List<string>();
 
+        [Description("Defines which meshes that results should be extracted for. Can generally be set to either pulled panel/mesh objects, or identifiers matching the software. If nothing is provided, results for all meshes will be assumed.")]
         public List<object> ObjectIds { get; set; } = new List<object>();
 
         /***************************************************/
