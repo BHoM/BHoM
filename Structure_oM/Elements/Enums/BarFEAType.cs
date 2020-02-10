ca2cb752a0002796b95349801cca6ebbf2594777
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-10 15:41:26 +0100

    tweak to Bar FEAType and some BarLoads

--- Structure_oM/Elements/Enums/BarFEAType.cs
+++ Structure_oM/Elements/Enums/BarFEAType.cs
@@ -29,13 +29,13 @@ namespace BH.oM.Structure.Elements
     [Description("Defines the finite element type of the bar and what forces the bar can accommodate, i.e. if the bar should be treated as a bending element or axial only in analysis packages.")]
     public enum BarFEAType
     {
-        [Description("Full axial, flexural and torsional behaviour. 2 x 6 DOF:s")]
+        [Description("Full axial, flexural and torsional behaviour. 2 x 6 DOFs")]
         Flexural = 0,
-        [Description("Axial only behaviour. 2 x 3 DOF:s. Can be used to model pin-pin elements")]
+        [Description("Axial only. 2 x 3 DOFs. Can be used to model pin-pin elements")]
         Axial,
-        [Description("Axial only behaviour, compression only. 2 x 3 DOF:s")]
+        [Description("Axial compression only. No tension stiffness. 2 x 3 DOFs")]
         CompressionOnly,
-        [Description("Axial only behaviour, tension only. 2 x 3 DOF:s")]
+        [Description("Axial tension only. No compression stiffness. 2 x 3 DOFs")]
         TensionOnly,
     }
 
