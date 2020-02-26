7e121d33325809c6f4ec53e4a82ae17bfca446e2
fgreenro <fgreenro@BTH-PF16AVCY>
2020-02-26 16:32:12 +0000

    Adding a fullstop

--- Structure_oM/Elements/Panel.cs
+++ Structure_oM/Elements/Panel.cs
@@ -30,7 +30,7 @@ using System.ComponentModel;
 namespace BH.oM.Structure.Elements
 {
     [Description("2D element for structural analysis. " +
-                 "\nThe Panel is a planar surface object defined by a list of planar 'Edges' (curves with properties) for both external and internal edges (openings)")]
+                 "\nThe Panel is a planar surface object defined by a list of planar 'Edges' (curves with properties) for both external and internal edges (openings).")]
     public class Panel : Base.BHoMObject, IAreaElement, IElement2D, IPanel<Edge,Opening>
     {
         /***************************************************/
