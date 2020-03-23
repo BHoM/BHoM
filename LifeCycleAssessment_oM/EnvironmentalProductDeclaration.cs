f151e44c05a39b265ad17e0f299656fb153f80bc
Erik Narhi <Erik.Narhi@burohappold.com>
2020-03-23 13:37:22 -0700

    Eliminated redundant GWP properties

--- LifeCycleAssessment_oM/EnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_oM/EnvironmentalProductDeclaration.cs
@@ -36,13 +36,11 @@ namespace BH.oM.LifeCycleAssessment
         public List<string> IndustryStandards { get; set; } = new List<string>();
         public string Id { get; set; } = "";
         public string Density { get; set; } = "";
-        public string GwpPerKG { get; set; } = "";
-        public string GwpPerDeclaredUnit { get; set; } = "";
-        public double BiogenicEmbodiedCarbon { get; set; } = double.NaN;
         public string DeclaredUnit { get; set; } = "";
         public string Description { get; set; } = "";
         public string Scope { get; set; } = "";
         public double GlobalWarmingPotential { get; set; } = double.NaN;
+        public double BiogenicEmbodiedCarbon { get; set; } = double.NaN;
         public double OzoneDepletionPotential { get; set; } = double.NaN;
         public double PhotochemicalOzoneCreationPotential { get; set; } = double.NaN; //needs convert method "smogPotential"
         public double AcidificationPotential { get; set; } = double.NaN;
