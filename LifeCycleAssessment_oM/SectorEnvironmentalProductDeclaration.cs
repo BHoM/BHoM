cf97e2b964b32e47b3cb2205aafc81197dc94be6
Michael Hoehn <michael.hoehn@burohappold.com>
2020-04-15 12:33:20 -0400

    remove declaredUnit property

--- LifeCycleAssessment_oM/SectorEnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_oM/SectorEnvironmentalProductDeclaration.cs
@@ -35,7 +35,6 @@ namespace BH.oM.LifeCycleAssessment
         public virtual List<string> Jurisdiction { get; set; } = new List<string>();
         public virtual string Id { get; set; } = "";
         public virtual double Density { get; set; } = double.NaN;
-        public virtual string DeclaredUnit { get; set; } = "";
         public virtual string Description { get; set; } = "";
         public virtual string Scope { get; set; } = "";
         public virtual double GlobalWarmingPotential { get; set; } = double.NaN;
