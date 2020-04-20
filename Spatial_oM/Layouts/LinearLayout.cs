8089ad150ca41e1a7bb2a5e1a9cecd69780e8bea
Isak Naslund <Isak.Naslund@burohappold.com>
2020-04-20 13:34:42 +0200

    Adding in inital 2D-layout objects

---
+++ Spatial_oM/Layouts/LinearLayout.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+
+namespace BH.oM.Spatial.Layouts
+{
+    [Description("Linear distribution of points along a vector from one side of the perimiter of the host object to the other.")]
+    public class LinearLayout : BHoMObject, ILayout2D, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("Number of points along the axis.")]
+        public int NumberOfPoints { get; }
+
+        [Description("Direction of the axis. Vector should lie in the XY-plane, i.e. have a Z-coordinate equal to 0.")]
+        public Vector Direction { get; }
+
+        [Description("Offset of the linear layout in relation to the centroid of the host object. Vector should lie in the XY-plane, i.e. have a Z-coordinate equal to 0.")]
+        public Vector Offset { get; }
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public LinearLayout(int numberOfPoints, Vector direction, Vector offset)
+        {
+            NumberOfPoints = numberOfPoints;
+            Direction = direction;
+            Offset = offset;
+        }
+
+        /***************************************************/
+    }
+}
