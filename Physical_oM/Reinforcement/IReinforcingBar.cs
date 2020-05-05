246b0a2df375205798ec82296019f76be94f438f
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-05 13:53:03 +0200

    Adding Reinforcement classes to phsyical_oM

---
+++ Physical_oM/Reinforcement/IReinforcingBar.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using BH.oM.Base;
+using BH.oM.Geometry;
+using BH.oM.Physical.Materials;
+using BH.oM.Quantities.Attributes;
+
+namespace BH.oM.Physical.Reinforcement
+{
+    [Description("Base interface for all reinforcement bars.")]
+    public interface IReinforcingBar : IBHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("Centreline of the Reinforcing bar in three-dimensional space. When the BendRadius is sufficient for any kinks in the rebar, this can be defined as a polyline, ommiting any arcs in corners.")]
+        ICurve CentreCurve { get; set; }
+
+        [Length]
+        double Diameter { get; set; }
+
+        Material Material { get; set; }
+
+        [Length]
+        [Description("Bend radius used for any discontinuities in the CentreCurve")]
+        double BendRadius { get; set; }
+
+        /***************************************************/
+    }
+}
