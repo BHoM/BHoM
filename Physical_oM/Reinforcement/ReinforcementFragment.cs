85ef622194447eedfc11d5d808ed9aa98cc964c2
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-05 13:53:03 +0200

    Adding Reinforcement classes to phsyical_oM

---
+++ Physical_oM/Reinforcement/ReinforcementFragment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.oM.Physical.Reinforcement
+{
+    public class ReinforcementFragment : IFragment
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual List<IReinforcingBar> ReinforcingBars { get; set; } = new List<IReinforcingBar>();
+
+        /***************************************************/
+    }
+}
