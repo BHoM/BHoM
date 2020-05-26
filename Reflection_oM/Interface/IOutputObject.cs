8f3b8754c6f3c88e6dd6fd901e9088d846ded212
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-26 18:07:50 +0200

    Adding the IOutputObejct interface

---
+++ Reflection_oM/Interface/IOutputObject.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.ComponentModel;
+
+namespace BH.oM.Reflection.Interface
+{
+    [Description("Interface to be attached to obejcts that are to be automatically exploded when returned in any UI. Objects implementing this interface will not be constructable from the UI.")]
+    public interface IOutputObject
+    {
+    }
+}
