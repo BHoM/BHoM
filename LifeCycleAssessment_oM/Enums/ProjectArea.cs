10fb3513e675dcfe0f80f0df7c1c698ebedf29a4
Michael Hoehn <michael.hoehn@burohappold.com>
2020-06-22 14:26:36 -0400

    Update ProjectArea.cs

--- LifeCycleAssessment_oM/Enums/ProjectArea.cs
+++ LifeCycleAssessment_oM/Enums/ProjectArea.cs
@@ -20,18 +20,23 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using System.ComponentModel;
+
 namespace BH.oM.LifeCycleAssessment
 {
+    [Description("ProjectArea in m2 for Life Cycle Assessment comparison.")]
     public enum ProjectArea
     {
         Undefined,
-        LessThan1000,
-        Between1000and2500,
-        Between2500and5000,
-        Between5000and10000,
-        Between10000and20000,
-        Between20000and50000,
-        Between50000and100000,
-        GreaterThan100000,
+        Under93,
+        Between94and465,
+        Between466and929,
+        Between930and2323,
+        Between2324and4645,
+        Between4646and9290,
+        Between9291and18580,
+        Between18581and46451,
+        Between46452and92903,
+        Over92903,
     }
 }
