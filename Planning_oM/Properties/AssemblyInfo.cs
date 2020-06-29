57861c2fd464ad527b0c670668bf69d62cc9a683
BHoMBot <BHoMBot@burohappold.com>
2020-06-29 17:35:34 +0100

    Update Assembly File Version

--- Planning_oM/Properties/AssemblyInfo.cs
+++ Planning_oM/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
 
