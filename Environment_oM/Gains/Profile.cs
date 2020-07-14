e0859cacd9567022d39cde6eab312bb50a3ebf8a
Kayleigh Houde <Kayleigh.Houde@burohappold.com>
2020-07-14 14:50:26 -0400

    UpdatedProfileDay

--- Environment_oM/Gains/Profile.cs
+++ Environment_oM/Gains/Profile.cs
@@ -43,7 +43,7 @@ namespace BH.oM.Environment.Gains
         public virtual ProfileType ProfileType { get; set; } = ProfileType.Undefined;
 
         [Description("Profile Day depicts the day described in the profile, whether it's a day of the week or a holiday")]
-        public virtual ProfileDay ProfileDay { get; set; } = ProfileDay.Undefined;
+        public virtual List<ProfileDay> ProfileDay { get; set; } = new List<ProfileDay>();
 
         [Description("Hourly Values denotes the hour-by-hour values for a 24-hour period. These values may be represented in temperature (degrees C) (thermostat), fraction (0.9) (lighting gain), or flow (m3/s) (ventilation)")]
         public virtual List<double> HourlyValues { get; set; } = new List<double>();
