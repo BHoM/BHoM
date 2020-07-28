e2487c731d863cfd2530d2c04da85ea3b0793cbc
Michael Hoehn <michael.hoehn@burohappold.com>
2020-07-28 14:43:11 -0400

    Object migration

---
+++ MEP_oM/Elements/Wire.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using BH.oM.Base;
+using BH.oM.Analytical.Elements;
+using BH.oM.MEP.SectionProperties;
+using BH.oM.Dimensional;
+
+
+namespace BH.oM.MEP.Elements
+{
+    [Description("Wire object to work within an MEP systems.")]
+    public class Wire : BHoMObject, IFlow, ILink<Node>, IElement1D, IElementM
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("The point at which the Wire object begins.")]
+        public virtual Node StartNode { get; set; } = null;
+
+        [Description("The point at which the Wire object ends.")]
+        public virtual Node EndNode { get; set; } = null;
+
+        [Description("The amount of current being carried by the wire, the ampacity of the wire.")]
+        public virtual double FlowRate { get; set; } = 0;
+
+        [Description("Section property of the Wire, containing all material as well as profile geometry and dimensions, where applicable.")]
+        public virtual WireSectionProperty SectionProperty { get; set; } = null;
+
+
+        /***************************************************/
+    }
+}
---
+++ MEP_oM/SectionProperties/WireSectionProperty.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using BH.oM.Base;
+using BH.oM.MEP.MaterialFragments;
+
+namespace BH.oM.MEP.SectionProperties
+{
+    public class WireSectionProperty : BHoMObject, IFlowSectionProperty, IImmutable
+    {
+        [Description("The wire material is the primary material that the wire is composed of (copper, aluminium).")]
+        public IMEPMaterial ConductiveMaterial { get; set; }
+
+        [Description("The insulation is the layer of material outside of the conductive material, meant to insulate the internal conditions of the conductive material.")]
+        public IMEPMaterial InsulationMaterial { get; set; }
+
+        [Description("The section profile of the object that will determine its use within a System.")]
+        public virtual SectionProfile SectionProfile { get; set; }
+
+        [Description("This area takes the element's thickness into account to determine the actual area of the 'solid' portion of the ShapeProfile.")]
+        public virtual double ElementSolidArea { get; }
+
+        [Description("The interior area within the element's shapeProfile. This corresponds to the actual open area less any material thickness.")]
+        public virtual double ElementVoidArea { get; }
+
+        [Description("The solid area of the lining within the element. This area takes into account the ElementSolidArea.")]
+        public virtual double LiningSolidArea { get; }
+
+        [Description("The innermost, open area within the Element and its interior lining. This corresponds to the actual open area of the element.")]
+        public virtual double LiningVoidArea { get; }
+
+        [Description("The actual area of the exterior insulation material.")]
+        public virtual double InsulationSolidArea { get; }
+
+        [Description("The interior area within the Insulation. This should correspond to the element's height and width properties.")]
+        public virtual double InsulationVoidArea { get; }
+
+        public WireSectionProperty(SectionProfile wireProfile, double elementSolidArea, double liningSolidArea, double insulationSolidArea, double elementVoidArea, double liningVoidArea, double insulationVoidArea)
+        {
+            SectionProfile = wireProfile;
+            ElementSolidArea = elementSolidArea;
+            ElementVoidArea = elementVoidArea;
+            LiningSolidArea = liningSolidArea;
+            LiningVoidArea = liningVoidArea;
+            InsulationSolidArea = insulationSolidArea;
+            InsulationVoidArea = insulationVoidArea;
+        }
+    }
+}
