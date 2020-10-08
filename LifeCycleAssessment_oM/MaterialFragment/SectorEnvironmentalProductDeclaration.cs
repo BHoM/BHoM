ae4ed3f10d73035956f5ee5ec387d8f0a79f6b84
Michael Hoehn <michael.hoehn@burohappold.com>
2020-10-08 14:46:07 -0400

    Update descriptions

--- LifeCycleAssessment_oM/MaterialFragment/SectorEnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_oM/MaterialFragment/SectorEnvironmentalProductDeclaration.cs
@@ -42,10 +42,10 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Density]
         public virtual double Density { get; set; } = 0;
 
-        [Description("The amount of depletion of non-renewable material resources measured in Sb (Antimony).")]
+        [Description("Depletion of Abiotic Resources refers to non-renewable resources such as minerals, clay, and peat measured in kg Sb (antimony) equivalent.")]
         public virtual double DepletionOfAbioticResources { get; set; } = double.NaN;
 
-        [Description("The amount of depletion of non-renewable, fossil fuel material resources measured in kg/MJ.")]
+        [Description("Depletion of non-renewable Abiotic Resources (fossil fuels) measured in Methyl Jasmonate.")]
         public virtual double DepletionOfAbioticResourcesFossilFuels { get; set; } = double.NaN;
 
         [Description("Brief summary of the EPD from the data source.")]
@@ -54,7 +54,7 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Description of the material's treatment after its useful life.")]
         public virtual string EndOfLifeTreatment { get; set; } = "";
 
-        [Description("The pollution state of aquatic ecosystems in which the over-fertilization of water and soil has turned into an increased growth of biomass measured in kg/PO4e.")]
+        [Description("Eutrophication, measured in kg N equivalents, refers to emissions of nutrients like nitrogen and phosphorus causing overfertilization, leading to overgrowth of biomass that depresses oxygen levels and suffocates ecosystems.")]
         public virtual double EutrophicationPotential { get; set; } = double.NaN;
 
         [Description("Exported Electrical Energy or EEE measured in MJ.")]
@@ -66,7 +66,7 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Use of freshwater resources measured in m^3.")]
         public virtual double FreshWater { get; set; } = double.NaN;
 
-        [Description("How much heat a greenhouse gas traps in the atmosphere up to a specific time horizon, relative to carbon dioxide measured in kg/CO2e.")]
+        [Description("Global Warming Potential, expressed in kgCO2e, refers to the emissions of carbon dioxide, methane and other gases that contribute to the greenhouse effect and global warming.")]
         public virtual double GlobalWarmingPotential { get; set; } = double.NaN;
 
         [Description("Hazardous Waste Disposed or HWD measured in kg.")]
@@ -81,10 +81,10 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Jurisdiction within which the sector based product EPD originates.")]
         public virtual List<string> Jurisdiction { get; set; } = new List<string>();
 
-        [Description("Phase abreviation for the scope of the EPD. For single phase entries, please input the relevant phase of evaluation i.e. A1 or A1-A3. More information on typical LCA phases can be found in the repository wiki.")]
+        [Description("Phase abbreviation for the scope of the EPD. For single phase entries, please input the relevant phase of evaluation i.e. A1 or A1-A3. More information on typical LCA phases can be found in the repository wiki.")]
         public virtual string LifeCycleAssessmentPhase { get; set; } = "Undefined";
 
-        [Description("The period of existence or duration for the product measured in whole years.")]
+        [Description("The period of useful life of the product measured in years.")]
         [Time]
         public virtual int Lifespan { get; set; } = 0;
 
@@ -103,10 +103,10 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Use of Non-renewable Secondary Fuels or NRSF measured in MJ, LHV.")]
         public virtual double NonRenewableSecondaryFuels { get; set; } = double.NaN;
 
-        [Description("The relative amount of degradation to the ozone layer measured in kg/CFC-11e.")]
+        [Description("Ozone Depletion Potential, measured in kg CFC-11 equivalents, refers to emissions which contribute to the depletion of the stratospheric ozone layer.")]
         public virtual double OzoneDepletionPotential { get; set; } = double.NaN;
 
-        [Description("The relative abilities of volatile organic compounds (VOCs) to produce ground level ozone (or Ethene) measured in kg/O3e.")]
+        [Description("Photochemical Ozone Creation Potential, measured in kgO3 equivalents, refers to emissions which contribute to the formation of ground-level smog.")]
         public virtual double PhotochemicalOzoneCreationPotential { get; set; } = double.NaN;
 
         [Description("Plant within which the product was created.")]
@@ -118,40 +118,44 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Amount of post consumer recycled content measured in kg.")]
         public virtual string PostConsumerRecycledContent { get; set; } = "";
 
-        [Description("Use of non-renewable primary energy excluding raw materials or PENRE measured in MJ, LHV.")]
+        [Description("Use of non-renewable primary energy excluding raw materials measured in megajoules.")]
         public virtual double PrimaryEnergyNonRenewableEnergy { get; set; } = double.NaN;
 
-        [Description("Primary Energy Non-renewable Resources or PENRT is measured in MJ, LHV.")]
+        [Description("Primary Energy Non-renewable Resources or PENRT is measured in megajoules.")]
         public virtual double PrimaryEnergyNonRenewableResource { get; set; } = double.NaN;
 
-        [Description("Primary Energy Renewable-Energy or PERE, is specified in MJ and calculated fro mthe lower calorific value of the energy resources deployed.")]
+        [Description("Use of renewable energy primary energy in megajoules.")]
         public virtual double PrimaryEnergyRenewableEnergy { get; set; } = double.NaN;
 
-        [Description("Primary Energy Renewable Total or PERT is the total energy resources required to produce a product.")]
+        [Description("Total use of renewable energy resources required to produce a product, measured in megajoules.")]
         public virtual double PrimaryEnergyRenewableTotal { get; set; } = double.NaN;
 
-        [Description("Primary Energy Resources used as Raw Materials or PERM measured in MJ.")]
+        [Description("Primary Energy Resources used as raw materials measured in megajoules.")]
         public virtual double PrimaryEnergyResourcesRawMaterials { get; set; } = double.NaN;
 
         [Description("Publisher responsible for recording and presenting all sector EPD information.")]
         public virtual List<string> Publisher { get; set; } = new List<string>();
 
-        [Description("Radioactive Waste Disposed or RWD measured in kg.")]
+        [Description("Radioactive Waste Disposed measured in kg.")]
         public virtual double RadioActiveWasteDisposed { get; set; } = double.NaN;
 
         [Description("Year in which the EPD was created.")]
         public virtual int ReferenceYear { get; set; } = 0;
 
-        [Description("Use of Renewable Secondary Fuels or RSF measured in MJ, LHV.")]
+        [Description("Use of Renewable Secondary fuels measured in megajoules.")]
         public virtual double RenewableSecondaryFuels { get; set; } = double.NaN;
 
-        [Description("List the construction scope of the product. Examples include Structures, Foundations, or Enclosures.")]
+        [Description("List the scope of the product application. Scopes used within the LCA_Toolkit are Structures, Foundations, Enclosures, MEP, and TenantImprovement.")]
         public virtual string Scope { get; set; } = "";
 
-        [Description("Use of Secondary Material or SM measured in MJ, LHV.")]
+        [Description("Use of secondary materials measured in kg.")]
         public virtual double SecondaryMaterial { get; set; } = double.NaN;
 
-        [Description("Key metric for evaluation methods to function. This property determines how the material is to be evaluated, based on Mass, Volume, Area, Item, or Length. Note that any EPD that does not contain this parameter will not be evaluated.")]
+        [Description("Note that any EPD that does not contain this parameter will not be evaluated." +
+            "This metric is based on the declared unit of the reference EPD, i.e. a declared unit of kg refers to QuantityType of mass, a declared unit of m3 refers to a QuantityType of volume, etc. " +
+            "All data should be normalized to metric declared units before integration in the BHoM. " +
+            "The quantity type is a key metric for evaluation methods to function. " +
+            "This property determines how the material is to be evaluated, based on Mass, Volume, Area, Item, or Length. ")]
         public virtual QuantityType QuantityType { get; set; } = QuantityType.Undefined;
         /***************************************************/
     }
