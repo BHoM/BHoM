669fff75da8517fc1bcee1e4008533b58637ebe1
Michael Hoehn <michael.hoehn@burohappold.com>
2020-10-09 15:34:08 -0400

    Edit Properties
    Remove Scope + Add QuantityTypeValue

--- LifeCycleAssessment_oM/MaterialFragment/SectorEnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_oM/MaterialFragment/SectorEnvironmentalProductDeclaration.cs
@@ -144,9 +144,6 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
         [Description("Use of Renewable Secondary fuels measured in megajoules.")]
         public virtual double RenewableSecondaryFuels { get; set; } = double.NaN;
 
-        [Description("List the scope of the product application. Scopes used within the LCA_Toolkit are Structures, Foundations, Enclosures, MEP, and TenantImprovement.")]
-        public virtual string Scope { get; set; } = "";
-
         [Description("Use of secondary materials measured in kg.")]
         public virtual double SecondaryMaterial { get; set; } = double.NaN;
 
@@ -156,6 +153,10 @@ namespace BH.oM.LifeCycleAssessment.MaterialFragments
             "The quantity type is a key metric for evaluation methods to function. " +
             "This property determines how the material is to be evaluated, based on Mass, Volume, Area, Item, or Length. ")]
         public virtual QuantityType QuantityType { get; set; } = QuantityType.Undefined;
+
+        [Description("The number of units in reference to quantity type. Example, 1000 kg per unit quantityType of Mass.")]
+        public virtual double QuantityTypeValue { get; set; } = 1;
+
         /***************************************************/
     }
 }
