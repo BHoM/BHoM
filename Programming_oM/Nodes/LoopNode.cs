5033cf8372f549f6e8197be3cba768889b989cea
adecler <arnaud.declercq@burohappold.com>
2020-10-15 15:55:33 +0800

    Remove dependency to GetShallowClone

--- Programming_oM/Nodes/LoopNode.cs
+++ Programming_oM/Nodes/LoopNode.cs
@@ -56,35 +56,19 @@ namespace BH.oM.Programming
         /**** Constructors                              ****/
         /***************************************************/
 
-        [Description("Returns a LoopNode to be used in a BHoM Syntax Tree.")]
-        [Input("content", "List of syntax nodes to include in the loop.")]
-        [Input("description", "Description of what this loop is doing.")]
-        public LoopNode(List<INode> content, string description = "")
+        public LoopNode(List<INode> internalNodes, List<ReceiverParam> inputs, List<DataParam> outputs, Guid bhomGuid, string description = "")
         {
-            List<ReceiverParam> receivers = content.SelectMany(n => n.Inputs.Where(x => x.SourceId != Guid.Empty)).ToList();
-            List<DataParam> emiters = content.SelectMany(n => n.Outputs.Where(x => x.TargetIds.Count > 0)).ToList();
-
-            List<Guid> receiverIds = receivers.Select(x => x.BHoM_Guid).ToList();
-            List<Guid> emiterIds = emiters.Select(x => x.BHoM_Guid).ToList();
-
-            Outputs = emiters.Where(x => x.TargetIds.Intersect(receiverIds).Count() == 0)
-                                        .Select(x =>
-                                        {
-                                            DataParam copy = x.GetShallowClone() as DataParam;
-                                            copy.ParentId = this.BHoM_Guid;
-                                            return copy;
-                                        }).ToList();
-
-            Inputs = receivers.Where(x => !emiterIds.Contains(x.SourceId))
-                                        .Select(x =>
-                                        {
-                                            ReceiverParam copy = x.GetShallowClone() as ReceiverParam;
-                                            copy.ParentId = this.BHoM_Guid;
-                                            return copy;
-                                        }).ToList();
-
-            InternalNodes = content;
+            InternalNodes = internalNodes;
+            Inputs = inputs;
+            Outputs = outputs;
             Description = description;
+            BHoM_Guid = bhomGuid;
+
+            foreach (ReceiverParam input in inputs)
+                input.ParentId = BHoM_Guid;
+
+            foreach (DataParam output in outputs)
+                output.ParentId = BHoM_Guid;
         }
 
         /***************************************************/
