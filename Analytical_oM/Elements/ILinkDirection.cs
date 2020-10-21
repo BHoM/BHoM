2cb70120110936fe6d2456d17edebcbfce45df7d
rolyh <roly.hudson@gmail.com>
2020-10-21 15:47:00 +0100

    Add supporting objects for routing and ILink converts

---
+++ Analytical_oM/Elements/ILinkDirection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Elements
+{
+    public enum ILinkDirection
+    {
+        Forwards = 0,
+        Backwards = 1,
+        Both = 2
+    }
+}
---
+++ Analytical_oM/Fragments/InputOutputFragment.cs
@@ -0,0 +1,20 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Fragments
+{
+    public class InputOutputFragment : IDependencyFragment
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        public virtual Guid Input { get; set; }
+
+        public virtual Guid Output { get; set; }
+        /***************************************************/
+    }
+}
