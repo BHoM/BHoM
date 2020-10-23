87d0c2203aa6659f3a24aca80c706744be6ce3df
rolyh <roly.hudson@gmail.com>
2020-10-23 11:47:26 +0100

    Reorganise graph elements

---
+++ Analytical_oM/Elements/Graph/Graph.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Elements
+{
+    public class Graph : BHoMObject
+    {
+        public virtual Dictionary<Guid, IBHoMObject> Entities { get; set; } = new Dictionary<Guid, IBHoMObject>();
+
+        public virtual List<IRelation> Relations { get; set; } = new List<IRelation>();
+
+    }
+}
