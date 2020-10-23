28170aac19e81129f8be9fb4df7309644f58bc43
rolyh <roly.hudson@gmail.com>
2020-10-23 10:33:32 +0100

    organise graph elements

---
+++ Analytical_oM/Elements/GraphRelations/ColumnGridValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Elements
+{
+    public class ColumnGridValidator : IValidator
+    {
+        public virtual bool Valid { get; set; } = false;
+    }
+}
