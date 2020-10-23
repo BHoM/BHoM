2abf87a1b65d7b44281d6074599a21498e616914
rolyh <roly.hudson@gmail.com>
2020-10-23 10:33:32 +0100

    organise graph elements

---
+++ Analytical_oM/Elements/GraphRelations/ValidationRelation.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Elements
+{
+    public class ValidationRelation : Relation, IRelation
+    {
+
+
+    }
+
+
+}
