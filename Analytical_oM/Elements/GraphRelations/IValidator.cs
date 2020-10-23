281be60acf37ac2111c7904e2f97d53770a98bf5
rolyh <roly.hudson@gmail.com>
2020-10-23 10:33:32 +0100

    organise graph elements

---
+++ Analytical_oM/Elements/GraphRelations/IValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Elements
+{
+    public interface IValidator
+    {
+        bool Valid { get; set; }
+    }
+}
