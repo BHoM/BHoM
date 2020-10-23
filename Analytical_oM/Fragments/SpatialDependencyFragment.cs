3e8e22daa5ecde6451c9a8683b29ed475938b840
rolyh <roly.hudson@gmail.com>
2020-10-23 15:59:59 +0100

    Add spatial relation and update path result

---
+++ Analytical_oM/Fragments/SpatialDependencyFragment.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.Base;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Fragments
+{
+    public class SpatialDependencyFragment : DependencyFragment, IDependencyFragment
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        public virtual ICurve Curve { get; set; }
+
+        /***************************************************/
+
+    }
+}
