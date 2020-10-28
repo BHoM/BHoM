6f0c55a97dc792b5061420c5c454f16cc2f0cde7
rolyh <roly.hudson@gmail.com>
2020-10-28 15:10:33 +0000

    Add object level descriptions.

--- Analytical_oM/Fragments/DependencyFragment.cs
+++ Analytical_oM/Fragments/DependencyFragment.cs
@@ -23,12 +23,14 @@
 using BH.oM.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BH.oM.Analytical.Fragments
 {
+    [Description("Base class for all dependency fragments.")]
     public class DependencyFragment : IDependencyFragment
     {
         /***************************************************/
