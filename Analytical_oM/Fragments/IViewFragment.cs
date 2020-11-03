169cc2efea23d960b23b0a73436cb5a7b9038dd7
rolyh <roly.hudson@gmail.com>
2020-11-03 08:51:25 +0000

    Add IViewFragment and implement it on ProcessViewFragment

---
+++ Analytical_oM/Fragments/IViewFragment.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Base;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Fragments
+{
+    [Description("Interface common to all ViewFragments.")]
+    public interface IViewFragment : IFragment
+    {
+        Point Position { get; set; }
+    }
+}
