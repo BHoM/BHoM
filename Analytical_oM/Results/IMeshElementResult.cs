c9a7903419cbb562f0cb3d51022b9ce003e398a2
Isak Naslund <Isak.Naslund@burohappold.com>
2020-11-09 10:56:06 +0100

    Updating to ID in descriptions

--- Analytical_oM/Results/IMeshElementResult.cs
+++ Analytical_oM/Results/IMeshElementResult.cs
@@ -32,10 +32,10 @@ namespace BH.oM.Analytical.Results
     [Description("Base interface for mesh element results. This is the result for a single discrete node or face of the mesh the result aligns with.")]
     public interface IMeshElementResult : IResult
     {
-        [Description("Id of the Node in the mesh that this result belongs to.")]
+        [Description("ID of the Node in the mesh that this result belongs to.")]
         IComparable NodeId { get; }
 
-        [Description("Id of the MeshFace that this result belongs to.")]
+        [Description("ID of the MeshFace that this result belongs to.")]
         IComparable MeshFaceId { get; }
     }
 }
