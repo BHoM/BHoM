59e520f0739129101e0faeb28213d5073fd58e00
Peter Nugent <Peter.Nugent@burohappold.com>
2020-11-10 20:21:55 +0000

    Change AreaTemperatureLoad to AreaUnfiromTemperatureLoad and add AreaDifferentialTemperatureLoad

--- Structure_oM/Loads/AreaDifferentialTemperatureLoad.cs
+++ Structure_oM/Loads/AreaDifferentialTemperatureLoad.cs
@@ -24,34 +24,33 @@ using BH.oM.Structure.Elements;
 using System.ComponentModel;
 using BH.oM.Quantities.Attributes;
 using BH.oM.Base;
-using System.Collections.ObjectModel;
-using System.Collections.Generic;
 using BH.oM.Geometry;
+using System.Collections.Generic;
 
 namespace BH.oM.Structure.Loads
 {
-    [Description("Differential temperature load for Bars.")]
-    public class BarDifferentialTemperatureLoad : BHoMObject, IElementLoad<Bar>
+    [Description("Differential temperature load for area elements such as Panels and FEMeshes.")]
+    public class AreaDifferentialTemperatureLoad : BHoMObject, IElementLoad<IAreaElement>
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
         [Temperature]
-        [Description("Differential temperature profile of the Bar expressed as a Dictionary of the parametric position from the top of the profile and the temperature at each location.")]
+        [Description("Differential temperature profile of the area element expressed as a Dictionary of the parametric position from the top of the surface and the temperature at each location.")]
         public virtual Dictionary<double,double> TemperatureProfile { get; set; }
 
-        [Description("The direction of the temperature variation, relative to the local axis of the profile. For most analysis packages this is limited to local y or local z.")]
+        [Description("The direction of the temperature variation, relative to the local axis of the surface property. For most analysis packages this is limited to local y or local z.")]
         public virtual Vector LocalDirection { get; set; }
 
         [Description("The Loadcase in which the load is applied.")]
         public virtual Loadcase Loadcase { get; set; }
 
-        [Description("The group of Bars that the load should be applied to. For most analysis packages the objects added here need to be pulled from the analysis package before being assigned to the load.")]
-        public virtual BHoMGroup<Bar> Objects { get; set; } = new BHoMGroup<Bar>();
+        [Description("The group of IAreaElements that the load should be applied to. For most analysis packages the objects added here need to be pulled from the analysis package before being assigned to the load.")]
+        public virtual BHoMGroup<IAreaElement> Objects { get; set; } = new BHoMGroup<IAreaElement>();
 
         [Description("Defines whether the load is applied in local or global coordinates.")]
-        public virtual LoadAxis Axis { get; set; } = LoadAxis.Local;
+        public virtual LoadAxis Axis { get; set; } = LoadAxis.Global;
 
         [Description("If true the load is projected to the element. This means that the load will be reduced when its direction is at an angle to the element.")]
         public virtual bool Projected { get; set; } = false;
