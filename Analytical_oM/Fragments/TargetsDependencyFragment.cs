d230fca9c6e3b2bcda1d43ef7625639a27d909fa
rolyh <roly.hudson@gmail.com>
2020-11-10 11:21:53 +0000

    Adjust dependency frags for use clarity. Add relations to reuslt of shortest path.

---
+++ Analytical_oM/Fragments/TargetsDependencyFragment.cs
@@ -0,0 +1,25 @@
+﻿using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Analytical.Fragments
+{
+    [Description("Dependency fragment used to define one or more Relations that terminate at the specified targets and use the IBHoMObject owning this fragment as the source.")]
+    public class TargetsDependencyFragment : IDependencyFragment
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        [Description("Collection of Guids of entities that are targets for Relations where the IBHoMObject owning this fragment is the source.")]
+        public virtual List<Guid> Targets { get; set; }
+
+        [Description("Collection of ICurves used to represent the Relations.")]
+        public virtual List<ICurve> Curves { get; set; }
+
+        /***************************************************/
+    }
+}
