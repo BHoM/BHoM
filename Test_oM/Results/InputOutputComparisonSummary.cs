27a301383576fd2b60c2afdbe90b7b74a04635cd
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-11-11 12:44:38 +0000

    Add Test_oM to BHoM #1076

---
+++ Test_oM/Results/InputOutputComparisonSummary.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Analytical.Results;
+using System.ComponentModel;
+
+namespace BH.oM.Test.Results
+{
+    public class InputOutputComparisonSummary : IResult, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("The hashes of the item being evaluated, comma separated")]
+        public virtual IComparable ObjectId { get; }
+
+        [Description("The sets the result belongs to, comma separated.")]
+        public virtual IComparable ResultCase { get; }
+
+        [Description("The type of the object being evaluated.")]
+        public virtual Type ObjectType { get; }
+
+        [Description("Not in use")]
+        public virtual double TimeStep { get; }
+
+        public virtual InputOutputComparisonType ResultType { get; }
+
+        [Description("Number of times a particular difference has been registred.")]
+        public virtual int Occurrences { get;  }
+
+        [Description("The name of the evaluated property. Only applicable for ResultType 'Difference'.")]
+        public virtual string PropertyId { get; }
+
+        [Description("Average difference between the input and returned item. Only applicable for Results of type 'Difference' for numerical values.")]
+        public virtual double AverageDifference { get;  }
+
+        [Description("Maximum difference between the input and returned item. Only applicable for Results of type 'Difference' for numerical values.")]
+        public virtual double MaximumDifference { get;  }
+
+        [Description("Value of the input item for the maximum difference. Only applicable for Results of type 'Difference' for numerical values.")]
+        public virtual double MaximumDifferenceInputItem { get;  }
+
+        [Description("Value of the returned item for the maximum difference. Only applicable for Results of type 'Difference' for numerical values.")]
+        public virtual double MaximumDifferenceReturnedItem { get;  }
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public InputOutputComparisonSummary(IComparable objectId, IComparable resultCase, Type objectType, InputOutputComparisonType resultType, string propertyId, double timeStep, int occurrences, double averageDifference, double maximumDifference, double maximumDifferenceInputItem, double maximumDifferenceReturnedItem)
+        {
+            ObjectId = objectId;
+            ResultCase = resultCase;
+            PropertyId = propertyId;
+            TimeStep = timeStep;
+            Occurrences = occurrences;
+            ObjectType = objectType;
+            ResultType = resultType;
+            AverageDifference = averageDifference;
+            MaximumDifference = maximumDifference;
+            MaximumDifferenceInputItem = maximumDifferenceInputItem;
+            MaximumDifferenceReturnedItem = maximumDifferenceReturnedItem;
+        }
+
+        /***************************************************/
+        /**** IComparable Interface                     ****/
+        /***************************************************/
+
+        public int CompareTo(IResult other)
+        {
+            InputOutputComparisonSummary otherRes = other as InputOutputComparisonSummary;
+
+            if (otherRes == null)
+                return this.GetType().Name.CompareTo(other.GetType().Name);
+
+            int objectType = this.ObjectType.ToString().CompareTo(otherRes.ObjectType.ToString());
+
+            if (objectType == 0)
+            {
+
+                int resType = this.ResultType.CompareTo(otherRes.ResultType);
+
+                if (resType == 0)
+                {
+
+                    int p = this.PropertyId.CompareTo(otherRes.PropertyId);
+                    if (p == 0)
+                    {
+                        int l = this.ResultCase.CompareTo(otherRes.ResultCase);
+                        if (l == 0)
+                        {
+                            return this.ObjectId.CompareTo(otherRes.ObjectId);
+
+                        }
+                        else
+                        {
+                            return l;
+                        }
+                    }
+                    else
+                    {
+                        return p;
+                    }
+                }
+                else
+                {
+                    return resType;
+                }
+            }
+            else
+            {
+                return objectType;
+            }
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Test_oM/UnitTests/TestData.cs
@@ -0,0 +1,57 @@
+﻿/*	
+ * This file is part of the Buildings and Habitats object Model (BHoM)	
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.	
+ *	
+ * Each contributor holds copyright over their respective contributions.	
+ * The project versioning (Git) records all such contribution source information.	
+ *                                           	
+ *                                                                              	
+ * The BHoM is free software: you can redistribute it and/or modify         	
+ * it under the terms of the GNU Lesser General Public License as published by  	
+ * the Free Software Foundation, either version 3.0 of the License, or          	
+ * (at your option) any later version.                                          	
+ *                                                                              	
+ * The BHoM is distributed in the hope that it will be useful,              	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               	
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 	
+ * GNU Lesser General Public License for more details.                          	
+ *                                                                            	
+ * You should have received a copy of the GNU Lesser General Public License     	
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      	
+ */
+
+using BH.oM.Base;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.oM.Test.UnitTests
+{
+    [Description("Defines input data and expected output data for one run of a particular unit test.")]
+    public class TestData : BHoMObject, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("Inputs to the method. Inputs are matched based on index in the list, i.e. index 0 in this list will be used as the first parameter of the method tested.")]
+        public virtual ReadOnlyCollection<object> Inputs { get; set; }
+
+        [Description("The expected outputs from the method for the provided inputs.\n" +
+                     "The length of this collection will only be greater than one if the method being tested returns an Output<T1,..,Tn>, for which the indices will match the ones of the Output<>.\n" +
+                     "This means methods returning a collection will be seen as returning one object, where the object is the collection.")]
+        public virtual ReadOnlyCollection<object> Outputs { get; set; } = null;
+
+
+        /***************************************************/
+
+        public TestData(IEnumerable<object> inputs, IEnumerable<object> outputs)
+        {
+            Inputs = new ReadOnlyCollection<object>(inputs == null ? new List<object>() : inputs.ToList());
+            Outputs = new ReadOnlyCollection<object>(outputs == null ? new List<object>() : outputs.ToList());
+        }
+
+        /***************************************************/
+    }
+}
