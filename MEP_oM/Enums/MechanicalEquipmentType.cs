e57209ccf96f136959513fc785d5ca3cefb882a6
Kayleigh Houde <Kayleigh.Houde@burohappold.com>
2020-11-17 13:38:00 -0500

    MEP_oM: Addition of Generic Mechanical Equipment object

---
+++ MEP_oM/Enums/MechanicalEquipmentType.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace BH.oM.MEP.Enums
+{
+    /***************************************************/
+
+    [Description("A type of mechanical equipment (air handling unit, boiler, chiller, fan coil unit.)")]
+    public enum MechanicalEquipmentType
+    {
+        Undefined,
+        AirHandlingUnit,
+        AirSeparator,
+        AirSourceHeatPump,
+        Boiler,
+        CabinetUnitHeater,
+        Chiller,
+        CoolingTower,
+        ExpansionTankVessel,
+        Fan,
+        FanCoilUnit,
+        HeatExchanger,
+        HeatingAndVentilationUnit,
+        Pump,
+        UnitHeater,
+        VariableAirVolumeBox,
+        WaterSourceHeatPump,
+        WaterStorageTank,
+    }
+
+    /***************************************************/
+}
