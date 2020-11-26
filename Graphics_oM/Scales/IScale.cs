b457037d4f8cfcd8414f16e7dd73c08c5cce3d6b
rolyh <roly.hudson@gmail.com>
2020-11-26 19:38:07 +0000

    Setting up scaling linear and ordinal

---
+++ Graphics_oM/Scales/IScale.cs
@@ -0,0 +1,15 @@
+﻿using BH.oM.Base;
+using BH.oM.Data.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Graphics.Scales
+{
+    public interface IScale : IObject
+    {
+        string Name { get; set; }
+    }
+}
---
+++ Graphics_oM/Views/DependencyChart.cs
@@ -0,0 +1,26 @@
+﻿using BH.oM.Base;
+using BH.oM.Graphics.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Graphics.Views
+{
+    public class DependencyChart : BHoMObject, IView
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        public virtual Boxes Boxes { get; set; } = null;
+
+        public virtual Links Links { get; set; } = null;
+
+        public virtual ViewConfig ViewConfig { get; set; } = new ViewConfig();
+
+        public virtual Title Title { get; set; } = new Title();
+
+        /***************************************************/
+    }
+}
