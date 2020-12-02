c91fb8c18050bb3d45240c6345ff5670ad3b052c
Kayleigh Houde <Kayleigh.Houde@burohappold.com>
2020-12-02 13:49:40 -0500

    AdditionofMissingMEPObjects

---
+++ MEP_oM/Equipment/ElectricalEquipment.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.MEP.Equipment.Parts;
+using BH.oM.MEP.Enums;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.MEP.Equipment
+{
+    public class ElectricalEquipment : BHoMObject, IEquipment
+    {
+        [Description("A type which describes the electrical equipment, more specifically whether it's switchgear, panelboard, transformer, etc.")]
+        public virtual ElectricalEquipmentType ElectricalEquipmentType { get; set; } = ElectricalEquipmentType.Undefined;
+
+        [Description("A collection of the parts (Electrical Connector) that make up the Electrical Equipment")]
+        public virtual List<IPart> Parts { get; set; } = new List<IPart>();
+
+        [Description("The point in space for the location of the electrical equipment.")]
+        public virtual Point Position { get; set; } = new Point();
+    }
+}
---
+++ MEP_oM/Fixtures/FireAlarmDevice.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.ComponentModel;
+using BH.oM.Base;
+using BH.oM.MEP.System.SectionProperties;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using BH.oM.Quantities.Attributes;
+using BH.oM.Analytical.Elements;
+using BH.oM.MEP.Enums;
+
+
+namespace BH.oM.MEP.Fixtures
+{
+    [Description("A device used to warn building occupants of emergencies (smoke, fire, carbon monoxide) by means of audio or visual appliances (smoke detectors, speaker strobes, heat detectors.)")]
+    public class FireAlarmDevice : BHoMObject, IElementM, INode
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        [Description("A type which describes the fire alarm device more specifically whether it's a smoke detector, carbon monoxide detector or speaker strobe.")]
+        public virtual FireAlarmDeviceType FireAlarmDeviceType { get; set; } = FireAlarmDeviceType.Undefined;
+
+        [Description("The point in space for the location of the Electrical Fixture.")]
+        public virtual Point Position { get; set; } = new Point();
+        /***************************************************/
+    }
+}
