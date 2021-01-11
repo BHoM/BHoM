aa85feaaafd58320221b041e451d20c3e0db9c17
Valerie Green <valerie.green@burohappold.com>
2021-01-11 11:29:41 -0800

    PR Comment Update (pt. 2)
    Fragment name, property description, and copyright year are updated

--- Facade_oM/Fragments/ConstructionOffset.cs
+++ Facade_oM/Fragments/ConstructionOffset.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -39,7 +39,7 @@ namespace BH.oM.Facade.Fragments
         /**** Properties                                ****/
         /***************************************************/
 
-        [Description("Offset distance from centerline of wall type or glazing section from centerline of facade section")]
+        [Description("Offset distance from centerline of wall or glazing section from centerline of facade section")]
         public virtual double OffsetDistance { get; set; } = 0;
 
         /***************************************************/
