a48289af4b9c535fa82682006a54972897fa1843
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-01-27 16:16:05 +0000

    Added first interfaces

---
+++ BHoM/Interface/IRepresentation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base
+{
+    [Description("Identifies an object that is a Representation of another object.")]
+    public interface IRepresentation
+    {
+    }
+}
---
+++ Geometry_oM/Interface/IGeometricalRepresentation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BH.oM.Base;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Geometry
+{
+    [Description("A geometrical object that is the representation of another object.")]
+    interface IGeometricalRepresentation : IGeometry
+    {
+        [Description("A geometry (or many geometry objects collected into a single `CompositeGeometry` object) that is the representation of another object.")]
+        IGeometry Geometry { get; set; }
+        [Description("Colour information (System.Color, or a Gradient, or anything interpretable as colour) applicable to the Geometry.")]
+        object ColourInfo { get; set; }
+    }
+}
