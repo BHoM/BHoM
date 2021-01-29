70e3d2ceea3b0bcdc1f54d9ae6f6f4062cdf53e3
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-01-29 10:44:51 +0000

    Added description

--- Geometry_oM/Misc/GeometricalRepresentation.cs
+++ Geometry_oM/Misc/GeometricalRepresentation.cs
@@ -12,7 +12,9 @@ namespace BH.oM.Geometry
     [Description("A geometrical object that is the representation of another object.")]
     public class GeometricalRepresentation : IGeometry
     {
-        [Description("A geometry (or many geometry objects collected into a single `CompositeGeometry` object) that is the representation of another object.")]
+        [Description("A geometry (or many geometry objects collected into a single `CompositeGeometry` object) that is the representation of another object." +
+            "Examples: for a Point, this could be a Sphere. For a Vector, this could be a CompositeGeometry containing multiple lines for arrow head and stem." +
+            "For a Beam, this could be an extrusion of the cross section along its centreline.")]
         public virtual IGeometry Geometry { get; set; }
         [Description("Colour information (System.Color, or a Gradient, or anything interpretable as colour) applicable to the Geometry.")]
         public virtual object ColourInfo { get; set; }
