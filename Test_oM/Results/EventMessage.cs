ae8c728a8756858431831c63b95536c10dea29e1
Isak Naslund <Isak.Naslund@burohappold.com>
2021-01-29 14:52:20 +0100

    Adding EventMessage

---
+++ Test_oM/Results/EventMessage.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Test;
+using System.ComponentModel;
+
+namespace BH.oM.Test.Results
+{
+    [Description("Class to store an event raised during the execution on a TestResult.")]
+    public class EventMessage : ITestInformation
+    {
+        [Description("Message raised during the execution.")]
+        public virtual string Message { get; set; }
+
+        [Description("Severity of the message.")]
+        public virtual TestStatus Status { get; set; }
+
+        [Description("Provides the UTC time of when the event was raised.")]
+        public virtual DateTime UTCTime { get; set; } = DateTime.UtcNow;
+    }
+}
