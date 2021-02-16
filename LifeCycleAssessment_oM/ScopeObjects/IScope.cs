6f943de2ec9a9092cb567f6e7f70456b437dd542
Michael Hoehn <michael.hoehn@burohappold.com>
2021-02-16 14:10:05 -0500

    Remove IFragment from IScope

--- LifeCycleAssessment_oM/ScopeObjects/IScope.cs
+++ LifeCycleAssessment_oM/ScopeObjects/IScope.cs
@@ -24,7 +24,7 @@ using BH.oM.Base;
 
 namespace BH.oM.LifeCycleAssessment
 {
-    public interface IScope : IFragment
+    public interface IScope : IBHoMObject
     {
         /***************************************************/
         /**** Properties                                ****/
