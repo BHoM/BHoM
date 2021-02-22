777f6bd32bc4a2e4a5dfd28072f7f674c04a3230
Johanna Olin <Johanna.Olin@BuroHappold.com>
2021-02-22 15:16:35 +0000

    Added new result objects for time index results and analysis results, also added new time index result property to Node object

---
+++ Environment_oM/Results/IAnalysisResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+
+namespace BH.oM.Environment.Results
+{
+    public class IAnalysisResult : BHoMObject
+    {
+        public virtual double Result { get; set; } = new double();
+    }
+}
