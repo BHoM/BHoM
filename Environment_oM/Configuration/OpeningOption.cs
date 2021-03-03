2cb5554cace09776a958467d40b64fa57fa5dbee
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2021-03-03 23:26:09 +0000

    Add opening configuration object

---
+++ Environment_oM/Configuration/OpeningOption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+using System.ComponentModel;
+using BH.oM.Quantities.Attributes;
+
+namespace BH.oM.Environment.Configuration
+{
+    [Description("Defines the design options for Openings.")]
+    public class OpeningOption : BHoMObject
+    {
+        [Length]
+        [Description("Defines the height the opening should be.")]
+        public virtual double Height { get; set; } = 0;
+        
+        [Length]
+        [Description("Defines the width the opening should be.")]
+        public virtual double Width { get; set; } = 0;
+
+        [Length]
+        [Description("The distance between the base of the panel and the bottom of the opening.")]
+        public virtual double SillHeight { get; set; } = 0;
+    }
+}
