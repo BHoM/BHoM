eb8a1539707413a69b3a67e803a6d6b57003de78
rolyh <roly.hudson@gmail.com>
2021-04-07 10:54:35 +0100

    Simplify AvalueSettings

--- Humans_oM/ViewQuality/AvalueSettings.cs
+++ Humans_oM/ViewQuality/AvalueSettings.cs
@@ -33,20 +33,14 @@ namespace BH.oM.Humans.ViewQuality
         /**** Properties                                ****/
         /***************************************************/
 
-        [Description("Closed Polyline defining the Spectators Cone of Vision. This is the boundary of the near clipping plane of the viewing frustum. Default value is a 0.115 * 0.115 square 0.1 from the eye reference location. ")]
+        [Description("Closed Polyline defining the Spectators Cone of Vision. This is the boundary of the near clipping plane of the viewing frustum. Default value is a 0.115 * 0.115 square 0.1 from the eye reference location.")]
         public virtual Polyline EffectiveConeOfVision { get; set; } = new Polyline();
+
+        [Description("The distance from a spectator to the far clipping plane of their view frustum. Spectators in front of this plane will not be used in the Avalue calculation.")]
+        public virtual double FarClippingPlaneDistance { get; set; } = 1.0;
 
         [Description("Calculate proportion of playing area obstructed by heads of Spectators in front. Default value is false")]
         public virtual bool CalculateOcclusion { get; set; } =  false;
-
-        [Description("Distance from eye reference point to the plane where the Avalue is calculated.")]
-        public virtual double EyeFrameDist { get; set; } = 0.1;
-
-        [Description("The distance from a spectator to the far clipping plane of their view frustum. Spectators in front of this plane will not be used in the Cvalue calculation.")]
-        public virtual double FarClippingPlaneDistance { get; set; } = 1.0;
-
-        [Description("Spectator view cone angle in radians. Default is approximately 2.0944 radians or 120 degrees. Field of view for a spectator, within which spectators in front are consider to be effectively blocking the view and used for the Cvalue calcualtion. ")]
-        public virtual double ViewConeAngle { get; set; } = Math.PI * 2 / 3;
 
         /***************************************************/
     }
