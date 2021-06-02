617c9955493a553c8167cebf006be1ab3ab54c77
Kayleigh Houde <Kayleigh.Houde@burohappold.com>
2021-06-02 09:35:19 -0400

    Updated Fitting Type Enum Descriptions

--- MEP_oM/Enums/FittingType.cs
+++ MEP_oM/Enums/FittingType.cs
@@ -30,18 +30,31 @@ namespace BH.oM.MEP.Enums
     public enum FittingType
     {
         Undefined,
+        [Description("A fitting that allows you to change the size/shape of the linear MEP segment at a given juncture.")]
         Adaptor,
+        [Description("A fitting that is placed at the end of a linear MEP segment.")]
         Cap,
+        [Description("A fitting that joins two linear MEP segments of the same size and shape.")]
         Coupling,
+        [Description("A fitting that physically connects four different linear MEP segments, each at 90-degree angles.")]
         Cross,
+        [Description("A fitting that physically connects two different linear MEP segments, and re-directs the segment at a 30,45,60 or 90-degree angle.")]
         Elbow,
+        [Description("A fitting that allows you to change the size of the linear MEP segment at a given juncture, and gives additional reinforcement to both segments.")]
         Olet,
+        [Description("A fitting that is placed at the end of a linear MEP segment, allowing future access for maintenance purposes (clean out).")]
         Plug,
+        [Description("A fitting that allows you to change the size of the linear MEP segment at a given juncture.")]
         Reducer,
+        [Description("A fitting that keeps the main linear MEP segment size the same and allows a 45-degree transition to a 90-degree branch.")]
         Tap,
+        [Description("A fitting that allows you to change size and/or shape along the main linear MEP segment .")]
         Transition,
+        [Description("A fitting that keeps the main linear MEP segment size the same and allows a branch at a 90-degree angle.")]
         Tee,
+        [Description("A fitting that joins two linear MEP segments of the same size and shape, of which each end can be unscrewed independently.")]
         Union,
+        [Description("A fitting that joins three linear MEP segments of differing sizes and shapes, also known as pants.")]
         Wye
     }
 
