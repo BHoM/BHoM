b64305dbf84342e96be41d89464ce6c8b6b3f09d
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-08-26 16:58:58 +0100

    Graphics_oM: Refactor `Vertex` and rename it to `RenderPoint`
    Closes #1273

--- Graphics_oM/Render/Vertex.cs
+++ Graphics_oM/Render/Vertex.cs
@@ -20,12 +20,13 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Dimensional;
 using BH.oM.Geometry;
 using System.Drawing;
 
 namespace BH.oM.Graphics
 {
-    public class Vertex : IGeometry
+    public class RenderPoint : IGeometry, IElement0D, IRender
     {
         /***************************************************/
         /**** Properties                                ****/
@@ -33,15 +34,15 @@ namespace BH.oM.Graphics
 
         public virtual Geometry.Point Point { get; set; } = new Geometry.Point();
 
-        public virtual Color Color { get; set; } = Color.FromArgb(255, 250, 34, 74);
+        public virtual Color Colour { get; set; } = Color.FromArgb(255, 250, 34, 74);
 
         /***************************************************/
         /**** Explicit Casting                          ****/
         /***************************************************/
 
-        public static explicit operator Vertex(Geometry.Point point)
+        public static explicit operator RenderPoint(Geometry.Point point)
         {
-            return new Vertex { Point = point};
+            return new RenderPoint { Point = point };
         }
 
         /***************************************************/
