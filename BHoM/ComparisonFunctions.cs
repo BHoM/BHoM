19bc7ac18d31b1c1e9d8d697fc85fbcb2f03819c
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-10-12 11:20:07 +0100

    null-handling: assign delegates

--- BHoM/ComparisonFunctions.cs
+++ BHoM/ComparisonFunctions.cs
@@ -36,24 +36,24 @@ namespace BH.oM.Base
             "\nA `property full name` is the full property path, e.g. for a property named `Position`, the full name could be `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered." +
             "\nFirst parameter (`string`): property full name. Second parameter (`object`): the object holding this property. Return value (`string`): the modified property name." +
             "\nExample of property path: if interested in the property `Position`, the property path can be specified as `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered, or simply as `Position` if any property named as such is to be matched.")]
-        public virtual Func<string, object, string> PropertyFullNameModifier { get; set; } = null;
+        public virtual Func<string, object, string> PropertyFullNameModifier { get; set; } = delegate (string propertyFullName, object obj) { return propertyFullName; };
 
         [Description("A filter function on each 'property full name' being considered during comparison." +
-            "\nA `property full name` is the full property path, e.g. for a property named `Position`, the full name could be `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered." + 
+            "\nA `property full name` is the full property path, e.g. for a property named `Position`, the full name could be `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered." +
             "\nIf the Func returns true, the property is skipped (not considered when comparing or computing the Hash)." +
             "\nFirst parameter (`string`): property full name. Second parameter (`object`): the object holding this property. Return value (`bool`): true if the property is to be skipped, false otherwise.")]
-        public virtual Func<string, object, bool> PropertyFullNameFilter { get; set; } = null;
+        public virtual Func<string, object, bool> PropertyFullNameFilter { get; set; } = delegate (string propertyFullName, object obj) { return false; };
 
-        [Description("A filter function on CustomData keys. If the Func returns true, the key is skipped (not considered when comparing or computing the Hash)."+
+        [Description("A filter function on CustomData keys. If the Func returns true, the key is skipped (not considered when comparing or computing the Hash)." +
             "\nFirst parameter (`string`): Custom Data key. Second parameter (`object`): the CustomData dictionary. Return value (`bool`): true if the key is to be skipped, false otherwise.")]
-        public virtual Func<string, object, bool> CustomDataKeyFilter { get; set; } = null;
+        public virtual Func<string, object, bool> CustomDataKeyFilter { get; set; } = delegate (string customDataKey, object obj) { return false; };
 
 
         [Description("This function is executed before a 'property full name' is returned as a difference, and it modifies it." +
             "\nA `property full name` is the full property path, e.g. for a property named `Position`, the full name could be `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered." +
             "\nFirst parameter (`string`): property full name. Second parameter (`object`): the object holding this property. Return value (`string`): the modified property name." +
             "\nExample of property path: if interested in the property `Position`, the property path can be specified as `BH.oM.Structure.Elements.Node.Position` or `BH.oM.Structure.Elements.Bar.StartNode.Position`, etc., depending on the object being considered, or simply as `Position` if any property named as such is to be matched.")]
-        public virtual Func<string, object, string> PropertyDisplayNameModifier { get; set; } = null;
+        public virtual Func<string, object, string> PropertyDisplayNameModifier { get; set; } = delegate (string propertyFullName, object obj) { return propertyFullName; };
 
         /***************************************************/
     }
