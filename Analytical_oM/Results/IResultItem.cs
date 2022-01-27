712299afc8fea913f6b6bd8dae859d3e5befe102
Isak Naslund <Isak.Naslund@burohappold.com>
2022-01-27 11:45:37 +0100

    #1352
    Adding IResultItem interface

---
+++ Analytical_oM/Results/IResultItem.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BH.oM.Analytical.Results
+{
+    [Description("Base interface to flag that the result is a simple result line.\n" +
+                 "A class implementing the IResultItem interface should generally only contain basic properties such as doubles, ints, strings, DateTime and similar.\n" + 
+                 "A general rule of thumb is that a class implementing this interface should be able to be represented and readable as a single row in Excel or a SQL database.")]
+    public interface IResultItem : IResult
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+
+        /***************************************************/
+    }
+}
+
+
+
