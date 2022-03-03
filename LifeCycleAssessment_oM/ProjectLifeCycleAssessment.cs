3f0890143e724a9be24f7ace71e640f5e48c1ffe
Michael Hoehn <michael.hoehn@burohappold.com>
2022-03-03 14:14:51 -0700

    Create ProjectLCA

---
+++ LifeCycleAssessment_oM/ProjectLifeCycleAssessment.cs
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using BH.oM.Base;
+using System.ComponentModel;
+using BH.oM.Dimensional;
+
+namespace BH.oM.LifeCycleAssessment
+{
+    [Description("Project LCA can be used to collect all objects used in an evaluation along with the project's specific metatdata for tracking within a database.")]
+    public class ProjectLifeCycleAssessment : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("LCA Scope metadata designed to help differentiate between project specifications.")]
+        public virtual LifeCycleAssessmentScope LCAScope { get; set; } = new LifeCycleAssessmentScope();
+
+        [Description("A collection of elements you wish to include in the project LCA.")]
+        public virtual List<IElementM> Elements { get; set; } = null;
+
+        /***************************************************/
+    }
+}
+
+
