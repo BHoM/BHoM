d9713515c855ceb0837f00ba28a366e81fd34819
Alessio Lombardi <alessio.lombardi@burohappold.com>
2022-03-07 19:24:50 +0000

    Create DifferenceType.cs

---
+++ Diffing_oM/DifferenceType.cs
@@ -0,0 +1,47 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using BH.oM.Base.Attributes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Diffing
+{
+    [Description("Specifies various kinds of Differences. It can be useful to mark and filter specific implementations of IPropertyDifferences.")]
+    public enum DifferenceType
+    {
+        [Description("Indicates that the difference is a modification.")]
+        Modified,
+        [Description("Indicates that the difference is a removal.")]
+        Removed,
+        [Description("Indicates that the difference is an addition.")]
+        Added
+    }
+}
+
+
+
