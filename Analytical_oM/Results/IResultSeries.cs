e0339f3f041004232b245b9bc6458777b1786c69
Isak Naslund <Isak.Naslund@burohappold.com>
2022-03-22 16:57:59 +0100

    Add ResultSeries interface #1378

---
+++ Analytical_oM/Results/IResultSeries.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BH.oM.Analytical.Results
+{
+    [Description("Base interface to flag that the result contains a series of results, for example values across each hour of the year and were the amount of data means memory usage is an issue.\n" +
+                 "A class inplementing this interface is assumed to have the result values stored on a property that implements the IReadOnlyList<double>.\n" + 
+                 "A class implementing the IResultItem interface should not implement this interface as well.")]
+    public interface IResultSeries : IResult
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+
+        /***************************************************/
+    }
+}
+
+
+
