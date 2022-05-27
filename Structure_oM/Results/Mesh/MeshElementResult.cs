050c3b56cf6c860f62f869da741221bbdc4ec6e6
Erik Narhi <Erik.Narhi@burohappold.com>
2022-05-27 10:34:54 -0700

    Descriptions updated

--- Structure_oM/Results/Mesh/MeshElementResult.cs
+++ Structure_oM/Results/Mesh/MeshElementResult.cs
@@ -38,10 +38,10 @@ namespace BH.oM.Structure.Results
         [Description("Id of the FEMesh/Panel that this result belongs to. When extracted from an analysis package, the object id will match the format and value used in that particular package.")]
         public virtual IComparable ObjectId { get; }
 
-        [Description("Id of the Node in the mesh that this result belongs to. Will be empty for smoothing types not relating to Nodes. When extracted from an analysis package, the Node id will correspond to the node id in the software and match the format and value used in that particular package.")]
+        [Description("Id of the Node in the mesh that this result belongs to. Will be empty for face-based results. When extracted from an analysis package, the Node id will correspond to the node id in the software and match the format and value used in that particular package.")]
         public virtual IComparable NodeId { get; }
 
-        [Description("Id of the FEFace that this result belongs to. Will be empty for smoothing types not relating to Faces. When extracted from an analysis package, the face id will correspond to the face id in the software and match the format and value used in that particular package.")]
+        [Description("Id of the FEFace that this result belongs to. Will be empty for node-based results. When extracted from an analysis package, the face id will correspond to the face id in the software and match the format and value used in that particular package.")]
         public virtual IComparable MeshFaceId { get; }
 
         [Description("Identifier for the Loadcase or LoadCombination that the result belongs to. Is generally name or number of the loadcase, depending on the analysis package.")]
