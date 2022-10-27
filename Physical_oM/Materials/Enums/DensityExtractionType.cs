ef12b5f296807e9130fb5a0c0ba741912035aa9e
Isak Naslund <Isak.Naslund@burohappold.com>
2022-10-27 13:51:46 +0200

    Adding objects to help control how Density is to be extracted from a Physical Material

---
+++ Physical_oM/Materials/Enums/DensityExtractionType.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace BH.oM.Physical.Materials
+{
+    [Description("Specifies how the density should be extracted from a physical Material with multiple IMaterialProperties of type IDensityProvider attached to it.")]
+    public enum DensityExtractionType
+    {
+        [Description("Require all densities across all IDensityProviders extracted to be equal within tolerance. If within tolerance, the average is computed.")]
+        AllMatching,
+        [Description("Compute and return the average value of all Densities found, independent of difference between them.")]
+        Average,
+        [Description("Return the maximum value of all densities found.")]
+        Maximum,
+        [Description("Minimum value of densities found.")]
+        Minimum
+    }
+}
---
+++ Physical_oM/Materials/Options/DensityExtractionOptions.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.oM.Physical.Materials.Options
+{
+    [Description("Settings for extraction of Density from a Physical Material.")]
+    public class DensityExtractionOptions : IObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("A specific type of IDensityProvider to limit the search to. If null all IDensityProviders on the material are considered.")]
+        public virtual Type Type { get; set; } = null;
+
+        public virtual DensityExtractionType ExtractionType { get; set; } = DensityExtractionType.AllMatching;
+
+        [Ratio]
+        [Description("The ratio tolerance for considering the value of the densities as equal. Comparison is made by comparing the differance between min and max over their mean.")]
+        public virtual double EqualTolerance { get; set; } = 0.001;
+
+        [Description("Ignores densities of 0 if true when computing average or checking for equality.")]
+        public virtual bool IgnoreZeroValues { get; set; } = true;
+        /***************************************************/
+
+    }
+}
+
+
