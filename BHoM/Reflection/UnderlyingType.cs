f5645200b6d51bc162c4d2b38997e888f65a5fd3
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-11-07 12:48:05 +0000

    Typo

--- BHoM/Reflection/UnderlyingType.cs
+++ BHoM/Reflection/UnderlyingType.cs
@@ -26,7 +26,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.oM.Base.RTeflection
+namespace BH.oM.Base.Reflection
 {
     public class UnderlyingType : BHoMObject
     {
