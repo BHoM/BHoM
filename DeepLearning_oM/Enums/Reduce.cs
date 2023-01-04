0053c6c353fd99932abfdeaf752a048e905d5056
BHoMBot <BHoMBot@burohappold.com>
2023-01-04 11:35:04 +0000

    Update copyright headers to 2023

--- DeepLearning_oM/Enums/Reduce.cs
+++ DeepLearning_oM/Enums/Reduce.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -31,6 +31,7 @@ namespace BH.oM.DeepLearning
         Sum = 2,
     }
 }
+
 
 
 
--- Test_oM/Results/InputOutputDifference.cs
+++ Test_oM/Results/InputOutputDifference.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -106,5 +106,6 @@ namespace BH.oM.Test.Results
         /***************************************************/
     }
 }
+
 
 
