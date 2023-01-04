7796ebd378b2207d7495b75132ace4a9637ff869
BHoMBot <BHoMBot@burohappold.com>
2023-01-04 11:35:04 +0000

    Update copyright headers to 2023

--- Environment_oM/SpaceCriteria/Lighting.cs
+++ Environment_oM/SpaceCriteria/Lighting.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -47,6 +47,7 @@ namespace BH.oM.Environment.SpaceCriteria
         public virtual double LuminousEfficacy { get; set; } = 0.0;
     }
 }
+
 
 
 
