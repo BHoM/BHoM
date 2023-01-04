60b2c7b322a4db0445d21dcc2cbe2a99b6aac509
BHoMBot <BHoMBot@burohappold.com>
2023-01-04 11:35:04 +0000

    Update copyright headers to 2023

--- Environment_oM/Elements/Enums/BuildingType.cs
+++ Environment_oM/Elements/Enums/BuildingType.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -33,6 +33,7 @@ namespace BH.oM.Environment.Elements
         Residential,
     }
 }
+
 
 
 
