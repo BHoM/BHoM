db79c7c0cfcf24a5cbe076ded972dc9fa04c4e73
Isak Naslund <Isak.Naslund@burohappold.com>
2023-01-19 14:38:11 +0100

    Update affected explicit castings

--- Geometry_oM/Curve/NurbsCurve.cs
+++ Geometry_oM/Curve/NurbsCurve.cs
@@ -86,9 +86,9 @@ namespace BH.oM.Geometry
         public static explicit operator NurbsCurve(Ellipse ellipse)
         {
 
-            Point centre = ellipse.Centre;
-            Vector d1 = ellipse.Radius1 * ellipse.Axis1;
-            Vector d2 = ellipse.Radius2 * ellipse.Axis2;
+            Point centre = ellipse.CoordinateSystem.Origin;
+            Vector d1 = ellipse.Radius1 * ellipse.CoordinateSystem.X;
+            Vector d2 = ellipse.Radius2 * ellipse.CoordinateSystem.Y;
             double factor = Math.Cos(Math.PI / 4);
 
             List<Point> points = new List<Point>
