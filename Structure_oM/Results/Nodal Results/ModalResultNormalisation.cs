c52e491778d3b172f03489824341a6c217decae5
Isak Naslund <Isak.Naslund@burohappold.com>
2023-02-02 16:11:22 +0100

    Add new NodeModalResult class containing modeshape and mass

---
+++ Structure_oM/Results/Nodal Results/ModalResultNormalisation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.Structure.Results.Nodal_Results
+{
+    public enum ModalResultNormalisation
+    {
+        Eigenvector,
+        Mass
+    }
+}
