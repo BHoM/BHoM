180b8f57452ab59dccb7ff2658521d035abf228e
Isak Naslund <Isak.Naslund@burohappold.com>
2023-02-14 11:56:51 +0100

    Remove split version and update property names to abbreviations
    The split solution become far to messy, hence rolling it back.
    To be able to more clearly understand the property names, they have been updated to the relevant abbreviations for all stiffness properties

--- Structure_oM/MaterialFragments/Glulam.cs
+++ Structure_oM/MaterialFragments/Glulam.cs
@@ -29,8 +29,9 @@ using System.ComponentModel;
 
 namespace BH.oM.Structure.MaterialFragments
 {
+
     [Description("Structural timber material of type Glued Laminated Timber to be used on structural elements and properties, or as a fragment of the physical material.")]
-    public class Glulam : BHoMObject, ITimber<YoungsModulusGlulam, ShearModulusGlulam, StrengthGlulam>
+    public class Glulam : BHoMObject, ITimber
     {
         /***************************************************/
         /**** Properties - General and analysis         ****/
@@ -40,11 +41,11 @@ namespace BH.oM.Structure.MaterialFragments
         public override string Name { get; set; }
 
         [Density]
-        [Description("Mean Density. Used to calculate mass. Called ρmean in Eurocode.")]
+        [Description("Mean Density. Used to calculate mass. Called ρg,mean in Eurocode.")]
         public virtual double Density { get; set; }
 
         [Density]
-        [Description("Characteristic Density. Used to calculate other mechanical properties (not mass). Called ρk in Eurocode.")]
+        [Description("Characteristic Density. Used to calculate other mechanical properties (not mass). Called ρg,k in Eurocode.")]
         public virtual double DensityCharacteristic { get; set; }
 
         [Ratio]
@@ -71,128 +72,73 @@ namespace BH.oM.Structure.MaterialFragments
         /**** Properties - Stiffness                    ****/
         /***************************************************/
 
-        [Description("Storage for YoungsModulus proeprties in various directions.")]
-        public virtual YoungsModulusGlulam YoungsModulusProperties { get; set; }
+        [YoungsModulus]
+        [Description("Mean modulus of elasticity parallel bending, E0,g,mean in Eurocode.")]
+        public virtual double E_0_Mean { get; set; }
 
-        [Description("Storage for Shear Modulus proeprties in various directions.")]
-        public virtual ShearModulusGlulam ShearModulusProperties { get; set; }
+        [YoungsModulus]
+        [Description("5 percentile modulus of elasticity parallel bending, E0,g,05 in Eurocode.")]
+        public virtual double E_0_k { get; set; }
 
-        [Description("Storage for Strength proeprties.")]
-        public virtual StrengthGlulam Strength { get; set; }
+        [YoungsModulus]
+        [Description("Mean modulus of elasticity perpendicular, E90,g,mean in Eurocode.")]
+        public virtual double E_90_Mean { get; set; }
+
+        [YoungsModulus]
+        [Description("Mean modulus of elasticity perpendicular, E90,g,05 in Eurocode.")]
+        public virtual double E_90_k { get; set; }
+
+        [ShearModulus]
+        [Description("Mean shear modulus parallel to the grain, Gg,mean in Eurocode.")]
+        public virtual double G_Mean { get; set; }
+
+        [ShearModulus]
+        [Description("Characteristic shear modulus parallel to the grain, Gg,05 in Eurocode.")]
+        public virtual double G_k { get; set; }
+
+        [ShearModulus]
+        [Description("Mean shear modulus parallel to the grain, Gr,g,mean in Eurocode.")]
+        public virtual double Gr_Mean { get; set; }
+
+        [ShearModulus]
+        [Description("Characteristic shear modulus parallel to the grain, Gr,g,05 in Eurocode.")]
+        public virtual double Gr_k { get; set; }
+
+        /***************************************************/
+        /**** Properties - Strength                     ****/
+        /***************************************************/
+
+        [Stress]
+        [Description("Bending Strength. Tension stress parallel to the grain at failure in bending as calculated from beam equations. Called fm,g,k in Eurocode.")]
+        public virtual double BendingStrength { get; set; }
+
+        [Stress]
+        [Description("Tension Parallel Strength. Tension stress parallel to the grain at failure in net tension. Called ft,0,g,k in Eurocode.")]
+        public virtual double TensionParallelStrength { get; set; }
+
+        [Stress]
+        [Description("Tension Perpendicular Strength. Tension stress perpendicular to the grain at failure in net tension. Called ft,90,g,k in Eurocode.")]
+        public virtual double TensionPerpendicularStrength { get; set; }
+
+        [Stress]
+        [Description("Compression Parallel Strength. Compression stress parallel to the grain at failure in net compression. Called fc,0,g,k in Eurocode.")]
+        public virtual double CompressionParallelStrength { get; set; }
+
+        [Stress]
+        [Description("Compression Perpendicular Strength. Compression stress perpendicular to the grain at failure in net compression. Called fc,90,g,k in Eurocode.")]
+        public virtual double CompressionPerpendicularStrength { get; set; }
+
+        [Stress]
+        [Description("Shear Strength. Shear stress parallel to the grain at failure in net shear, i.e. shear relevant to beam bending. Called fv,g,k in Eurocode.")]
+        public virtual double ShearStrength { get; set; }
+
+        [Stress]
+        [Description("Rolling Shear Strength. Shear stress perpendicular to the grain at failure in net shear. Called fr,g,k in Eurocode.")]
+        public virtual double RollingShearStrength { get; set; }
 
         /***************************************************/
 
     }
-
-    //[Description("Structural timber material of type Glued Laminated Timber to be used on structural elements and properties, or as a fragment of the physical material.")]
-    //public class Glulam : BHoMObject, ITimber
-    //{
-    //    /***************************************************/
-    //    /**** Properties - General and analysis         ****/
-    //    /***************************************************/
-
-    //    [Description("Name. A unique name is required for some structural packages to create and identify the object.")]
-    //    public override string Name { get; set; }
-
-    //    [Density]
-    //    [Description("Mean Density. Used to calculate mass. Called ρg,mean in Eurocode.")]
-    //    public virtual double Density { get; set; }
-
-    //    [Density]
-    //    [Description("Characteristic Density. Used to calculate other mechanical properties (not mass). Called ρg,k in Eurocode.")]
-    //    public virtual double DensityCharacteristic { get; set; }
-
-    //    [Ratio]
-    //    [Description("Dynamic Damping Ratio. Ratio between actual damping and critical damping.")]
-    //    public virtual double DampingRatio { get; set; }
-
-    //    [YoungsModulus]
-    //    [Description("Modulus Of Elasticity of the material to be used for Analysis. Ratio between stress and strain in all directions. Vector components should generally be made up of: X - Parallel; Y - Perpendicular; Z - Perpendicular.")]
-    //    public virtual Vector YoungsModulus { get; set; }
-
-    //    [Ratio]
-    //    [Description("Poisson's Ratio. Ratio between axial and transverse strain. Typically take as 0.4 in all directions, though value varies depending on timber species. Vector components made up of: X - Parallel; Y - Perpendicular; Z - Perpendicular.")]
-    //    public virtual Vector PoissonsRatio { get; set; }
-
-    //    [ThermalExpansionCoefficient]
-    //    [Description("Thermal Expansion Coefficeint. Strain induced in the material per unit change of temperature. Typically take as 5x10^-6 in all directions, though value varies depending on timber species. Vector components made up of: X - Parallel; Y - Perpendicular; Z - Perpendicular.")]
-    //    public virtual Vector ThermalExpansionCoeff { get; set; }
-
-    //    [ShearModulus]
-    //    [Description("Shear Modulus or Modulus of Rigidity of the material to be used for Analysis. Ratio between shear stress and shear strain. Vector components should generally be made up of: X - Parallel; Y - Parallel; Z - Perpendicular.")]
-    //    public virtual Vector ShearModulus { get; set; }
-
-    //    /***************************************************/
-    //    /**** Properties - Stiffness                    ****/
-    //    /***************************************************/
-
-    //    [YoungsModulus]
-    //    [Description("Mean modulus of elasticity parallel bending, E0,g,mean in Eurocode.")]
-    //    public virtual double YoungsModulusParallelMean { get; set; }
-
-    //    [YoungsModulus]
-    //    [Description("5 percentile modulus of elasticity parallel bending, E0,g,05 in Eurocode.")]
-    //    public virtual double YoungsModulusParallelCharacteristic { get; set; }
-
-    //    [YoungsModulus]
-    //    [Description("Mean modulus of elasticity perpendicular, E90,g,mean in Eurocode.")]
-    //    public virtual double YoungsModulusPerpendicularMean { get; set; }
-
-    //    [YoungsModulus]
-    //    [Description("Mean modulus of elasticity perpendicular, E90,g,05 in Eurocode.")]
-    //    public virtual double YoungsModulusPerpendicularCharacteristic { get; set; }
-
-    //    [ShearModulus]
-    //    [Description("Mean shear modulus parallel to the grain, Gg,mean in Eurocode.")]
-    //    public virtual double ShearModulusMean { get; set; }
-
-    //    [ShearModulus]
-    //    [Description("Characteristic shear modulus parallel to the grain, Gg,05 in Eurocode.")]
-    //    public virtual double ShearModulusCharacteristic { get; set; }
-
-    //    [ShearModulus]
-    //    [Description("Mean shear modulus parallel to the grain, Gr,g,mean in Eurocode.")]
-    //    public virtual double RollingShearModulusMean { get; set; }
-
-    //    [ShearModulus]
-    //    [Description("Characteristic shear modulus parallel to the grain, Gr,g,05 in Eurocode.")]
-    //    public virtual double RollingShearModulusCharacteristic { get; set; }
-
-    //    /***************************************************/
-    //    /**** Properties - Strength                     ****/
-    //    /***************************************************/
-
-    //    [Stress]
-    //    [Description("Bending Strength. Tension stress parallel to the grain at failure in bending as calculated from beam equations. Called fm,g,k in Eurocode.")]
-    //    public virtual double BendingStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Tension Parallel Strength. Tension stress parallel to the grain at failure in net tension. Called ft,0,g,k in Eurocode.")]
-    //    public virtual double TensionParallelStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Tension Perpendicular Strength. Tension stress perpendicular to the grain at failure in net tension. Called ft,90,g,k in Eurocode.")]
-    //    public virtual double TensionPerpendicularStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Compression Parallel Strength. Compression stress parallel to the grain at failure in net compression. Called fc,0,g,k in Eurocode.")]
-    //    public virtual double CompressionParallelStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Compression Perpendicular Strength. Compression stress perpendicular to the grain at failure in net compression. Called fc,90,g,k in Eurocode.")]
-    //    public virtual double CompressionPerpendicularStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Shear Strength. Shear stress parallel to the grain at failure in net shear, i.e. shear relevant to beam bending. Called fv,g,k in Eurocode.")]
-    //    public virtual double ShearStrength { get; set; }
-
-    //    [Stress]
-    //    [Description("Rolling Shear Strength. Shear stress perpendicular to the grain at failure in net shear. Called fr,g,k in Eurocode.")]
-    //    public virtual double RollingShearStrength { get; set; }
-
-    //    /***************************************************/
-
-    //}
 }
 
 
