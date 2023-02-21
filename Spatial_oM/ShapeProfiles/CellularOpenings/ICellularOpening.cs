f44d5d22bff0ac87abc544c3cf7e24843ec2c636
Isak Naslund <Isak.Naslund@burohappold.com>
2023-02-21 09:32:28 +0100

    Add Cellular shape profile

---
+++ Spatial_oM/ShapeProfiles/CellularOpenings/ICellularOpening.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BH.oM.Quantities.Attributes;
+using System.ComponentModel;
+
+namespace BH.oM.Spatial.ShapeProfiles.CellularOpenings
+{
+    public interface ICellularOpening : IBHoMObject
+    {
+        [Length]
+        [Description("Length of the intermidiate web post.")]
+        double WidthWebPost { get; }
+
+        [Length]
+        [Description("Length of the end web post.")]
+        double LengthEndPost { get; }
+
+        [Length]
+        [Description("Centre distance between openings.")]
+        double Spacing { get; }
+
+        [Length]
+        [Description("Height of any spacer plate between the two profile sides.")]
+        double SpacerHeight { get; }
+    }
+}
---
+++ Spatial_oM/ShapeProfiles/CellularOpenings/SinusoidalOpening.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Spatial.ShapeProfiles.CellularOpenings
+{
+    public class SinusoidalOpening : BHoMObject, ICellularOpening, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Length]
+        [Description("Total height of the opening.")]
+        public virtual double Height { get; }
+
+        [Length]
+        [Description("Total width of the sinusoidal part of the opening.")]
+        public virtual double SinusoidalLength { get; }
+
+        [Length]
+        [Description("Height of any spacer plate between the two profile sides.")]
+        public virtual double SpacerHeight { get; }
+
+        [Length]
+        [Description("Length of the intermidiate web post.")]
+        public virtual double WidthWebPost { get; }
+
+        [Length]
+        [Description("Length of the end web post.")]
+        public virtual double LengthEndPost { get; }
+
+        [Length]
+        [Description("Centre distance between openings.")]
+        public virtual double Spacing { get; }
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public SinusoidalOpening(double height, double sinusoidalLength, double topWidth, double spacerHeight, double widthWebPost, double lengthEndPost, double spacing)
+        { 
+            Height = height;
+            SinusoidalLength = sinusoidalLength;
+            SpacerHeight = spacerHeight;
+            WidthWebPost = widthWebPost;
+            LengthEndPost = lengthEndPost;
+            Spacing = spacing;
+        }
+
+        /***************************************************/
+    }
+}
