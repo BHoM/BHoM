25725e2f8414c1b1c71802d78d8150f7faf12489
Isak Naslund <Isak.Naslund@burohappold.com>
2023-02-24 12:44:52 +0100

    Create VoidedISectionProfile.cs

---
+++ Spatial_oM/ShapeProfiles/VoidedISectionProfile.cs
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Geometry;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using BH.oM.Quantities.Attributes;
+
+namespace BH.oM.Spatial.ShapeProfiles
+{
+    [Description("I-shaped profile with parallel flanges with equal thickness, with a opaning/void through the web.")]
+    public class VoidedISectionProfile : BHoMObject, IProfile, IImmutable
+    {
+
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual ShapeType Shape { get; } = ShapeType.ISection;
+
+        [Length]
+        [Description("Full depth between the extreme fibres of the flanges.")]
+        public virtual double Height { get; }
+
+        [Length]
+        [Description("Height of opening through centre of the web.")]
+        public virtual double OpeningHeight { get; }
+
+        [Length]
+        [Description("Height of the T section at each end of the opening.")]
+        public virtual double HeightTSection { get; }
+
+        [Length]
+        [Description("Full width of both flanges between the extreme fibres of the flanges.")]
+        public virtual double Width { get; }
+
+        [Length]
+        public virtual double WebThickness { get; }
+
+        [Length]
+        [Description("Thickness of both flanges.")]
+        public virtual double FlangeThickness { get; }
+
+        [Length]
+        [Description("Fillet radius between inner face of the flanges and faces of the web.")]
+        public virtual double RootRadius { get; }
+
+        [Length]
+        [Description("Fillet radius at the end of the flanges. Value need to be smaller or equal than the flange thickness.")]
+        public virtual double ToeRadius { get; }
+
+        [Description("Edge curves that matches the dimensions in the global XY-plane.")]
+        public virtual ReadOnlyCollection<ICurve> Edges { get; }
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public VoidedISectionProfile(double height, double openingHeight, double heightTSection, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius, IEnumerable<ICurve> edges)
+        {
+            Height = height;
+            OpeningHeight = openingHeight;
+            HeightTSection = heightTSection;
+            Width = width;
+            WebThickness = webthickness;
+            FlangeThickness = flangeThickness;
+            RootRadius = rootRadius;
+            ToeRadius = toeRadius;
+            Edges = new ReadOnlyCollection<ICurve>(edges.ToList());
+        }
+
+        /***************************************************/
+    }
+}
+
+
+
+
