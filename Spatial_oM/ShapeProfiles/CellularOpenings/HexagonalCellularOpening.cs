5a53ce75bdbd28256c84faa09416acc096a9239e
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-10 11:35:40 +0100

    Add "Cellular" to name of section and update descriptions of WIdthWebPost

--- Spatial_oM/ShapeProfiles/CellularOpenings/HexagonalCellularOpening.cs
+++ Spatial_oM/ShapeProfiles/CellularOpenings/HexagonalCellularOpening.cs
@@ -29,19 +29,28 @@ using System.Text;
 
 namespace BH.oM.Spatial.ShapeProfiles.CellularOpenings
 {
-    public class CircularOpening : BHoMObject, ICellularOpening, IImmutable
+    public class HexagonalCellularOpening : BHoMObject, ICellularOpening, IImmutable
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
         [Length]
-        [Description("Diameter of the opening.")]
-        public virtual double Diameter { get; }
+        [Description("Height of the hexagonal opening, equal to the height of the cut being made in the section being castellated.\n" +
+                     "For case of non-zero SpacerHeight, the total height of the opening will be Height+SpacerHeight.")]
+        public virtual double Height { get; }
 
         [Length]
-        [Description("Length of the intermediate web post.")]
+        [Description("Total width of the opening.")]
+        public virtual double Width { get; }
+
+        [Length]
+        [Description("Clear distance between openings.")]
         public virtual double WidthWebPost { get; }
+
+        [Length]
+        [Description("Height of any spacer plate between the two profile sides. Results in an octagonal opening for a non-zero value.")]
+        public virtual double SpacerHeight { get; }
 
         [Length]
         [Description("Centre to centre distance between openings.")]
@@ -51,9 +60,11 @@ namespace BH.oM.Spatial.ShapeProfiles.CellularOpenings
         /**** Constructors                              ****/
         /***************************************************/
 
-        public CircularOpening(double diameter, double widthWebPost, double spacing)
-        {
-            Diameter = diameter;
+        public HexagonalCellularOpening(double height, double width, double spacerHeight, double widthWebPost, double spacing)
+        { 
+            Height = height;
+            Width = width;
+            SpacerHeight = spacerHeight;
             WidthWebPost = widthWebPost;
             Spacing = spacing;
         }
