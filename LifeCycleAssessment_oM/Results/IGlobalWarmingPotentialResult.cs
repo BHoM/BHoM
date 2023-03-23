7a191a514c21f98a358a7441d4c58b019d768be4
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-23 09:29:17 +0100

    Add base interfaces for all results

---
+++ LifeCycleAssessment_oM/Results/IGlobalWarmingPotentialResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.LifeCycleAssessment.Results
+{
+    [Description("base itnerface for all Global Warming Potential results.")]
+    public interface IGlobalWarmingPotentialResult : ILifeCycleAssesmentResult
+    {
+        [Description("")]      
+        double BiogenicCarbon { get; }
+    }
+}
