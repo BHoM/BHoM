234ffe9df627c644ecd7bd80db677a55757c20f2
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-23 08:50:57 +0100

    Add GWP Metric and GWP/x Quantity attribute

---
+++ Quantities_oM/Attributes/GlobalWarmingPotentialPerQuantity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for global warming potential per quantity.")]
+    public class GlobalWarmingPotentialPerQuantity : QuantityAttribute
+    {
+        public override string SIUnit { get; } = "kg CO2eq / x";
+    }
+}
