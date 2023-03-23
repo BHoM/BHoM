571119c3145c62ad04e9496032ce085fcc34a367
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-23 08:51:22 +0100

    Add Acidification potential metric and Quantity per x

---
+++ Quantities_oM/Attributes/AcidificationPotentialPerQuantity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for acidification potential per quantity.")]
+    public class AcidificationPotentialPerQuantity : QuantityAttribute
+    {
+        public override string SIUnit { get; } = "mol H+ eq / x";
+    }
+}
