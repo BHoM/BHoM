0ab6f1c4581dad4dd0a8b856b4851b26af5aef2b
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-23 09:29:57 +0100

    Add Quantities for GWP and AP

---
+++ Quantities_oM/Attributes/AcidificationPotential.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for acidification potential.")]
+    public class AcidificationPotential : QuantityAttribute
+    {
+        public override int N { get; } = 1;
+
+        public override string SIUnit { get; } = "mol H+ eq";
+    }
+}
