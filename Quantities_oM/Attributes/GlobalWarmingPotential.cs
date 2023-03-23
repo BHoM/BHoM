eab4f55f6bf01171d793762b20819fb868e3c62c
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-23 09:29:57 +0100

    Add Quantities for GWP and AP

---
+++ Quantities_oM/Attributes/GlobalWarmingPotential.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for global warming potential.")]
+    public class GlobalWarmingPotential : QuantityAttribute
+    {
+        public override int M { get; } = 1;
+
+        public override string SIUnit { get; } = "kg CO2eq";
+    }
+}
