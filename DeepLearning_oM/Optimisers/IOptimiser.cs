22fdea8a612a2ad8fdb61b61ba21d0e67e0ddd57
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-04-27 15:09:20 +0100

    Add IObject compliance for DeepLearning_oM

--- DeepLearning_oM/Optimisers/IOptimiser.cs
+++ DeepLearning_oM/Optimisers/IOptimiser.cs
@@ -20,13 +20,11 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Base;
+
 namespace BH.oM.DeepLearning.Optimisers
 {
-    public interface IOptimiser
+    public interface IOptimiser : IObject
     {
     }
 }
-
-
-
-
