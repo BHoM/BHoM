cc88a347741e9d692ae33498092112004ac3d410
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-04-27 15:06:56 +0100

    Update Analytical_oM for IObject compliance

--- Analytical_oM/IAnalytical.cs
+++ Analytical_oM/IAnalytical.cs
@@ -25,10 +25,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Base;
 
 namespace BH.oM.Analytical
 {
-    public interface IAnalytical
+    public interface IAnalytical : IObject
     {
     }
 }
