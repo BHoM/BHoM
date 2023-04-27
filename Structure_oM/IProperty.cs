4cdf62d4078cca7ddd917431efb9c4cdba440151
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-04-27 15:10:10 +0100

    Add IObject compliance for Structure_oM

--- Structure_oM/IProperty.cs
+++ Structure_oM/IProperty.cs
@@ -20,15 +20,13 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Base;
 using System.ComponentModel;
 
 namespace BH.oM.Structure
 {
     [Description("Base interface for all structural properties.")]
-    public interface IProperty
+    public interface IProperty : IObject
     {
     }
 }
-
-
-
