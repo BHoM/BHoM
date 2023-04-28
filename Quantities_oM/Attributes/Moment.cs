808a72a592beb42ac366b41f46dea04dd02cd993
Pawel Baran <pawel.baran@burohappold.com>
2023-04-28 11:58:00 +0200

    refactor of the attributes in progress

--- Quantities_oM/Attributes/Moment.cs
+++ Quantities_oM/Attributes/Moment.cs
@@ -20,6 +20,7 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Base.Attributes;
 using System;
 
 namespace BH.oM.Quantities.Attributes
