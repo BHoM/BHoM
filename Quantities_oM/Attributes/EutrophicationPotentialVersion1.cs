fdbb16262c9203bdc4b6aaf91391a4f06c131479
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-12 16:33:01 +0200

    Add new base quantity attributes for additional indicators

---
+++ Quantities_oM/Attributes/EutrophicationPotentialVersion1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for Eutrophication Potential for version 1 of list of indicators.")]
+    public class EutrophicationPotentialVersion1 : QuantityAttribute
+    {
+        public override int M { get; } = 1;
+
+        public override string SIUnit { get; } = "kg PO43- eq";
+    }
+}
