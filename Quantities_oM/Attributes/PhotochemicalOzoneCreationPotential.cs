d9010a987d965e0023687d1e658379e8eca3e098
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-12 16:33:01 +0200

    Add new base quantity attributes for additional indicators

---
+++ Quantities_oM/Attributes/PhotochemicalOzoneCreationPotential.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for Photochemical Ozone Creation Potential.")]
+    public class PhotochemicalOzoneCreationPotential : QuantityAttribute
+    {
+        public override int M { get; } = 1;
+
+        public override string SIUnit { get; } = "kg NMVOC eq";
+    }
+}
