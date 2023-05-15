4351dc8d5e90e7c55b696f86c5047f880ef035c3
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-15 08:26:08 +0200

    Adding PerQuantity Quantity attributes for new metrics

---
+++ Quantities_oM/Attributes/EutrophicationPotentialTerrestrialPerQuantity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for Eutrophication Potential Terrestrial per quantity.")]
+    public class EutrophicationPotentialTerrestrialPerQuantity : QuantityAttribute
+    {
+        public override int N { get; } = 1;
+
+        public override string SIUnit { get; } = "mol N eq per unit";
+    }
+}
