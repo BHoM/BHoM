b1483fca2a245e90beb1c8d896b9e4a1d613a26b
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-15 08:26:08 +0200

    Adding PerQuantity Quantity attributes for new metrics

---
+++ Quantities_oM/Attributes/AbioticDepletionPotentialNonFossilPerQuantity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for Abiotic depletion potential (ADP) for minerals and metals (non-fossil resources) per quantity.")]
+    public class AbioticDepletionPotentialNonFossilPerQuantity : QuantityAttribute
+    {
+        public override int M { get; } = 1;
+
+        public override string SIUnit { get; } = "kg Sb eq per unit";
+    }
+}
---
+++ Quantities_oM/Attributes/OzoneDepletionPotentialPerQuantity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Quantities.Attributes
+{
+    [Description("Quantity type for Ozone Depletion Potential per quantity.")]
+    public class OzoneDepletionPotentialPerQuantity : QuantityAttribute
+    {
+        public override int M { get; } = 1;
+
+        public override string SIUnit { get; } = "kg CFC11 eq per unit";
+    }
+}
