e269f8aeeda5c88862e2237da053e84f163cd3f9
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-15 07:58:13 +0200

    remove protected setters from element results

--- LifeCycleAssessment_oM/Results/ElementResults/AcidificationPotentialElementResult.cs
+++ LifeCycleAssessment_oM/Results/ElementResults/AcidificationPotentialElementResult.cs
@@ -40,75 +40,75 @@ namespace BH.oM.LifeCycleAssessment.Results
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the Raw materials in the product stage.")]
-        public override double A1 { get; protected set; }
+        public override double A1 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the Transport in the product stage.")]
-        public override double A2 { get; protected set; }
+        public override double A2 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the Manufacturing in the product stage.")]
-        public override double A3 { get; protected set; }
+        public override double A3 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the full product stage.")]
-        public override double A1toA3 { get; protected set; }
+        public override double A1toA3 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the transport during the assembly stage.")]
-        public override double A4 { get; protected set; }
+        public override double A4 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the final assembly during the assembly stage.")]
-        public override double A5 { get; protected set; }
+        public override double A5 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the general use during the usage stage.")]
-        public override double B1 { get; protected set; }
+        public override double B1 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the maintance during the usage stage.")]
-        public override double B2 { get; protected set; }
+        public override double B2 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the repair during the usage stage.")]
-        public override double B3 { get; protected set; }
+        public override double B3 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the replacement during the usage stage.")]
-        public override double B4 { get; protected set; }
+        public override double B4 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the refurbishment during the usage stage.")]
-        public override double B5 { get; protected set; }
+        public override double B5 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the operational energy use during the usage stage.")]
-        public override double B6 { get; protected set; }
+        public override double B6 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the operational water use during the usage stage.")]
-        public override double B7 { get; protected set; }
+        public override double B7 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the deconstruction and/or demolition during the end of life stage.")]
-        public override double C1 { get; protected set; }
+        public override double C1 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the transport during the end of life stage.")]
-        public override double C2 { get; protected set; }
+        public override double C2 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the waste processing during the end of life stage.")]
-        public override double C3 { get; protected set; }
+        public override double C3 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the disposal during the end of life stage.")]
-        public override double C4 { get; protected set; }
+        public override double C4 { get; }
 
         [AcidificationPotential]
         [Description("Resulting Acidification Potential due to the stage beyond the system boundary.")]
-        public override double D { get; protected set; }
+        public override double D { get; }
 
 
         /***************************************************/
--- LifeCycleAssessment_oM/Results/ElementResults/ElementResult.cs
+++ LifeCycleAssessment_oM/Results/ElementResults/ElementResult.cs
@@ -60,58 +60,58 @@ namespace BH.oM.LifeCycleAssessment.Results
         /***************************************************/
 
         [Description("Resulting values due to the Raw materials in the product stage.")]
-        public abstract double A1 { get; protected set; }
+        public abstract double A1 { get; }
 
         [Description("Resulting values due to the Transport in the product stage.")]
-        public abstract double A2 { get; protected set; }
+        public abstract double A2 { get; }
 
         [Description("Resulting values due to the Manufacturing in the product stage.")]
-        public abstract double A3 { get; protected set; }
+        public abstract double A3 { get; }
 
         [Description("Resulting values due to the full product stage.")]
-        public abstract double A1toA3 { get; protected set; }
+        public abstract double A1toA3 { get; }
 
         [Description("Resulting values due to the transport during the assembly stage.")]
-        public abstract double A4 { get; protected set; }
+        public abstract double A4 { get; }
 
         [Description("Resulting values due to the final assembly during the assembly stage.")]
-        public abstract double A5 { get; protected set; }
+        public abstract double A5 { get; }
 
         [Description("Resulting values due to the general use during the usage stage.")]
-        public abstract double B1 { get; protected set; }
+        public abstract double B1 { get; }
 
         [Description("Resulting values due to the maintance during the usage stage.")]
-        public abstract double B2 { get; protected set; }
+        public abstract double B2 { get; }
 
         [Description("Resulting values due to the repair during the usage stage.")]
-        public abstract double B3 { get; protected set; }
+        public abstract double B3 { get; }
 
         [Description("Resulting values due to the replacement during the usage stage.")]
-        public abstract double B4 { get; protected set; }
+        public abstract double B4 { get; }
 
         [Description("Resulting values due to the refurbishment during the usage stage.")]
-        public abstract double B5 { get; protected set; }
+        public abstract double B5 { get; }
 
         [Description("Resulting values due to the operational energy use during the usage stage.")]
-        public abstract double B6 { get; protected set; }
+        public abstract double B6 { get; }
 
         [Description("Resulting values due to the operational water use during the usage stage.")]
-        public abstract double B7 { get; protected set; }
+        public abstract double B7 { get; }
 
         [Description("Resulting values due to the deconstruction and/or demolition during the end of life stage.")]
-        public abstract double C1 { get; protected set; }
+        public abstract double C1 { get; }
 
         [Description("Resulting values due to the transport during the end of life stage.")]
-        public abstract double C2 { get; protected set; }
+        public abstract double C2 { get; }
 
         [Description("Resulting values due to the waste processing during the end of life stage.")]
-        public abstract double C3 { get; protected set; }
+        public abstract double C3 { get; }
 
         [Description("Resulting values due to the disposal during the end of life stage.")]
-        public abstract double C4 { get; protected set; }
+        public abstract double C4 { get; }
 
         [Description("Resulting values due to the stage beyond the system boundary.")]
-        public abstract double D { get; protected set; }
+        public abstract double D { get; }
 
         /***************************************************/
         /**** Constructors                              ****/
