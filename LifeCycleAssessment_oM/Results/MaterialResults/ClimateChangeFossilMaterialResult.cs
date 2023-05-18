bba164876b63cc2523a28abd5f41a81c6515cc39
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-18 10:09:45 +0200

    Bulk addition of allr easult types corresponding to the metrics
    As for metrics, AcidificationPotential -> Acidification and ClimeChange -> CLimateChangeTotal

--- LifeCycleAssessment_oM/Results/MaterialResults/ClimateChangeFossilMaterialResult.cs
+++ LifeCycleAssessment_oM/Results/MaterialResults/ClimateChangeFossilMaterialResult.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
@@ -20,114 +20,111 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
-using BH.oM.Quantities.Attributes;
 
 namespace BH.oM.LifeCycleAssessment.Results
 {
-    [Description("Result class for resulting Acidification Potential for a particular Element.")]
-    public class AcidificationPotentialElementResult : ElementResult<AcidificationPotentialMaterialResult>
+    [Description("Result class for resulting Climate change - fossil for a particular Material and EPD.")]
+    public class ClimateChangeFossilMaterialResult : MaterialResult
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
         [Description("Enum indicating the metric type the object relates to.")]
-        public override EnvironmentalMetrics MetricType { get; } = EnvironmentalMetrics.AcidificationPotential;
+        public override EnvironmentalMetrics MetricType { get; } = EnvironmentalMetrics.ClimateChangeFossil;
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Raw Material Supply module in the Product stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Raw Material Supply module in the Product stage.")]
         public override double A1 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Transport module in the Product stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Transport module in the Product stage.")]
         public override double A2 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Manufacturing module in the Product stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Manufacturing module in the Product stage.")]
         public override double A3 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the full product stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the full Product stage.")]
         public override double A1toA3 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Transport module in the Construction Process stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Transport module in the Construction Process stage.")]
         public override double A4 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Construction Installation Process module in the Construction Process stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Construction Installation Process module in the Construction Process stage.")]
         public override double A5 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Use module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Use module in the Use stage.")]
         public override double B1 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Maintenance module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Maintenance module in the Use stage.")]
         public override double B2 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Repair module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Repair module in the Use stage.")]
         public override double B3 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Replacement module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Replacement module in the Use stage.")]
         public override double B4 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Refurbishment module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Refurbishment module in the Use stage.")]
         public override double B5 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Operational Energy Use module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Operational Energy Use module in the Use stage.")]
         public override double B6 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Operational Water Use module in the Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Operational Water Use module in the Use stage.")]
         public override double B7 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the full Use stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the full Use Stage.")]
         public override double B1toB7 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the De-construction Demolition module in the End of Life stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the De-construction Demolition module in the End of Life stage.")]
         public override double C1 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Transport module in the End of Life stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Transport module in the End of Life stage.")]
         public override double C2 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Waste Processing module in the End of Life stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Waste Processing module in the End of Life stage.")]
         public override double C3 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to the Disposal module in the End of Life stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the Disposal module in the End of Life stage.")]
         public override double C4 { get; }
 
-        [AcidificationPotential]
-        [Description("Acidification Potential relating to the full End of Life stage.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to the full End of Life stage.")]
         public override double C1toC4 { get; }
 
-        [AcidificationPotential]
-        [Description("Resulting Acidification Potential relating to benefits and loads beyond the system boundary.")]
+        [ClimateChange]
+        [Description("Resulting Climate change - fossil relating to benefits and loads beyond the system boundary.")]
         public override double D { get; }
-
 
         /***************************************************/
         /**** Constructors                              ****/
         /***************************************************/
 
-        public AcidificationPotentialElementResult(
-            IComparable objectId, 
-            ScopeType scope, 
-            ObjectCategory category, 
-            IReadOnlyList<AcidificationPotentialMaterialResult> materialResults,
+        public ClimateChangeFossilMaterialResult(
+            string materialName,
+            string environmentalProductDeclarationName,
 
             double a1,
             double a2,
@@ -142,12 +139,14 @@ namespace BH.oM.LifeCycleAssessment.Results
             double b5,
             double b6,
             double b7,
+            double b1tob7,
             double c1,
             double c2,
             double c3,
             double c4,
+            double c1toc4,
             double d
-            ) : base(objectId, scope, category, materialResults)
+            ) : base(materialName, environmentalProductDeclarationName)
         {
             A1 = a1;
             A2 = a2;
@@ -162,10 +161,12 @@ namespace BH.oM.LifeCycleAssessment.Results
             B5 = b5;
             B6 = b6;
             B7 = b7;
+            B1toB7 = b1tob7;
             C1 = c1;
             C2 = c2;
             C3 = c3;
             C4 = c4;
+            C1toC4 = c1toc4;
             D = d;
         }
 
