9f76b6f146457600f614b6fc4a99f2423d929f89
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-22 08:40:14 +0200

    LULUC -> LandUse

--- LifeCycleAssessment_oM/Results/MaterialResults/ClimateChangeLandUseMaterialResult.cs
+++ LifeCycleAssessment_oM/Results/MaterialResults/ClimateChangeLandUseMaterialResult.cs
@@ -22,109 +22,104 @@
 
 using BH.oM.Base;
 using BH.oM.Quantities.Attributes;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-
 
 namespace BH.oM.LifeCycleAssessment.Results
 {
-    [Description("Result class for resulting Climate change - LULUC for a particular Element.")]
-    public class ClimateChangeLULUCElementResult : ElementResult<ClimateChangeLULUCMaterialResult>, IImmutable
+    [Description("Result class for resulting Climate change - Land Use and Land Use Change for a particular Material and EPD.")]
+    public class ClimateChangeLandUseMaterialResult : MaterialResult, IImmutable
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Raw Material Supply module in the Product stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Raw Material Supply module in the Product stage.")]
         public override double A1 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Transport module in the Product stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Transport module in the Product stage.")]
         public override double A2 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Manufacturing module in the Product stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Manufacturing module in the Product stage.")]
         public override double A3 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the full Product stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the full Product stage.")]
         public override double A1toA3 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Transport module in the Construction Process stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Transport module in the Construction Process stage.")]
         public override double A4 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Construction Installation Process module in the Construction Process stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Construction Installation Process module in the Construction Process stage.")]
         public override double A5 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Use module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Use module in the Use stage.")]
         public override double B1 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Maintenance module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Maintenance module in the Use stage.")]
         public override double B2 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Repair module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Repair module in the Use stage.")]
         public override double B3 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Replacement module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Replacement module in the Use stage.")]
         public override double B4 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Refurbishment module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Refurbishment module in the Use stage.")]
         public override double B5 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Operational Energy Use module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Operational Energy Use module in the Use stage.")]
         public override double B6 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Operational Water Use module in the Use stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Operational Water Use module in the Use stage.")]
         public override double B7 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the full Use Stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the full Use Stage.")]
         public override double B1toB7 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the De-construction Demolition module in the End of Life stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the De-construction Demolition module in the End of Life stage.")]
         public override double C1 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Transport module in the End of Life stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Transport module in the End of Life stage.")]
         public override double C2 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Waste Processing module in the End of Life stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Waste Processing module in the End of Life stage.")]
         public override double C3 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the Disposal module in the End of Life stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the Disposal module in the End of Life stage.")]
         public override double C4 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to the full End of Life stage.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to the full End of Life stage.")]
         public override double C1toC4 { get; protected set; }
 
         [ClimateChange]
-        [Description("Resulting Climate change - LULUC relating to benefits and loads beyond the system boundary.")]
+        [Description("Resulting Climate change - Land Use and Land Use Change relating to benefits and loads beyond the system boundary.")]
         public override double D { get; protected set; }
 
         /***************************************************/
         /**** Constructors                              ****/
         /***************************************************/
 
-        public ClimateChangeLULUCElementResult(
-            IComparable objectId,
-            ScopeType scope,
-            ObjectCategory category,
-            IReadOnlyList<ClimateChangeLULUCMaterialResult> materialResults,
+        public ClimateChangeLandUseMaterialResult(
+            string materialName,
+            string environmentalProductDeclarationName,
 
             double a1,
             double a2,
@@ -146,7 +141,7 @@ namespace BH.oM.LifeCycleAssessment.Results
             double c4,
             double c1toc4,
             double d
-            ) : base(objectId, scope, category, EnvironmentalMetrics.ClimateChangeLULUC, materialResults, a1, a2, a3, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d)
+            ) : base(materialName, environmentalProductDeclarationName, EnvironmentalMetrics.ClimateChangeLandUse, a1, a2, a3, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d)
         {
         }
 
