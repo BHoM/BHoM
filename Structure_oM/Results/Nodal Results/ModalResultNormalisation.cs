a85d841fe03488f04c3cc91e6f60d75025efcf7b
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-24 14:25:10 +0200

    Expand modal results normalisation enum

--- Structure_oM/Results/Nodal Results/ModalResultNormalisation.cs
+++ Structure_oM/Results/Nodal Results/ModalResultNormalisation.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace BH.oM.Structure.Results.Nodal_Results
 {
     public enum ModalResultNormalisation
     {
-        Eigenvector,
-        Mass
+        [Description("Results normalised so that the absolute value of the maximum component of the mode shape for the mode and case is equal to 1.")]
+        EigenvectorComponent,
+        [Description("Results normalised so that the square root sum of squares for the displacement of the mode and case is equal to 1.")]
+        EigenvectorTotal,
+        [Description("Results normalised so that the absolute value of the maximum component of the mass for the mode and case is equal to 1.")]
+        MassComponent,
+        [Description("Results normalised so that the square root sum of squares for mass of the mode and case is equal to 1.")]
+        MassTotal
     }
 }
