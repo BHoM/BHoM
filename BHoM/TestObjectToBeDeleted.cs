038bb065c9b9e5c995d658b896d8e537df0608b1
Pawel Baran <pawel.baran@burohappold.com>
2023-05-29 12:44:47 +0200

    test object added

---
+++ BHoM/TestObjectToBeDeleted.cs
@@ -0,0 +1,19 @@
+﻿using BH.oM.Base.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.Base
+{
+    public class TestObjectToBeDeleted : BHoMObject
+    {
+        [FilePath]
+        public virtual string FilePath { get; set; }
+
+        [FilePath(new string[] { "exe", "bat", "png" })]
+        public virtual string FilePathWithExtensions { get; set; }
+
+        [FolderPath]
+        public virtual string FolderPath { get; set; }
+    }
+}
