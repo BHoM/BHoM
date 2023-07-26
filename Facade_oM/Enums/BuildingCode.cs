789961b9c48b7aa3412fd5312409822e64b6041b
Pawel Baran <pawel.baran@burohappold.com>
2023-07-26 18:33:26 +0200

    BuildingCode and SupportType enums added

---
+++ Facade_oM/Enums/BuildingCode.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Base.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.Facade.Enums
+{
+    public enum BuildingCode
+    {
+        [DisplayText("BS EN 1990:2002")]
+        BSEN19902002,
+        ASCE705,
+        ASCE710,
+        ASCE716 
+    }
+}
---
+++ Facade_oM/Enums/SupportType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BH.oM.Facade.Enums
+{
+    public enum SupportType
+    {
+        FixFix,
+        PinPin
+    }
+}
