af474c9c60c62f8728c9fd0f11621d09fd0a9e37
Peter Nugent <Peter.Nugent@burohappold.com>
2023-08-22 14:25:27 +0100

    Addressing @pawelbaran's comments

--- Structure_oM/Elements/PileFoundation.cs
+++ Structure_oM/Elements/PileFoundation.cs
@@ -14,13 +14,13 @@ using BH.oM.Spatial.Layouts;
 
 namespace BH.oM.Structure.Elements
 {
-    [Description("PileFoundation is a composite object consisting of a pile cap and a group of piles.")]
+    [Description("A composite object representing a pile foundation. This object contains a pile cap and a pile group which can be used for structural analysis.")]
     public class PileFoundation : BHoMObject, IFoundation, IElementM
     {
         [Description("The pile cap defined as a PadFoundation.")]
         public virtual PadFoundation PileCap { get; set; }
 
-        [Description("Pile Layout")]
+        [Description("One or more PileGroup objects used to define sets of piles (grouped by matching lengths and section.")]
         public virtual List<PileGroup> PileGroups { get; set; }
     }
 }
--- Structure_oM/Elements/PileGroup.cs
+++ Structure_oM/Elements/PileGroup.cs
@@ -15,15 +15,18 @@ using BH.oM.Structure.SectionProperties;
 
 namespace BH.oM.Structure.Elements
 {
+    [Description("A group of piles defined by their length and section property. The positions are defined by a layout about the World Origin.")]
     public class PileGroup : BHoMObject, IFoundation
     {
+        [Length]
         [Description("The length of the piles within the PileGroup.")]
         public virtual double PileLength { get; set; }
 
         [Description("Section property of the PileGroup, containing all sectional constants and material as well as profile geometry and dimensions, where applicable.")]
         public virtual ISectionProperty PileSection { get; set; }
 
-        [Description("Pile Layout defining the position of the piles about the World Origin.")]
+        [Description("Pile Layout defining the position of the piles about the World Origin. when used with the PileFoundation object, the piles will be located to the centre " +
+            "of the pile using the Basis provided by the PileCap.")]
         public virtual ExplicitLayout PileLayout { get; set; }
 
     }
