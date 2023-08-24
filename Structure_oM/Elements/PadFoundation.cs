202e7366addb493f31ba26f6e4b00ca6bb62a0b8
peterjamesnugent <Peter.Nugent@burohappold.com>
2023-08-24 12:06:03 +0100

    Add Pile object in place of PileGroup following discussions

--- Structure_oM/Elements/PadFoundation.cs
+++ Structure_oM/Elements/PadFoundation.cs
@@ -22,8 +22,17 @@ namespace BH.oM.Structure.Elements
         [Description("Defines the thickness property and material of the PadFoundation.")]
         public virtual ISurfaceProperty Property { get; set; } = null;
 
-        [Description("Local x, y, and z axes of the PadFoundation as a vector Basis. Defaults to null which is interpreted to defaults when pushed to software and world axes in BHoM.")]
-        public virtual Basis Orientation { get; set; } = null;
+        //[Description("Local x, y, and z axes of the PadFoundation as a vector Basis. Defaults to null which is interpreted to defaults when pushed to software and world axes in BHoM.")]
+        //public virtual Basis Orientation { get; set; } = null;
+
+        [Angle]
+        [Description("Defines the angle that the local x and y axes are rotated around the normal (i.e. local z) of the Panel.\n" +
+             "The normal of the Panel is determined by the right hand curl rule dictated by the order of the edge list.\n" +
+             "Local x is found by projecting the global X to the plane of the Panel and is then rotated with the OrientationAngle, " +
+             "except for the case when the Normal is parallel with the global X. " +
+             "For this case the local y is instead found by projecting global Y to the plane of the Panel and is then rotated with the OrientationAngle.\n" +
+             "The remaining local axis is determined by the right hand rule.")]
+        public virtual double OrientationAngle { get; set; } = 0;
 
     }
 }
---
+++ Structure_oM/Elements/Pile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
+using BH.oM.Base.Attributes;
+using BH.oM.Base.Attributes.Enums;
+using BH.oM.Geometry;
+
+using BH.oM.Structure.SectionProperties;
+
+namespace BH.oM.Structure.Elements
+{
+    [Description("A pile object defined by it's geometry and section.")]
+    public class Pile : BHoMObject, IFoundation
+    {
+        [Description("Defines the start position of the Pile. Note that Nodes can contain Supports.")]
+        public virtual Node StartNode { get; set; }
+
+        [Description("Defines the end position of the Pile. Note that Nodes can contain Supports.")]
+        public virtual Node EndNode { get; set; }
+
+
+        [Description("Section property of the Pile, containing all sectional constants and material as well as profile geometry and dimensions, where applicable.")]
+        public virtual ISectionProperty PileSection { get; set; }
+
+        [Angle]
+        [Description("Controls the local axis orientation of the Pile \n" +
+             "For non-vertical members the local z is aligned with the global Z and rotated with the orientation angle about the local x. \n" +
+             "For vertical members the local y is aligned with the global Y and rotated with the orientation angle about the local x. \n" +
+             "A Pile is vertical if its projected length to the horizontal plane is less than 0.0001, i.e. a tolerance of 0.1mm on verticality. \n" +
+             "For general structural conventions please see the documentation.")]
+        [DocumentationURL("https://bhom.xyz/documentation/Conventions/BHoM-Structural-Conventions/", DocumentationType.Documentation)]
+        public virtual double OrientationAngle { get; set; } = 0;
+
+    }
+}
