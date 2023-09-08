604a3445cd19ac984ec9653ba6ff61ef30e0ea39
Peter Nugent <Peter.Nugent@burohappold.com>
2023-09-08 20:28:34 +0100

    Add classes for Borehole, Geology and their properties

---
+++ Ground_oM/Borehole.cs
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using System.ComponentModel;
+using System.Collections.Generic;
+using BH.oM.Base;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using BH.oM.Geometry.CoordinateSystem;
+using BH.oM.Quantities.Attributes;
+using BH.oM.Base.Attributes;
+using BH.oM.Base.Attributes.Enums;
+
+namespace BH.oM.Ground
+{
+
+    [Description("A representation of a borehole defined by a coordinate system, start point and end point based on the AGS schema.")]
+    public class GeologyUnit : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        [Description("Location identifier for the borehole unique to the project (LOCA_ID).")]
+        public virtual string id { get; set; }
+
+        [Description("The top of the borehole within the coordinate system provided (LOCA_NATE, LOCA_NATEN, LOCA_GL).")]
+        public virtual Point Top { get; set; }
+
+        [Description("The bottom of the borehole within the coordinate system provided (LOCA_ETRV, LOCA_NTRV, LOCA_FDEP).")]
+        public virtual Point Bottom { get; set; }
+
+        [Description("The coordinate system referenced by the top and bottom point. (LOCA_GREF, LOCA_NATD).")]
+        public virtual Cartesian CoordinateSystem { get; set; }
+
+        [Description("An object containing the strata, geology units and descriptions of the ground.")]
+        public virtual Geology Geology { get; set; }
+
+        [Description("A list of properties related to the borehole.")]
+        public virtual List<IGeologicalProperties> BoreholeProperties { get; set; }
+
+        /***************************************************/
+    }
+}
---
+++ Ground_oM/BoreholeProperties/Reference.cs
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using System;
+using System.ComponentModel;
+using BH.oM.Base;
+
+namespace BH.oM.Ground
+{
+
+    [Description("References to associated files, storage links or previous boreholes.")]
+    public class Reference : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+        [Description("Associated file reference including instructions and photographs (FILE_FSET).")]
+        public virtual string File { get; set; }
+
+        [Description("Link to storage of borehole data.")]
+        public virtual  string URL { get; set; }
+
+        [Description("Original hole id (LOCA_ORID).")]
+        public virtual string OriginalId { get; set; }
+
+        [Description("Original job reference (LOCA_ORJO).")]
+        public virtual string OriginalReference { get; set; }
+
+        [Description("Originating company (LOCA_ORCO).")]
+        public virtual string OriginalCompany { get; set; }
+
+        /***************************************************/
+    }
+}
