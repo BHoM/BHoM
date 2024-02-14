18c3a8aaed05bf6d3fea6dfdb76fa0df9bd163e9
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2024-02-14 19:40:23 +0000

    Promote UsageLogEntry with versioning

---
+++ BHoM/UsageLogEntry.cs
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Base.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Base
+{
+    public class UsageLogEntry : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual DateTime Time { get; set; } = DateTime.UtcNow;
+
+        public virtual string UI { get; set; } = "";
+
+        public virtual string UiVersion { get; set; } = "";
+
+        public virtual string BHoMVersion { get; set; } = "";
+
+        public virtual Guid ComponentId { get; set; } = Guid.Empty;
+
+        public virtual string CallerName { get; set; } = "";
+
+        public virtual object SelectedItem { get; set; } = null;
+
+        public virtual string FileId { get; set; } = "";
+
+        public virtual string FileName { get; set; } = "";
+
+        public virtual string ProjectID { get; set; } = "";
+
+        public virtual List<Event> Errors { get; set; } = new List<Event>();
+
+
+        /***************************************************/
+    }
+}
+
+
+
+
+
