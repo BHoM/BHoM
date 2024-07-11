37e991bef5ef274f20850bcc5158d8cf13b833ea
Kalle Edstroem <kalle.edstroem@burohappold.com>
2024-07-11 13:45:00 +0100

    Update Retaining wall base object to be 1 object and update properties

--- Structure_oM/Elements/RTBase.cs
+++ Structure_oM/Elements/RTBase.cs
@@ -4,10 +4,11 @@ using BH.oM.Base;
 using BH.oM.Dimensional;
 using BH.oM.Geometry;
 using BH.oM.Structure.MaterialFragments;
+using BH.oM.Structure.SurfaceProperties;
 
 namespace BH.oM.Structure.Elements
 {
-    public class BaseHeel : BHoMObject, IElement2D, IElementM
+    public class RTBase : BHoMObject, IElement2D, IElementM
     {
         /***************************************************/
         /**** Properties                                ****/
@@ -16,14 +17,8 @@ namespace BH.oM.Structure.Elements
         [Description("RetainingWallStem object defining the stem.")]
         public virtual PolyCurve Outline { get; set; } = null;
 
-        [Description("Thickness of the base at the toe edge")]
-        public virtual double ThicknessHeel { get; set; } = 0.0;
-
-        [Description("Thickness of the base at stem connection")]
-        public virtual double ThicknessStem { get; set; } = 0.0;
-
-        [Description("Structural material of the property.")]
-        public virtual IMaterialFragment Material { get; set; } = null;
+        [Description("Defines the thickness property and material of the Base.")]
+        public virtual ISurfaceProperty Property { get; set; } = null;
 
         /***************************************************/
 
--- Structure_oM/Elements/RetainingWall.cs
+++ Structure_oM/Elements/RetainingWall.cs
@@ -3,8 +3,6 @@ using System.ComponentModel;
 using BH.oM.Base;
 using BH.oM.Dimensional;
 using BH.oM.Geometry;
-using BH.oM.Structure.MaterialFragments;
-using BH.oM.Structure.Reinforcement;
 
 namespace BH.oM.Structure.Elements
 {
@@ -18,10 +16,7 @@ namespace BH.oM.Structure.Elements
         public virtual Stem Stem { get; set; } = new Stem();
 
         [Description("RetainingWallToe object defining the toe of the base.")]
-        public virtual BaseToe BaseToe { get; set; } = new BaseToe();
-
-        [Description("RetainingWallFloor object defining the heel of the base.")]
-        public virtual BaseHeel BaseHeel { get; set; } = new BaseHeel();
+        public virtual RTBase RTBase { get; set; } = new RTBase();
 
         [Description("The retained height of the wall. From Ground to top of wall.")] // Definition of this number. Where does it measure from. 
         public virtual double RetainedHeight { get; set; } = 0.0;
@@ -34,11 +29,6 @@ namespace BH.oM.Structure.Elements
 
         [Description("Angle of retention between wall and ground. Measured at top of wall in degrees.")] //Does BHoM have convetion for using radians or degrees. Double check this 
         public virtual double RetentionAngle { get; set; } = 0.0;
-
-        //This should maybe handled as a fragment of rebardensity instead
-        //[Description("Rebar intent")] //This should possibly be divided out to the diff retaining wall sub-objects feeding in... 
-        //public virtual IRebarIntent RebarIntent { get; set; } = null;
-
 
         /***************************************************/
     }
