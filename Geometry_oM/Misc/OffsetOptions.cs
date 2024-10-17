35e09bbb01cdd4c12025c4803eda52db7fd0e139
IsakNaslundBh <isak.naslund@burohappold.com>
2024-10-17 09:32:51 +0200

    Improve offset option descriptions

--- Geometry_oM/Misc/OffsetOptions.cs
+++ Geometry_oM/Misc/OffsetOptions.cs
@@ -35,16 +35,16 @@ namespace BH.oM.Geometry
         /**** Properties                                ****/
         /***************************************************/
 
-        [Description("If true segments shorter than tolerance are removed. Only applicable for polyline offsets. Defaults to true.")]
+        [Description("If true segments shorter than tolerance are removed. Only applicable for polyline offsets. Recomended for the general case to be set to true. To be noted that when this option is true, the topology of the polyline might change due to segments being removed. Defaults to true.")]
         public virtual bool RemoveShortSegments { get; set;} = true;
 
-        [Description("If true mehtod avoids creation of self intersections for offsets of concave curves by removal of segments tending to 0 length. Only applicable for Polyline offsets. Defaults to true.")]
+        [Description("If true mehtod avoids creation of self intersections for offsets of concave curves by removal of segments tending to 0 length. Only applicable for Polyline offsets. Recomended for the general case to be set to true. To be noted that when this option is true, the topology of the polyline might change due to segments being removed. Defaults to true.")]
         public virtual bool HandleCreatedLocalSelfIntersections { get; set; } = true;
 
-        [Description("If true mehtod avoids creation of self intersections for offsets of concave curves by removal of segments tending to 0 length. Only applicable for Polyline offsets. Defaults to true.")]
+        [Description("If true mehtod avoids creation of self intersections for offsets of concave curves checking if any vertex being offsetted is ending up on or beyond another segment. Recomended for the general case to be set to true. To be noted that when this option is true, the topology of the polyline might change due to segments being removed. Only applicable for Polyline offsets. Defaults to true.")]
         public virtual bool HandleGeneralCreatedSelfIntersections { get; set; } = true;
 
-        [Description("If true handles adjecent parallel segemnts, that is, segments that have an angle below tolerance. This is handled by removal of the vertex connection the two segments for a concave offset and introductions of one additional vertex for a convex offset. Only applicable for Polyline offsets. Defaults to true.")]
+        [Description("If true handles adjecent parallel segemnts, that is, segments that have an angle below tolerance. This is handled by removal of the vertex connection the two segments for a concave offset and introductions of one additional vertex for a convex offset. Recomended for the general case to be set to true. To be noted that when this option is true, the topology of the polyline might change due to segments being added and/or removed. Only applicable for Polyline offsets. Defaults to true.")]
         public virtual bool HandleAdjacentParallelSegments { get; set; } = true;
         
         /***************************************************/
