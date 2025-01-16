46b10bd8b9da126762189bb9b4674a8737795060
Pawel Baran <pawel.baran@burohappold.com>
2025-01-16 15:09:57 +0100

    support for formula conditions added

---
+++ Verification_oM/Conditions/FormulaCondition.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.oM.Verification.Conditions
+{
+    [Description("Condition that verifies an object based on a given formula.")]
+    public class FormulaCondition : ICondition
+    {
+        /***************************************************/
+        /****                Properties                 ****/
+        /***************************************************/
+
+        public virtual Dictionary<string, IValueSource> Inputs { get; set; } = new Dictionary<string, IValueSource>();
+
+        public virtual Dictionary<string, string> CalculationFormulae { get; set; } = new Dictionary<string, string>();
+
+        public virtual string VerificationFormula { get; set; } = "";
+
+        public virtual double Tolerance { get; set; }
+
+        /***************************************************/
+    }
+}
+
+
