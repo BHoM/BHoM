5063b9e822bb0a2c054bb4c2e06a6caf7b394100
Jack Trainor <jack.trainor@burohappold.com>
2025-03-07 13:52:07 +0000

    Update Ground_oM/ITestProperties/TriaxialResultProperties.cs
    Co-authored-by: Peter Nugent <Peter.Nugent@burohappold.com>

--- Ground_oM/ITestProperties/TriaxialResultProperties.cs
+++ Ground_oM/ITestProperties/TriaxialResultProperties.cs
@@ -67,8 +67,9 @@ namespace BH.oM.Ground
 
         [Description("Mean rate of shear (TRIT_RATE).")]
         public virtual string ShearRate { get; set; }
+        
         [Ratio]
-        [Description("Failure zone water content, if measured (TRIT_FZWC).")]
+        [Description("Failure zone water content (TRIT_FZWC).")]
         public virtual double FailureZoneWaterContent { get; set; }
 
         /***************************************************/
