8d6199eaf9246ef99d541fe2528fcfbb62b36803
Isak Näslund <isak.naslund@burohappold.com>
2025-05-16 07:14:18 +0100

    Update all metrics

--- LifeCycleAssessment_oM/MaterialFragments/EnvironmentalMetrics/AcidificationMetric.cs
+++ LifeCycleAssessment_oM/MaterialFragments/EnvironmentalMetrics/AcidificationMetric.cs
@@ -21,130 +21,26 @@
  */
 
 using BH.oM.Base;
+using BH.oM.Base.Attributes;
 using BH.oM.Quantities.Attributes;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace BH.oM.LifeCycleAssessment.MaterialFragments
 {
     [Description("Acidification, measured in moles of H+ (per EN 15804+A2, this was previously measured in SO2 per EN 15804+A1), it refers to compounds that contribute to acid rain. This environmental indicator forms part of an Environmental Product Declaration and should be evaluated based on the Quantity Type stated on the Environmental Product Declaration.")]
-    public class AcidificationMetric : EnvironmentalMetric, IImmutable
+    public class AcidificationMetric : BHoMObject, IEnvironmentalMetricFactors, IDynamicObject
     {
         /***************************************************/
         /**** Properties                                ****/
         /***************************************************/
 
+        [DynamicProperty]
         [AcidificationPerQuantity]
-        [Description("Acidification relating to the Raw Material Supply module in the Product stage.")]
-        public override double A1 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Transport module in the Product stage.")]
-        public override double A2 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Manufacturing module in the Product stage.")]
-        public override double A3 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the full Product stage.")]
-        public override double A1toA3 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Transport module in the Construction Process stage.")]
-        public override double A4 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Construction Installation Process module in the Construction Process stage.")]
-        public override double A5 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Use module in the Use stage.")]
-        public override double B1 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Maintenance module in the Use stage.")]
-        public override double B2 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Repair module in the Use stage.")]
-        public override double B3 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Replacement module in the Use stage.")]
-        public override double B4 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Refurbishment module in the Use stage.")]
-        public override double B5 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Operational Energy Use module in the Use stage.")]
-        public override double B6 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Operational Water Use module in the Use stage.")]
-        public override double B7 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the full Use Stage.")]
-        public override double B1toB7 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the De-construction Demolition module in the End of Life stage.")]
-        public override double C1 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Transport module in the End of Life stage.")]
-        public override double C2 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Waste Processing module in the End of Life stage.")]
-        public override double C3 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the Disposal module in the End of Life stage.")]
-        public override double C4 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to the full End of Life stage.")]
-        public override double C1toC4 { get; protected set; }
-
-        [AcidificationPerQuantity]
-        [Description("Acidification relating to benefits and loads beyond the system boundary.")]
-        public override double D { get; protected set; }
-
-        /***************************************************/
-        /**** Constructors                              ****/
-        /***************************************************/
-
-        public AcidificationMetric(
-            double a1,
-            double a2,
-            double a3,
-            double a1toa3,
-            double a4,
-            double a5,
-            double b1,
-            double b2,
-            double b3,
-            double b4,
-            double b5,
-            double b6,
-            double b7,
-            double b1tob7,
-            double c1,
-            double c2,
-            double c3,
-            double c4,
-            double c1toc4,
-            double d
-            ) : base(MetricType.Acidification, a1, a2, a3, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d)
-        {
-        }
+        [Description("Set of factors per module.")]
+        public virtual Dictionary<Module, double> Indicators { get; set; } = new Dictionary<Module, double>();
 
         /***************************************************/
 
     }
 }
-
-
