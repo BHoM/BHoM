44de0269f653650e7b9466d4d01a1bb4836b7f2c
Arnaud Declercq <arnaud.declercq@burohappold.com>
2025-06-04 09:13:16 +0100

    Refactoring of the versioning toolkit

---
+++ Versioning_oM/Attributes/DowngraderAttribute.cs
@@ -0,0 +1,66 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Base.Debugging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BH.oM.Versioning
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [Description("Attribute identifying a class as a downgrader for versioning.")]
+    public class DowngraderAttribute : Attribute, IImmutable
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public virtual int MajorVersion { get; private set; } = 0;
+
+        public virtual int MinorVersion { get; private set; } = 0;
+
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public DowngraderAttribute(int majorVersion, int minorVersion)
+        {
+            MajorVersion = majorVersion;
+            MinorVersion = minorVersion;
+        }
+
+        /***************************************************/
+    }
+}
+
+
+
+
+
+
