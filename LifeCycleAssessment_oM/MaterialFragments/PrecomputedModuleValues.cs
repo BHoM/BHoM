d75a2e42acaf57148f4c4c531ebbc177659c64fa
Isak Näslund <isak.naslund@burohappold.com>
2025-09-04 13:01:10 +0200

    Add class to store precomputed values for a particular module. Used by LCA_Engine when when evaluation with CombinedFactors

---
+++ LifeCycleAssessment_oM/MaterialFragments/PrecomputedModuleValues.cs
@@ -0,0 +1,22 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.oM.LifeCycleAssessment.MaterialFragments
+{
+    [Description("Class containing a set of pre-computed values per metric type for a particular module that can be used to override existing values or fill in missing values where they dont exist. Values should be the resulting total.")]
+    public class PrecomputedModuleValues : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        [Description("Dictionary containing resulting values per metric type for a particular module that can be used to override existing values or fill in missing values where they dont exist. Values should be the resulting total.")]
+        public virtual Dictionary<MetricType, double> ModuleValues { get; set; } = new Dictionary<MetricType, double>();
+
+        [Description("If true, any existing values for the module will be overwritten with the pre-computed values. If false, only missing values will be filled in.")]
+        public virtual bool OverwriteExistingValues { get; set; } = true;
+    }
+}
