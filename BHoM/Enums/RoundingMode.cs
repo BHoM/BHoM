cedeb920e8195ec7f69937b8d99cc8b3e3f4a494
Christopher Short <Christopher.Short@burohappold.com>
2026-08-27 11:41:46 +0100

    add RoundingMode Enum

---
+++ BHoM/Enums/RoundingMode.cs
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2026, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *
+ *
+ * The BHoM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License, or
+ * (at your option) any later version.
+ *
+ * The BHoM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+
+using System.ComponentModel;
+
+namespace BH.oM.Base
+{
+    [Description("How a value is snapped to a rounding step.")]
+    public enum RoundingMode
+    {
+        [Description("Round to the nearest step.")]
+        Round,
+
+        [Description("Round up (away from zero for positive values, towards zero for negative values) to the next step.")]
+        Ceiling,
+
+        [Description("Round down (towards zero for positive values, away from zero for negative values) to the previous step.")]
+        Floor
+    }
+}
+
+
